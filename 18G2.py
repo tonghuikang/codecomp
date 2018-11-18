e51c613e474f679787ee7347f3f970880040fb91
Tong Hui Kang <tonghuikang@outlook.com>
2018-11-18 15:53:55 +0800

    rename

--- 18G2.py
+++ 18G2.py
@@ -1,54 +1,83 @@
-file = open("B-small-attempt1.in", "r") 
-lines = file.read().splitlines()
+cases = []
+t = int(input())  # read a line with a single integer
+for case_num in range(1, t + 1):
+    N, Q = [int(s) for s in input().split(" ")]
+    X1, X2, A1, B1, C1, M1 = [int(s) for s in input().split(" ")]
+    Y1, Y2, A2, B2, C2, M2 = [int(s) for s in input().split(" ")]
+    Z1, Z2, A3, B3, C3, M3 = [int(s) for s in input().split(" ")]
+    cases.append([case_num, N, Q, X1, X2, A1, B1, C1, M1, Y1, Y2, A2, B2, C2, M2, Z1, Z2, A3, B3, C3, M3])
 
-tests_total = int(lines[0])
-tests = []
-test_number = 1
 
-i = 1
-while i < len(lines):
-    num_list = list(lines[i+1])
-    num_list = [int(num) for num in num_list]
     
-    tests.append((test_number, num_list))
     
-    i += 2
-    test_number += 1
+def solve_case(case):
+    case_num, N, Q, X1, X2, A1, B1, C1, M1, Y1, Y2, A2, B2, C2, M2, Z1, Z2, A3, B3, C3, M3 = case
+    X = [X1, X2]
+    Y = [Y1, Y2]
+    Z = [Z1, Z2]
 
-print(tests)
+    for i in range(2,N):
+        X.append((A1 * X[i - 1] + B1 * X[i - 2] + C1) % M1)
+        Y.append((A2 * Y[i - 1] + B2 * Y[i - 2] + C2) % M2)
+
+    for i in range(2,Q):
+        Z.append((A3 * Z[i - 1] + B3 * Z[i - 2] + C3) % M3)
+    Z = Z[:Q]
+
+    L = []
+    R = []
+    K = []
+    for i in range(N):
+        L.append(min(X[i],Y[i]) + 1)
+        R.append(max(X[i],Y[i]) + 1)
+
+    K = [z + 1 for z in Z]
+
+    num_students = sum([R[i]-L[i]+1 for i in range(N)])
+
+    counter = 0
+    for i,k in enumerate(K):
+        lock = True
+        desired_rank = k
+        lower_score = 0
+        upper_score = max(R) + 1
+        score = 0
+
+        if desired_rank > num_students:
+            score = 0
+        else:
+            while lock:
+
+                rank_L = sum([max(0, R[i] - score + 1) 
+                              if L[i] < score else R[i] - L[i] + 1 
+                              for i in range(N)])
+                rank_U = sum([max(0, R[i] - (score+1) + 1) 
+                              if L[i] < (score+1) else R[i] - L[i] + 1 
+                              for i in range(N)])
+
+                # print(rank_L, rank_U)
+
+                if desired_rank > rank_L: # score too low
+                    upper_score = score
+                    score = int((lower_score + upper_score)/2)
+                elif desired_rank < rank_U + 1:
+                    lower_score = score
+                    score = int((lower_score + upper_score)/2)
+                else:
+                    lock = False
+        # print(score)
+        counter += score*(i+1)
+    
+    result = counter
+    print("Case #{}: {}".format(case_num, result))
 
 
-def test_function(test):
-    test_number, b_list = test
+import multiprocessing as mp
+n_thread = mp.cpu_count()
+with mp.Pool(n_thread) as p:
+    results = p.map(q1, cases)
 
-    values = []
-    for i,b in enumerate(b_list):
-        # going right, destroys from right
-        rightest_index = int((len(b_list)+i)/2+0.5)-1
-        going_right = sum(b_list[i:rightest_index+1])
-        values.append(going_right)
+results.sort(key=lambda x: x[0])
 
-        # going left, destroys from left
-        leftest_index = int((i/2+0.5))
-        going_left = sum((b_list[leftest_index:i+1]))
-        values.append(going_left)
-
-#         print(leftest_index,rightest_index)
-
-        sub_list = b_list[leftest_index:rightest_index+1]
-        if rightest_index - leftest_index +1 >= len(b_list)/2:
-            for j in range(rightest_index-int(len(b_list)/2)+1):
-                spread = sub_list[j:j+int(len(b_list)/2+1)]
-    #             print(sum(spread))
-                values.append(sum(spread)%(7+10**9))
-
-    count = max(values)
-#     print(count)
-    return test_number, count
-
-
-with open("18G2.out", "w") as f:
-    for test in tests:
-        test_number, count = test_function(test)
-        print("Case #{}: {}".format(test_number, count))
-        f.writelines("Case #{}: {}\n".format(test_number, count))
+for result in results:
+    print("Case #{}: {}".format(result[0], result[1]))
