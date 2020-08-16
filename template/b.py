9e9b168fd161e9d5a3bce371ec116728340929a3
hkmac <tonghuikang@outlook.com>
2020-08-16 17:40:55 +0800

    intro to ray

--- template/b.py
+++ template/b.py
@@ -9,8 +9,12 @@ from intervaltree import IntervalTree
 # import numpy as np
 # import scipy
 
+import ray
+ray.init()
 
-def solve(lrr,wrr,hrr):  # fix inputs here
+@ray.remote
+def solve(inp):  # fix inputs here
+    lrr,wrr,hrr = inp
     console("----- solving ------")
     console("starts")
     console(lrr)
@@ -77,16 +81,14 @@ def solve(lrr,wrr,hrr):  # fix inputs here
 
 
 
-    # return a string (i.e. not a list or matrix)
-    return ""  
-
-
 def console(*args):  # the judge will not read these print statement
     # print('\033[36m', *args, '\033[0m', file=sys.stderr)
     return
 
 # fast read all
 # sys.stdin.readlines()
+
+inputs = []
 
 for case_num in range(int(input())):
     # read line as a string
@@ -119,9 +121,12 @@ for case_num in range(int(input())):
     # for _ sin range(nrows):
     #     grid.append(list(map(int,input().split())))
 
-    res = solve(lrr,wrr,hrr)  # please change
-    
-    # Google - case number required
+    inputs.append([lrr,wrr,hrr])
+
+futures = [solve.remote(inp) for inp in inputs]
+results = ray.get(futures)
+
+for case_num, res in enumerate(results):
     print("Case #{}: {}".format(case_num+1, res))
 
     # Codeforces - no case number required
