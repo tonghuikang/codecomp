f540fc48703d9175927812a3c7c75bc6eda21ea0
hkmac <tonghuikang@outlook.com>
2020-08-30 01:17:03 +0800

    a.val

--- template/a.py
+++ template/a.py
@@ -7,96 +7,31 @@ from collections import Counter, defaultdict
 # import numpy as np
 # import scipy
 
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
+# import matplotlib.pyplot as plt
+# import matplotlib.patches as patches
 
-plotting = False
+# plotting = False
 debugging = False
+# debugging = True
 
-def solve(n,k,w,
-          lrr,al,bl,cl,dl,
-          hrr,ah,bh,ch,dh):  # fix inputs here
+def solve(srr, xrr, yrr):  # fix inputs here
     console("----- solving ------")
-    console(n,k,w)
-    # console(lrr)
-    # console(al,bl,cl,dl)
-    # console(hrr)
-    # console(ah,bh,ch,dh)
+    console(srr, sum(srr))
+    console(xrr, sum(xrr))
+    console(yrr, sum(yrr))
 
-    for _ in range(k, n):
-        lrr.append(((al * lrr[-2] + bl * lrr[-1] + cl) % dl) + 1)
-        hrr.append(((ah * hrr[-2] + bh * hrr[-1] + ch) % dh) + 1)
-    
-    console("bottom_left")
-    console(lrr)
-    console("width")
-    console(w)
-    console("heights")
-    console(hrr)
+    if sum(srr) > sum(xrr) + sum(yrr) or sum(srr) < sum(xrr):
+        return -1
 
-    # ===== algorithm starts here =====
-    # cumulative variables tracked
-    corners = []
-    heapq.heapify(corners)
-    detours = 0
-    
-    base_covered = 0
-    previous_heights = 0
+    res_1 = 0
+    res_2 = 0
+    for s,x,y in zip(srr, xrr, yrr):
+        if s < x:
+            res_1 += x-s
+        if s > x+y:
+            res_2 += s-(x+y)
 
-    cur_max_height = 0
-    cur_min_height = 0
-    cur_block_start = lrr[0]
-    cur_extent = lrr[0] + w
-
-    result = []
-
-    for l,h in zip(lrr,hrr):
-
-        if l > cur_extent:  # make new block
-            base_covered += cur_extent - cur_block_start
-            previous_heights += cur_max_height
-            cur_block_start = l
-            cur_max_height, cur_min_height = h,h
-            corners = []
-            heapq.heapify(corners)
-
-        cur_extent = l+w
-
-        while corners and corners[0][1] < l:
-            heapq.heappop(corners)
-
-        if corners and -corners[0][0] < h:
-            added_detour = min(cur_max_height, h) + corners[0][0]
-            console(added_detour)
-            detours += added_detour
-        cur_max_height = max(h, cur_max_height)
-
-        heapq.heappush(corners, (-h, cur_extent))
-
-        console(base_covered, previous_heights, " ", detours, " ", cur_block_start, cur_max_height, cur_min_height)
-        res = 2*((base_covered + previous_heights) + (l - cur_block_start + detours + cur_max_height + w))
-        result.append(res)
-
-    console("result")
-    console(result)
-
-    if plotting:
-        fig1 = plt.figure()
-        ax1 = fig1.add_subplot(111, aspect='equal')
-        for l,h in list(zip(lrr,hrr))[::-1]:
-            ax1.add_patch(  # DELETE
-                patches.Rectangle((l, 0), w, h, ec="b"))
-        plt.xlim(-1, max(lrr) + w + 1)
-        plt.ylim(-1, max(hrr) + 1)
-        plt.show()
-
-    submission = 1
-    for res in result:
-        submission = submission*res%(10**9+7)
-
-
-    # return a string (i.e. not a list or matrix)
-    return submission
+    return max(res_1, res_2)
 
 
 def console(*args):  # the judge will not read these print statement
@@ -118,11 +53,19 @@ for case_num in range(int(input())):
     # lst = input().split()
 
     # read one line and parse each word as an integer
-    n,k,w = list(map(int,input().split()))
-    lrr = list(map(int,input().split()))
-    al, bl, cl, dl = list(map(int,input().split()))
-    hrr = list(map(int,input().split()))
-    ah, bh, ch, dh = list(map(int,input().split()))
+    n,k = list(map(int,input().split()))
+    srr = list(map(int,input().split()))
+    a_s, b_s, c_s, d_s = list(map(int,input().split()))
+    xrr = list(map(int,input().split()))
+    a_x, b_x, c_x, d_x = list(map(int,input().split()))
+    yrr = list(map(int,input().split()))
+    a_y, b_y, c_y, d_y = list(map(int,input().split()))
+
+
+    for _ in range(k, n):
+        srr.append(((a_s * srr[-2] + b_s * srr[-1] + c_s) % d_s))
+        xrr.append(((a_x * xrr[-2] + b_x * xrr[-1] + c_x) % d_x))
+        yrr.append(((a_y * yrr[-2] + b_y * yrr[-1] + c_y) % d_y))
 
     # read matrix and parse as integers (after reading read nrows)
     # lst = list(map(int,input().split()))
@@ -131,9 +74,7 @@ for case_num in range(int(input())):
     # for _ in range(nrows):
     #     grid.append(list(map(int,input().split())))
 
-    res = solve(n,k,w,
-                lrr,al,bl,cl,dl,
-                hrr,ah,bh,ch,dh)  # please change
+    res = solve(srr, xrr, yrr)  # please change
     
     # Google - case number required
     print("Case #{}: {}".format(case_num+1, res))
