2fa3cbfa482fbfe35daf9c554cbfe658cdd64569
hkmac <tonghuikang@outlook.com>
2020-08-30 03:06:44 +0800

    I see the error

--- template/b.py
+++ template/b.py
@@ -15,33 +15,39 @@ debugging = False
 debugging = True
 
 def solve(n, p_elim):  # fix inputs here
+    console("--- solving ---")
     console(n,p_elim)
 
     standing = [1 for _ in range(n)]
     res = [0 for _ in range(n)]
 
     for i in range(n-1):
-        
-        cs = 0
-        psum = [0]
-        total_prob = sum(standing)
-        for s in standing:
-            cs += s/total_prob
-            psum.append(cs)
-        
-        p_win = [psum[i]/(1-standing[i]/total_prob) if (1-standing[i]/total_prob) > 0 else 0 for i,x in enumerate(standing)]  # ???
+        # console()
 
-        console(i)
-        console(standing)
-        console(psum)
-        console(p_win)
-        console(res)
+        cs = 0
+        # psum = [0]
+        total_prob = sum(standing)
+
+        # for s in standing:
+        #     cs += s/total_prob
+        #     psum.append(cs)
+        # p_win = [psum[i]/(1-standing[i]/total_prob) for i,x in enumerate(standing)]  # ???
+
+        p_win = [sum(standing[:i])/(total_prob-standing[i]) for i,x in enumerate(standing)]  # ???
+
+        console("standing", standing)
+        console("standing", p_win)
+        # console("average_win_rate", sum([p*(total_prob-s)/total_prob for p,s in zip(p_win,standing)]))
+        # console(psum)
+        # console(res)
 
         res = [r+s for r,s in zip(res,standing)]
+        # p_select = 2/total_prob
         p_select = 2/(n-i)
-        standing = [s - s*p_select*(1-p)*(p_elim) - s*p_select*(p)*(1-p_elim) for s,p in zip(standing, p_win)]
+        standing = [s - p_select*s*(1-p)*(p_elim) - p_select*s*(p)*(1-p_elim) for s,p in zip(standing, p_win)]
+        console("sum(standing)", sum(standing))
 
-
+    console("sum(res)", sum(res), n*(n+1)/2 - 1)
     return res
 
 
@@ -76,7 +82,7 @@ for case_num in range(int(input())):
     # Google - case number required
     print("Case #{}:".format(case_num+1))
     for r in res:
-        print("{:.7f}".format(r))
+        print("{:.8f}".format(r))
 
     # Codeforces - no case number required
     # print(res)
