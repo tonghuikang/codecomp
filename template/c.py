b08bd258c9b85118d87f395220e4856b6149cf82
hkmac <tonghuikang@outlook.com>
2020-09-28 01:17:11 +0800

    yay at least increase a little

--- template/c.py
+++ template/c.py
@@ -1,3 +1,26 @@
+# obtained from https://codeforces.com/profile/anishde85, original sources to be determined
+# ---------------------------iye ha aam zindegi---------------------------------------------
+import math
+import random
+import heapq, bisect
+import sys
+from collections import deque, defaultdict
+from fractions import Fraction
+import sys
+import threading
+from collections import defaultdict
+# threading.stack_size(1<<27)
+mod = 10**9 + 7
+mod1 = 998244353
+
+# ------------------------------warmup----------------------------
+import os
+import sys
+from io import BytesIO, IOBase
+# sys.setrecursionlimit(300000)
+
+BUFSIZE = 8192
+
 import sys
 import heapq, functools, collections
 import math, random
@@ -59,8 +82,52 @@ def console(*args):  # the judge will not read these print statement
     # print('\033[36m', *args, '\033[0m', file=sys.stderr)
     return
 
-# fast read all
-# sys.stdin.readlines()
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
 
 for case_num in range(int(input())):
     # read line as a string
