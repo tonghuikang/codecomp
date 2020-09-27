04f5d9d484aab48618126087dd57124decdb694c
hkmac <tonghuikang@outlook.com>
2020-09-27 08:27:32 +0800

    stash

--- template/a.py
+++ template/a.py
@@ -8,15 +8,18 @@ from collections import Counter, defaultdict
 # import scipy
 
 
-def solve():  # fix inputs here
+def solve(arr, k):  # fix inputs here
     console("----- solving ------")
+    console(lst, k)
 
-    # return a string (i.e. not a list or matrix)
-    return ""  
+    brr = sorted([(-((-a)//k),i+1) for i,a in enumerate(arr)])
+
+    return [i for b,i in brr]
+
 
 
 def console(*args):  # the judge will not read these print statement
-    print('\033[36m', *args, '\033[0m', file=sys.stderr)
+    # print('\033[36m', *args, '\033[0m', file=sys.stderr)
     return
 
 # fast read all
@@ -33,7 +36,8 @@ for case_num in range(int(input())):
     # lst = input().split()
 
     # read one line and parse each word as an integer
-    # lst = list(map(int,input().split()))
+    _, k = list(map(int,input().split()))
+    lst = list(map(int,input().split()))
 
     # read matrix and parse as integers (after reading read nrows)
     # lst = list(map(int,input().split()))
@@ -42,10 +46,11 @@ for case_num in range(int(input())):
     # for _ in range(nrows):
     #     grid.append(list(map(int,input().split())))
 
-    res = solve()  # please change
+    res = solve(lst, k)  # please change
     
+    res = " ".join([str(x) for x in res])
     # Google - case number required
-    # print("Case #{}: {}".format(case_num+1, res))
+    print("Case #{}: {}".format(case_num+1, res))
 
     # Codeforces - no case number required
     # print(res)
--- template/b.py
+++ template/b.py
@@ -8,21 +8,36 @@ from collections import Counter, defaultdict
 # import scipy
 
 
-def solve():  # fix inputs here
+def solve(grid,k):  # fix inputs here
     console("----- solving ------")
-
+    console(grid,k)
     # return a string (i.e. not a list or matrix)
-    return ""  
+    grid = sorted(grid)
+    
+    curextent = -1
+    res = 0
+
+    for a,b in grid:
+        if b > curextent:
+            covered_until = max(curextent, a)
+            need_to_cover = b - covered_until
+            robots_to_cover = -((-need_to_cover)//k)
+            curextent = covered_until + k*robots_to_cover
+            console(covered_until, need_to_cover, robots_to_cover, curextent, res)
+            res += robots_to_cover
+    
+    return res
 
 
 def console(*args):  # the judge will not read these print statement
-    print('\033[36m', *args, '\033[0m', file=sys.stderr)
+    # print('\033[36m', *args, '\033[0m', file=sys.stderr)
     return
 
 # fast read all
-# sys.stdin.readlines()
+inp = sys.stdin.readlines()
+currow = 0
 
-for case_num in range(int(input())):
+for case_num in range(int(inp[currow])):
     # read line as a string
     # strr = input()
 
@@ -31,21 +46,22 @@ for case_num in range(int(input())):
     
     # read one line and parse each word as a string
     # lst = input().split()
-
+    currow += 1
     # read one line and parse each word as an integer
-    # lst = list(map(int,input().split()))
+    nrows,k = list(map(int,inp[currow].split()))
 
     # read matrix and parse as integers (after reading read nrows)
     # lst = list(map(int,input().split()))
     # nrows = lst[0]  # index containing information, please change
-    # grid = []
-    # for _ in range(nrows):
-    #     grid.append(list(map(int,input().split())))
+    grid = []
+    for _ in range(nrows):
+        currow += 1
+        grid.append(list(map(int,inp[currow].split())))
 
-    res = solve()  # please change
+    res = solve(grid,k)  # please change
     
     # Google - case number required
-    # print("Case #{}: {}".format(case_num+1, res))
+    print("Case #{}: {}".format(case_num+1, res))
 
     # Codeforces - no case number required
     # print(res)
