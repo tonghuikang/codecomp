cee97dae6c943ddfdc512b826bf1f2f363592d75
hkmac <tonghuikang@outlook.com>
2020-09-28 17:17:48 +0800

    huh at b

--- template/a.py
+++ template/a.py
@@ -8,11 +8,13 @@ from collections import Counter, defaultdict
 # import scipy
 
 
-def solve():  # fix inputs here
+def solve(n,x):  # fix inputs here
     console("----- solving ------")
 
-    # return a string (i.e. not a list or matrix)
-    return ""  
+    if n <= 2:
+        return 1
+    
+    return -(-((n-2)) // x) + 1
 
 
 def console(*args):  # the judge will not read these print statement
@@ -33,7 +35,7 @@ for case_num in range(int(input())):
     # lst = input().split()
 
     # read one line and parse each word as an integer
-    # lst = list(map(int,input().split()))
+    n,k = list(map(int,input().split()))
 
     # read matrix and parse as integers (after reading read nrows)
     # lst = list(map(int,input().split()))
@@ -42,10 +44,10 @@ for case_num in range(int(input())):
     # for _ in range(nrows):
     #     grid.append(list(map(int,input().split())))
 
-    res = solve()  # please change
+    res = solve(n,k)  # please change
     
     # Google - case number required
     # print("Case #{}: {}".format(case_num+1, res))
 
     # Codeforces - no case number required
-    # print(res)
+    print(res)
--- template/b.py
+++ template/b.py
@@ -8,11 +8,39 @@ from collections import Counter, defaultdict
 # import scipy
 
 
-def solve():  # fix inputs here
+def solve(grid, k):  # fix inputs here
     console("----- solving ------")
 
+    console(grid, k)
+
+    if k%2:
+        return "NO"
+
     # return a string (i.e. not a list or matrix)
-    return ""  
+    g = set()
+    check_1 = False    
+    check_2 = False    
+
+    for (a,b),(c,d) in grid:
+        if a == d and c == b:
+            return "YES"
+        if (a,c,b,d) in g:
+            check_1 = True
+        if c == b:
+            check_2 = True
+        g.add((a,b,c,d))
+
+    console(g)
+    console(k, check_1, check_2)
+
+    if k == 2:
+        if check_2:
+            return "YES"
+
+    if check_1 and check_2 and k > 2:
+        return "YES"
+
+    return "NO"  
 
 
 def console(*args):  # the judge will not read these print statement
@@ -33,19 +61,22 @@ for case_num in range(int(input())):
     # lst = input().split()
 
     # read one line and parse each word as an integer
-    # lst = list(map(int,input().split()))
+    nrows, k = list(map(int,input().split()))
 
     # read matrix and parse as integers (after reading read nrows)
     # lst = list(map(int,input().split()))
     # nrows = lst[0]  # index containing information, please change
-    # grid = []
-    # for _ in range(nrows):
-    #     grid.append(list(map(int,input().split())))
+    grid = []
+    for _ in range(nrows):
+        arr = []
+        arr.append(list(map(int,input().split())))
+        arr.append(list(map(int,input().split())))
+        grid.append(arr)
 
-    res = solve()  # please change
+    res = solve(grid, k)  # please change
     
     # Google - case number required
     # print("Case #{}: {}".format(case_num+1, res))
 
     # Codeforces - no case number required
-    # print(res)
+    print(res)
