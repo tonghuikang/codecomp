db599c670be3b0bd3a99bd28be223ac7e5469eb5
hkmac <tonghuikang@outlook.com>
2020-10-26 04:05:41 +0800

    not faster

--- template/d.py
+++ template/d.py
@@ -64,8 +64,8 @@ def console(*args):
 
 # if Codeforces environment
 if os.path.exists('input.txt'):
-    sys.stdin = open("input.txt","r")
-    sys.stdout = open("output.txt","w")
+    # sys.stdin = open("input.txt","r")
+    # sys.stdout = open("output.txt","w")
 
     def console(*args):
         pass
@@ -74,18 +74,58 @@ if os.path.exists('input.txt'):
 def solve(*args):
     # screen input
     console("----- solving ------")
-    console(*args)
+    # console(*args)
     console("----- ------- ------")
     return solve_(*args)
 
 
-if True:
-    # if memory is not a constraint
-    inp = iter(sys.stdin.buffer.readlines())
-    input = lambda: next(inp)
-else:
-    # if memory is a constraint
-    input = sys.stdin.buffer.readline
+import os
+import sys
+from io import BytesIO, IOBase
+ 
+BUFSIZE = 8192
+class FastIO(IOBase):
+    newlines = 0
+ 
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+ 
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+ 
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+ 
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
 
 
 for case_num in [1]:
@@ -108,10 +148,10 @@ for case_num in [1]:
     grid = []
     for _ in range(2*k):
         strr = input()
-        if strr[0] == 43:
+        if strr == '+':
             grid.append(-1)
         else:
-            i = int("".join([chr(x) for x in strr][2:-1]))
+            i = int("".join([x for x in strr][2:]))
             grid.append(i)
         # grid.append(list(map(int,input().split())))
 
