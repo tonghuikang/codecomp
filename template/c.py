d493c0aa754c604fee81898e2575ff07a6424ffd
hkmac <tonghuikang@outlook.com>
2020-11-15 22:22:14 +0800

    FORGOT TO MOD THE FIRST ROW-COL

--- template/c.py
+++ template/c.py
@@ -1,7 +1,7 @@
 import sys, os
-# import heapq, functools, collections
-# import math, random
-# from collections import Counter, defaultdict
+import heapq, functools, collections
+import math, random
+from collections import Counter, defaultdict
 
 # available on Google, not available on Codeforces
 # import numpy as np
@@ -9,52 +9,98 @@ import sys, os
 
 MOD = (10**9 + 7)
 
-# def solve_(grid, h, w):
-#     # your solution here
+def solve_(grid, h, w):
+    # your solution here
+
+    dp = [[0 for _ in row] for row in grid]
+    dp_hori = [[0 for _ in row] for row in grid]
+    dp_vert = [[0 for _ in row] for row in grid]
+    dp_diag = [[0 for _ in row] for row in grid]
+    dp[0][0] = 1
+    dp_vert[0][0] = 1
+    dp_diag[0][0] = 1
+    dp_hori[0][0] = 1
+
+    for i,row in enumerate(grid[1:], start=1):
+        if grid[i][0] == 1:
+            continue
+        dp[i][0] = dp_vert[i-1][0]
+        dp_vert[i][0] = (dp[i][0] + dp_vert[i-1][0])%MOD
+        dp_diag[i][0] = dp[i][0]
+        dp_hori[i][0] = dp[i][0]
+
+    for j,cell in enumerate(grid[0][1:], start=1):
+        if grid[0][j] == 1:
+            continue
+        dp[0][j] = dp_hori[0][j-1]
+        dp_hori[0][j] = (dp[0][j] + dp_hori[0][j-1])%MOD
+        dp_diag[0][j] = dp[0][j]
+        dp_vert[0][j] = dp[0][j]
+
+    # console(dp)
+    # console(dp_vert)
+    # console(dp_hori)
+    # console(dp_diag)
+    # console("----")
+    # console(dp_vert)
+    # console(dp_vert[1-1][1])
+    # console("-----")
+
+    for i,row in enumerate(grid[1:], start=1):
+        for j,cell in enumerate(row[1:], start=1):
+            # console(i,j,dp_vert[i-1][j],dp_hori[i][j-1],dp_diag[i-1][j-1])
+            if cell == 1:
+                continue
+            dp[i][j] = dp_vert[i-1][j] + dp_hori[i][j-1] + dp_diag[i-1][j-1]
+            dp_vert[i][j] = (dp[i][j] + dp_vert[i-1][j])%MOD
+            dp_hori[i][j] = (dp[i][j] + dp_hori[i][j-1])%MOD
+            dp_diag[i][j] = (dp[i][j] + dp_diag[i-1][j-1])%MOD
+
+    # console(dp)
+    # console(dp_vert)
+    # console(dp_hori)
+    # console(dp_diag)
+
+    return dp[-1][-1]%MOD
 
 
-
-#     return dp[-1][-1]%MOD
-
-
-# def console(*args):  
-#     # print on terminal in different color
-#     print('\033[36m', *args, '\033[0m', file=sys.stderr)
-#     pass
+def console(*args):  
+    # print on terminal in different color
+    print('\033[36m', *args, '\033[0m', file=sys.stderr)
+    pass
 
 
-# ONLINE_JUDGE = False
+ONLINE_JUDGE = False
 
-# # if Codeforces environment
-# if os.path.exists('input.txt'):
-#     ONLINE_JUDGE = True
+# if Codeforces environment
+if os.path.exists('input.txt'):
+    ONLINE_JUDGE = True
 
-# if ONLINE_JUDGE:
-#     sys.stdin = open("input.txt","r")
-#     sys.stdout = open("output.txt","w")
+if ONLINE_JUDGE:
+    sys.stdin = open("input.txt","r")
+    sys.stdout = open("output.txt","w")
 
-#     def console(*args):
-#         pass
+    def console(*args):
+        pass
 
 
-# def solve(*args):
-#     # screen input
-#     # if not ONLINE_JUDGE:
-#     #     console("----- solving ------")
-#     #     console(*args)
-#     #     console("----- ------- ------")
-#     return solve_(*args)
+def solve(*args):
+    # screen input
+    # if not ONLINE_JUDGE:
+    #     console("----- solving ------")
+    #     console(*args)
+    #     console("----- ------- ------")
+    return solve_(*args)
 
 
 if True:
     # if memory is not a constraint
-    inp = iter(sys.stdin.readlines())
+    inp = iter(sys.stdin.buffer.readlines())
     input = lambda: next(inp)
-# else:
-#     # if memory is a constraint
-#     input = sys.stdin.buffer.readline
+else:
+    # if memory is a constraint
+    input = sys.stdin.buffer.readline
 
-# input = sys.stdin.readline
 
 for case_num in [1]:
     # read line as a string
@@ -74,78 +120,14 @@ for case_num in [1]:
     # nrows = lst[0]  # index containing information, please change
     grid = []
     for _ in range(h):
-        grid.append(list(input().strip()))
+        grid.append([0 if chr(x) == '.' else 1 for x in input().strip()])
 
-    dp = [[0]*w for _ in range(h)]
-    dp_hori = [[0]*w for _ in range(h)]
-    dp_vert = [[0]*w for _ in range(h)]
-    dp_diag = [[0]*w for _ in range(h)]
-    dp[0][0] = 1
-    dp_vert[0][0] = 1
-    dp_diag[0][0] = 1
-    dp_hori[0][0] = 1
-
-    for i in range(1, len(grid)):
-        if grid[i][0] == "#":
-            continue
-        dp[i][0] = dp_vert[i-1][0]
-        dp_vert[i][0] = dp[i][0] + dp_vert[i-1][0]
-        dp_diag[i][0] = dp[i][0]
-        dp_hori[i][0] = dp[i][0]
-
-    for j in range(1, len(grid[0])):
-        if grid[0][j] == "#":
-            continue
-        dp[0][j] = dp_hori[0][j-1]
-        dp_hori[0][j] = dp[0][j] + dp_hori[0][j-1]
-        dp_diag[0][j] = dp[0][j]
-        dp_vert[0][j] = dp[0][j]
-
-    # console(dp)
-    # console(dp_vert)
-    # console(dp_hori)
-    # console(dp_diag)
-    # console("----")
-    # console(dp_vert)
-    # console(dp_vert[1-1][1])
-    # console("-----")
-
-    for i in range(1, len(grid)):
-        for j in range(1, len(grid[0])):
-            # console(i,j,dp_vert[i-1][j],dp_hori[i][j-1],dp_diag[i-1][j-1])
-            if grid[i][j] == "#":
-                continue
-            dp[i][j] = dp_vert[i-1][j] + dp_hori[i][j-1] + dp_diag[i-1][j-1]
-            dp_vert[i][j] = (dp[i][j] + dp_vert[i-1][j])
-            dp_hori[i][j] = (dp[i][j] + dp_hori[i][j-1])
-            dp_diag[i][j] = (dp[i][j] + dp_diag[i-1][j-1])
-
-            dp[i][j] %= MOD
-            dp_vert[i][j] %= MOD
-            dp_hori[i][j] %= MOD
-            dp_diag[i][j] %= MOD
-
-            # if dp[i][j] > MOD:
-            #     dp[i][j] = dp[i][j]%MOD
-            # if dp_vert[i][j] > MOD:
-            #     dp_vert[i][j] = dp_vert[i][j]%MOD
-            # if dp_hori[i][j] > MOD:
-            #     dp_hori[i][j] = dp_hori[i][j]%MOD
-            # if dp_diag[i][j] > MOD:
-            #     dp_diag[i][j] = dp_diag[i][j]%MOD
-
-    # console(dp)
-    # console(dp_vert)
-    # console(dp_hori)
-    # console(dp_diag)
-
-    # print(grid)
-    # res = solve_(grid, h, w)  # please change
+    res = solve(grid, h, w)  # please change
     
     # print result
     # Google - case number required
     # print("Case #{}: {}".format(case_num+1, res))
 
     # Codeforces - no case number required
-    print(dp[-1][-1]%MOD)
+    print(res)
     # print(*res)  # if printing a list
