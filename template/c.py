e87b91aa64b280f54f8e19cec7beb00605588bd4
hkmac <tonghuikang@outlook.com>
2020-11-15 21:40:03 +0800

    WA

--- template/c.py
+++ template/c.py
@@ -7,35 +7,41 @@ from collections import Counter, defaultdict
 # import numpy as np
 # import scipy
 
+import numpy as np
+import numba
+from numba import njit, b1, i4, i8, f8
+
 MOD = (10**9 + 7)
 
-def solve_(grid, h, w):
+@njit((i8[:,:],), cache=True)
+def solve_(grid):
     # your solution here
 
-    dp = [[0 for _ in row] for row in grid]
-    dp_hori = [[0 for _ in row] for row in grid]
-    dp_vert = [[0 for _ in row] for row in grid]
-    dp_diag = [[0 for _ in row] for row in grid]
-    dp[0][0] = 1
-    dp_vert[0][0] = 1
-    dp_diag[0][0] = 1
-    dp_hori[0][0] = 1
+    dp = np.zeros(grid.shape)
+    dp_hori = np.zeros(grid.shape)
+    dp_vert = np.zeros(grid.shape)
+    dp_diag = np.zeros(grid.shape)
+    # print(dp)
+    dp[0,0] = 1
+    dp_vert[0,0] = 1
+    dp_diag[0,0] = 1
+    dp_hori[0,0] = 1
 
-    for i,row in enumerate(grid[1:], start=1):
-        if grid[i][0] == 1:
+    for i in range(1, grid.shape[0]):
+        if grid[i,0] == 1:
             continue
-        dp[i][0] = dp_vert[i-1][0]
-        dp_vert[i][0] = dp[i][0] + dp_vert[i-1][0]
-        dp_diag[i][0] = dp[i][0]
-        dp_hori[i][0] = dp[i][0]
+        dp[i,0] = dp_vert[i-1,0]
+        dp_vert[i,0] = dp[i,0] + dp_vert[i-1,0]
+        dp_diag[i,0] = dp[i,0]
+        dp_hori[i,0] = dp[i,0]
 
-    for j,cell in enumerate(grid[0][1:], start=1):
-        if grid[0][j] == 1:
+    for j in range(1, grid.shape[1]):
+        if grid[0,j] == 1:
             continue
-        dp[0][j] = dp_hori[0][j-1]
-        dp_hori[0][j] = dp[0][j] + dp_hori[0][j-1]
-        dp_diag[0][j] = dp[0][j]
-        dp_vert[0][j] = dp[0][j]
+        dp[0,j] = dp_hori[0,j-1]
+        dp_hori[0,j] = dp[0,j] + dp_hori[0,j-1]
+        dp_diag[0,j] = dp[0,j]
+        dp_vert[0,j] = dp[0,j]
 
     # console(dp)
     # console(dp_vert)
@@ -43,25 +49,34 @@ def solve_(grid, h, w):
     # console(dp_diag)
     # console("----")
     # console(dp_vert)
-    # console(dp_vert[1-1][1])
+    # console(dp_vert[1-1,1])
     # console("-----")
 
-    for i,row in enumerate(grid[1:], start=1):
-        for j,cell in enumerate(row[1:], start=1):
-            # console(i,j,dp_vert[i-1][j],dp_hori[i][j-1],dp_diag[i-1][j-1])
-            if cell == 1:
+    for i in range(1, grid.shape[0]):
+        for j in range(1, grid.shape[1]):
+            # console(i,j,dp_vert[i-1,j],dp_hori[i,j-1],dp_diag[i-1,j-1])
+            if grid[i,j] == 1:
                 continue
-            dp[i][j] = dp_vert[i-1][j] + dp_hori[i][j-1] + dp_diag[i-1][j-1]
-            dp_vert[i][j] = (dp[i][j] + dp_vert[i-1][j])%MOD
-            dp_hori[i][j] = (dp[i][j] + dp_hori[i][j-1])%MOD
-            dp_diag[i][j] = (dp[i][j] + dp_diag[i-1][j-1])%MOD
+            dp[i,j] = dp_vert[i-1,j] + dp_hori[i,j-1] + dp_diag[i-1,j-1]
+            dp_vert[i,j] = (dp[i,j] + dp_vert[i-1,j])
+            dp_hori[i,j] = (dp[i,j] + dp_hori[i,j-1])
+            dp_diag[i,j] = (dp[i,j] + dp_diag[i-1,j-1])
+            if dp_vert[i,j] > MOD:
+                dp_vert[i,j] %= MOD
+            if dp_hori[i,j] > MOD:
+                dp_hori[i,j] %= MOD
+            if dp_diag[i,j] > MOD:
+                dp_diag[i,j] %= MOD
+            if dp[i,j] > MOD:
+                dp[i,j] %= MOD
+        
 
     # console(dp)
     # console(dp_vert)
     # console(dp_hori)
     # console(dp_diag)
 
-    return dp[-1][-1]%MOD
+    return int(dp[-1,-1]%MOD)
 
 
 def console(*args):  
@@ -115,6 +130,13 @@ for case_num in [1]:
     # read one line and parse each word as an integer
     h, w = list(map(int,input().split()))
 
+    read = sys.stdin.buffer.read
+    readline = sys.stdin.buffer.readline
+    readlines = sys.stdin.buffer.readlines
+    
+    # H, W = map(int, readline().split())
+    # grid = np.array(read().split(), np.int64)
+
     # read matrix and parse as integers (after reading read nrows)
     # lst = list(map(int,input().split()))
     # nrows = lst[0]  # index containing information, please change
@@ -122,7 +144,10 @@ for case_num in [1]:
     for _ in range(h):
         grid.append([0 if chr(x) == '.' else 1 for x in input().strip()])
 
-    res = solve(grid, h, w)  # please change
+    # print(grid)
+    grid = np.array(grid)
+
+    res = solve(grid)  # please change
     
     # print result
     # Google - case number required
