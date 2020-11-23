821512cdc5fbb241df49ff8a4ac7b4938a6b7831
hkmac <tonghuikang@outlook.com>
2020-11-23 10:30:03 +0800

    they fixed C lol

--- template/c.py
+++ template/c.py
@@ -1,16 +1,7 @@
-import sys, os
-import heapq, functools, collections
-import math, random
-from collections import Counter, defaultdict
+a,b = list(map(int,input().split()))
+c,d = list(map(int,input().split()))
 
-# available on Google, not available on Codeforces
-# import numpy as np
-# import scipy
-
-
-def solve_(a,b,c,d):
-    console(a-b, c-d)
-    # your solution here
+def solve(a,b,c,d):
     if a == c and b == d:
         return 0
 
@@ -28,71 +19,5 @@ def solve_(a,b,c,d):
 
     return 3
 
-def console(*args):  
-    # print on terminal in different color
-    print('\033[36m', *args, '\033[0m', file=sys.stderr)
-    pass
+print(solve(a,b,c,d))
 
-
-ONLINE_JUDGE = False
-
-# if Codeforces environment
-if os.path.exists('input.txt'):
-    ONLINE_JUDGE = True
-
-if ONLINE_JUDGE:
-    sys.stdin = open("input.txt","r")
-    sys.stdout = open("output.txt","w")
-
-    def console(*args):
-        pass
-
-
-def solve(*args):
-    # screen input
-    if not ONLINE_JUDGE:
-        console("----- solving ------")
-        console(*args)
-        console("----- ------- ------")
-    return solve_(*args)
-
-
-if True:
-    # if memory is not a constraint
-    inp = iter(sys.stdin.buffer.readlines())
-    input = lambda: next(inp)
-else:
-    # if memory is a constraint
-    input = sys.stdin.buffer.readline
-
-
-for case_num in [1]:
-    # read line as a string
-    # strr = input()
-
-    # read line as an integer
-    # k = int(input())
-    
-    # read one line and parse each word as a string
-    # lst = input().split()
-
-    # read one line and parse each word as an integer
-    a,b = list(map(int,input().split()))
-    c,d = list(map(int,input().split()))
-
-    # read matrix and parse as integers (after reading read nrows)
-    # lst = list(map(int,input().split()))
-    # nrows = lst[0]  # index containing information, please change
-    # grid = []
-    # for _ in range(nrows):
-    #     grid.append(list(map(int,input().split())))
-
-    res = solve(a,b,c,d)  # please change
-    
-    # print result
-    # Google - case number required
-    # print("Case #{}: {}".format(case_num+1, res))
-
-    # Codeforces - no case number required
-    print(res)
-    # print(*res)  # if printing a list
