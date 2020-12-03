48c4cbd357a3eb910bafdf326572eb85a274a04f
hkmac <tonghuikang@outlook.com>
2020-12-03 13:37:05 +0800

    meh

--- template/3.py
+++ template/3.py
@@ -16,33 +16,48 @@ def log(*args):
         print('\033[36m', *args, '\033[0m', file=sys.stderr)
 
 
-def solve_1_(lst):
+def solve_1_(arr):
     # your solution here
 
-    low, high = lst[0].split("-")
-    low, high = int(low), int(high)
-    alpha = lst[1][0]
-    password = lst[2]
+    cnt = 0
 
-    c = Counter(password)
+    for i,row in enumerate(arr):
+        if arr[i][(3*i)%len(arr[0])] == "#":
+            cnt += 1
 
-    if low <= c[alpha] <= high:
-        return 1
-
-    return 0
+    return cnt
 
 def solve_2_(lst):
+
+    # Right 1, down 1.
+    # Right 3, down 1. (This is the slope you already checked.)
+    # Right 5, down 1.
+    # Right 7, down 1.
+    # Right 1, down 2.
     # your solution here
 
-    low, high = lst[0].split("-")
-    low, high = int(low)-1, int(high)-1
-    alpha = lst[1][0]
-    password = lst[2]
+    res = []
 
-    if (password[low] == alpha) + (password[high] == alpha) == 1:
-        return 1
+    for x in [1,3,5,7]:
+        cnt = 0
+        for i,row in enumerate(arr):
+            if arr[i][(x*i)%len(arr[0])] == "#":
+                cnt += 1
 
-    return 0
+        res.append(cnt)
+
+    cnt = 0
+    x = 1
+    for i,row in enumerate(arr[::2]):
+        if row[(x*i)%len(arr[0])] == "#":
+            # if i%2 == 1:
+            cnt += 1
+        
+
+    res.append(cnt)
+
+    log(res)
+    return res[0]*res[1]*res[2]*res[3]*res[4]
 
 
 def solve_1(*args):
@@ -71,8 +86,8 @@ def read_strings(rows):
 
 overall_res = 0
 
-for case_num in range(1000):  # no loop over test case
-# for case_num in [1]:  # no loop over test case
+# for case_num in range(323):  # no loop over test case
+for case_num in [1]:  # no loop over test case
 # for case_num in range(int(input())):
 
     # read line as a string
@@ -82,17 +97,17 @@ for case_num in range(1000):  # no loop over test case
     # lst = input().split()
 
     # read line as an integer
-    lst = input().split()
+    # lst = input().split()
     
     # read one line and parse each word as an integer
     # lst = list(map(int,input().split()))
 
     # read multiple rows
     # mrr = read_matrix(k)
-    # arr = read_strings(k)
+    arr = read_strings(323)
 
-    res = solve_1(lst)  # please change
-    # res = solve_2(lst)  # please change
+    # res = solve_1(arr)  # please change
+    res = solve_2(arr)  # please change
     
     overall_res += res
     # print result
