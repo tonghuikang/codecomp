7086d94c0abe17df8e99ac6c4ee80fb8d8a9c0a5
hkmac <tonghuikang@outlook.com>
2020-12-03 12:54:50 +0800

    template

--- template/3.py
+++ template/3.py
@@ -16,19 +16,50 @@ def log(*args):
         print('\033[36m', *args, '\033[0m', file=sys.stderr)
 
 
-def solve_():
+def solve_1_(lst):
     # your solution here
 
-    return ""
+    low, high = lst[0].split("-")
+    low, high = int(low), int(high)
+    alpha = lst[1][0]
+    password = lst[2]
+
+    c = Counter(password)
+
+    if low <= c[alpha] <= high:
+        return 1
+
+    return 0
+
+def solve_2_(lst):
+    # your solution here
+
+    low, high = lst[0].split("-")
+    low, high = int(low)-1, int(high)-1
+    alpha = lst[1][0]
+    password = lst[2]
+
+    if (password[low] == alpha) + (password[high] == alpha) == 1:
+        return 1
+
+    return 0
 
 
-def solve(*args):
+def solve_1(*args):
     # screen input
     if OFFLINE_TEST:
         log("----- solving ------")
         log(*args)
         log("----- ------- ------")
-    return solve_(*args)
+    return solve_1_(*args)
+
+def solve_2(*args):
+    # screen input
+    if OFFLINE_TEST:
+        log("----- solving ------")
+        log(*args)
+        log("----- ------- ------")
+    return solve_2_(*args)
 
 
 def read_matrix(rows):
@@ -37,8 +68,12 @@ def read_matrix(rows):
 def read_strings(rows):
     return [input().strip() for _ in range(rows)]
 
+
+overall_res = 0
+
+for case_num in range(1000):  # no loop over test case
 # for case_num in [1]:  # no loop over test case
-for case_num in range(int(input())):
+# for case_num in range(int(input())):
 
     # read line as a string
     # strr = input().strip()
@@ -47,7 +82,7 @@ for case_num in range(int(input())):
     # lst = input().split()
 
     # read line as an integer
-    # k = int(input())
+    lst = input().split()
     
     # read one line and parse each word as an integer
     # lst = list(map(int,input().split()))
@@ -56,13 +91,17 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)
     # arr = read_strings(k)
 
-    res = solve()  # please change
+    res = solve_1(lst)  # please change
+    # res = solve_2(lst)  # please change
     
+    overall_res += res
     # print result
     # Google - case number required
     # print("Case #{}: {}".format(case_num+1, res))
 
     # Other platforms - no case number required
-    print(res)
+    # print(res)
     # print(len(res))  # if printing length of list
     # print(*res)  # if printing a list
+
+print(overall_res)
