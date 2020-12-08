00154e595b1fd3b971f5b5135da4d23d9cd46d28
hkmac <tonghuikang@outlook.com>
2020-12-08 13:16:30 +0800

    day 8

--- template/8.py
+++ template/8.py
@@ -16,20 +16,43 @@ def log(*args):
         print('\033[36m', *args, '\033[0m', file=sys.stderr)
 
 
-def solve_():
-    # your solution here
+def solve_1_(arr):
+    brr = []
+    for ar in arr:
+        a, c = ar.split()
+        c = int(c)
+        brr.append((a,c))
+        
+    visited = set()
+    cur = 0
+    cnt = 0
+    while True:
+        a, c = brr[cur]
+        if a == "acc":
+            cnt += c
+            cur = cur+1
+        elif a == "nop":
+            cur = cur+1
+        elif a == "jmp":
+            cur = cur + c
+        if cur in visited:
+            return -1
+        visited.add(cur)
+        log(cur,cnt)
+        if cur == 636:
+            return cnt
 
-    return ""
 
-
-def solve(*args):
+def solve_1(*args):
     # screen input
     if OFFLINE_TEST:
         log("----- solving ------")
         log(*args)
         log("----- ------- ------")
-    return solve_(*args)
+    return solve_1_(*args)
 
+
+overall_res = 0
 
 def read_matrix(rows):
     return [list(map(int,input().split())) for _ in range(rows)]
@@ -37,8 +60,11 @@ def read_matrix(rows):
 def read_strings(rows):
     return [input().strip() for _ in range(rows)]
 
+entries = []
+
+while True:
 # for case_num in [1]:  # no loop over test case
-for case_num in range(int(input())):
+# for case_num in range(1):
 
     # read line as a string
     # strr = input().strip()
@@ -55,14 +81,28 @@ for case_num in range(int(input())):
     # read multiple rows
     # mrr = read_matrix(k)
     # arr = read_strings(k)
-
-    res = solve()  # please change
     
-    # print result
-    # Google - case number required
-    # print("Case #{}: {}".format(case_num+1, res))
+    strr = input().strip()
+    if strr == "EXIT":
+        break
+    # if strr == "":
+    entries.append(strr)
 
-    # Other platforms - no case number required
-    print(res)
-    # print(len(res))  # if printing length of list
-    # print(*res)  # if printing a list
+    # overall_res += solve_1()
+
+for i in range(len(entries)):
+    entries_copy = [x for x in entries]
+    if entries_copy[i][:3] == "acc":
+        continue
+        
+    elif entries_copy[i][:3] == "nop":
+        entries_copy[i] = "jmp" + entries_copy[i][3:]
+        res = solve_1(entries_copy)
+        overall_res = max(overall_res, res)
+
+    elif entries_copy[i][:3] == "jmp":
+        entries_copy[i] = "nop" + entries_copy[i][3:]
+        res = solve_1(entries_copy)
+        overall_res = max(overall_res, res)
+
+print(overall_res)
