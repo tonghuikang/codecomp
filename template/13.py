7023c1273f7a4538084217bf6b66356792d2c2b1
hkmac <tonghuikang@outlook.com>
2020-12-13 13:17:43 +0800

    STILL NOT 100 URGH

--- template/13.py
+++ template/13.py
@@ -16,20 +16,57 @@ def log(*args):
         print('\033[36m', *args, '\033[0m', file=sys.stderr)
 
 
-def solve_():
-    # your solution here
+from functools import reduce
+def chinese_remainder(n, a):
+    sum=0
+    prod=reduce(lambda a, b: a*b, n)
+    for n_i, a_i in zip(n,a):
+        p=prod//n_i
+        sum += a_i* mul_inv(p, n_i)*p
+    return sum % prod
+def mul_inv(a, b):
+    b0= b
+    x0, x1= 0,1
+    if b== 1: return 1
+    while a>1 :
+        q=a//b
+        a, b= b, a%b
+        x0, x1=x1 -q *x0, x0
+    if x1<0 : x1+= b0
+    return x1
+n=[3,5,7]
+a=[2,3,2]
+print(chinese_remainder(n,a))
+23.0
 
-    return ""
+def solve_1_(arr):
+    nrr = [(int(a),i)[0] for i,a in enumerate(arr.split(",")) if a != "x"]
+    arr = [-(int(a),i)[1] for i,a in enumerate(arr.split(",")) if a != "x"]
+
+    print(nrr,arr)
+    minres = 10**18
+    for offset in range(1000):
+        res = chinese_remainder(nrr,[(x + offset)%n for x,n in zip(arr,nrr)])
+        minres = min(res, minres)
+        # break
+    # for i in range(b, b+1000):
+    #     for a in arr:
+    #         if i%a == 0:
+    #             return a*(b-i)
+
+    return minres
 
 
-def solve(*args):
+def solve_1(*args):
     # screen input
     if OFFLINE_TEST:
         log("----- solving ------")
         log(*args)
         log("----- ------- ------")
-    return solve_(*args)
+    return solve_1_(*args)
 
+
+overall_res = 0
 
 def read_matrix(rows):
     return [list(map(int,input().split())) for _ in range(rows)]
@@ -37,8 +74,11 @@ def read_matrix(rows):
 def read_strings(rows):
     return [input().strip() for _ in range(rows)]
 
+entries = []
+
+# while True:
 # for case_num in [1]:  # no loop over test case
-for case_num in range(int(input())):
+for case_num in range(7):
 
     # read line as a string
     # strr = input().strip()
@@ -48,21 +88,22 @@ for case_num in range(int(input())):
 
     # read line as an integer
     # k = int(input())
-    
+    # b = int(input())
+
     # read one line and parse each word as an integer
     # lst = list(map(int,input().split()))
 
     # read multiple rows
     # mrr = read_matrix(k)
     # arr = read_strings(k)
-
-    res = solve()  # please change
     
-    # print result
-    # Google - case number required
-    # print("Case #{}: {}".format(case_num+1, res))
+    strr = input().strip()
+    # if strr == "EXIT":
+    #     break
+    # # if strr == "":
+    # entries.append(strr)
 
-    # Other platforms - no case number required
-    print(res)
-    # print(len(res))  # if printing length of list
-    # print(*res)  # if printing a list
+    overall_res = solve_1(strr)
+    print(overall_res)
+
+print(overall_res)
