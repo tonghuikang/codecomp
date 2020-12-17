171a07913f868ba2c494679a34427e216d71126f
hkmac <tonghuikang@outlook.com>
2020-12-17 22:13:23 +0800

    comp

--- template/17.py
+++ template/17.py
@@ -19,29 +19,32 @@ def log(*args):
 def solve_(field):
 
     for _ in range(6):
-        new_field = [[[0 for _ in range(dims)] 
-                         for _ in range(dims)] 
-                         for _ in range(dims)]
+        new_field = [[[[0 for _ in range(dims)] 
+                          for _ in range(dims)] 
+                          for _ in range(dims)]
+                          for _ in range(dims)]
         for i,plate in enumerate(field[1:-1], start=1):
             for j,row in enumerate(plate[1:-1], start=1):
                 for k,cell in enumerate(row[1:-1], start=1):
-                    count = 0
-                    for x in [-1,0,1]:
-                        for y in [-1,0,1]:
-                            for z in [-1,0,1]:
-                                if x == y == z == 0:
-                                    continue
-                                if field[i+x][j+y][k+z] == 1:
-                                    count += 1
-                    if field[i][j][k] == 1:
-                        if count < 2 or count > 3:
-                            new_field[i][j][k] = 0
+                    for l,_ in enumerate(row[1:-1], start=1):
+                        count = 0
+                        for x in [-1,0,1]:
+                            for y in [-1,0,1]:
+                                for z in [-1,0,1]:
+                                    for v in [-1,0,1]:
+                                        if x == y == z == v == 0:
+                                            continue
+                                        if field[i+x][j+y][k+z][l+v] == 1:
+                                            count += 1
+                        if field[i][j][k][l] == 1:
+                            if count < 2 or count > 3:
+                                new_field[i][j][k][l] = 0
+                            else:
+                                new_field[i][j][k][l] = 1
                         else:
-                            new_field[i][j][k] = 1
-                    else:
-                        if count == 3:
-                            new_field[i][j][k] = 1
-        totals = sum(sum(sum(row) for row in plate) for plate in new_field)
+                            if count == 3:
+                                new_field[i][j][k][l] = 1
+        totals = sum(sum(sum(sum(cell) for cell in row) for row in plate) for plate in new_field)
         log(totals)
         field = new_field
     
@@ -68,12 +71,15 @@ dims = 30
 mid = dims//2
 
 def process(string_input):
-    field = [[[0 for _ in range(dims)] for _ in range(dims)] for _ in range(dims)]
+    field = [[[[0 for _ in range(dims)] 
+                  for _ in range(dims)] 
+                  for _ in range(dims)]
+                  for _ in range(dims)]
 
     for i,row in enumerate(string_input.split("\n")):
         for j,cell in enumerate(row):
             if cell == "#":
-                field[mid+i][mid+j][mid] = 1
+                field[mid+i][mid+j][mid][mid] = 1
     return field
 
 sample_input="""
@@ -86,7 +92,7 @@ sample_input="""
 sample_input = sample_input.strip()
 sample_input = process(sample_input)
 
-sample_res = solve(sample_input)
+# sample_res = solve(sample_input)
 # print(sample_res)
 
 
