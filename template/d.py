964cedfb9f587f90475f0caaaed2b82ed4db2d7f
hkmac <tonghuikang@outlook.com>
2020-12-19 19:32:08 +0800

    not a knapsack lol

--- template/d.py
+++ template/d.py
@@ -16,10 +16,54 @@ def log(*args):
         print('\033[36m', *args, '\033[0m', file=sys.stderr)
 
 
-def solve_():
-    # your solution here
 
-    return ""
+def knapsack2(n, weight, count, values, weights):
+    dp = [[[0] * (weight + 1) for _ in range(n + 1)] for _ in range(count + 1)]
+    for z in range(1, count + 1):
+        for y in range(1, n + 1):
+            for x in range(weight + 1):
+                if weights[y - 1] <= x:
+                    dp[z][y][x] = max(dp[z][y - 1][x],
+                                      dp[z - 1][y - 1][x - weights[y - 1]] + values[y - 1])
+                else:
+                    dp[z][y][x] = dp[z][y - 1][x]
+
+    return dp[-1][-1][-1]
+
+
+# maximise (min (sum of capacity, all_water/2 -  sum of water/2))
+# s.t. cups
+
+def solve_(mrr):
+    # your solution here
+    mrr = [(2*x[0],2*x[1]) for x in mrr]
+    log(mrr)
+
+    res = []
+    total_water = sum([x[0]//2 for x in mrr])
+
+    for k in range(1, len(mrr)+1):
+        n = len(mrr)
+        weight = total_water//2
+        count = k
+        values = [x[1]//2 for x in mrr]
+        weights = [x[0] - x[1]//2 for x in mrr]
+
+        cur = knapsack2(n, weight, count, values, weights)
+
+        log(n)
+        log(weight)
+        log(count)
+        log(values)
+        log(weights)
+        log(cur)
+        log()
+
+        res.append((total_water/2 + cur))
+    
+    # mrr = sorted(mrr)
+
+    return [x // 2 for x in res]
 
 
 def solve(*args):
@@ -37,8 +81,8 @@ def read_matrix(rows):
 def read_strings(rows):
     return [input().strip() for _ in range(rows)]
 
-# for case_num in [1]:  # no loop over test case
-for case_num in range(int(input())):
+for case_num in [1]:  # no loop over test case
+# for case_num in range(int(input())):
 
     # read line as a string
     # strr = input().strip()
@@ -47,22 +91,22 @@ for case_num in range(int(input())):
     # lst = input().split()
 
     # read line as an integer
-    # k = int(input())
+    k = int(input())
     
     # read one line and parse each word as an integer
     # lst = list(map(int,input().split()))
 
     # read multiple rows
-    # mrr = read_matrix(k)
+    mrr = read_matrix(k)
     # arr = read_strings(k)
 
-    res = solve()  # please change
+    res = solve(mrr)  # please change
     
     # print result
     # Google - case number required
     # print("Case #{}: {}".format(case_num+1, res))
 
     # Other platforms - no case number required
-    print(res)
+    # print(res)
     # print(len(res))  # if printing length of list
-    # print(*res)  # if printing a list
+    print(*res)  # if printing a list
