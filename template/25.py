982387a8bb769c7bc2d36590d01886c076f88dff
hkmac <tonghuikang@outlook.com>
2020-12-25 13:45:41 +0800

    merry xmas

--- template/25.py
+++ template/25.py
@@ -1,97 +1,16 @@
-import sys, os, getpass
-import heapq as hq
-import math, random, functools, itertools
-from collections import Counter, defaultdict, deque
-input = sys.stdin.readline
-
-# available on Google, AtCoder Python3
-# not available on Codeforces
-# import numpy as np
-# import scipy
-
-# if testing locally, print to terminal with a different color
-OFFLINE_TEST = getpass.getuser() == "hkmac"
-def log(*args):  
-    if OFFLINE_TEST:
-        print('\033[36m', *args, '\033[0m', file=sys.stderr)
-
-
-def solve_1_():
-    # your solution here
-
-    return 1
-
-
-def solve_2_():
-    # your solution here
-
-    return 1
-
-
-def solve_1(*args):
-    # screen input
-    if OFFLINE_TEST:
-        log("----- solving ------")
-        log(*args)
-        log("----- ------- ------")
-    return solve_1_(*args)
-
-def solve_2(*args):
-    # screen input
-    if OFFLINE_TEST:
-        log("----- solving ------")
-        log(*args)
-        log("----- ------- ------")
-    return solve_2_(*args)
-
-
-def read_matrix(rows):
-    return [list(map(int,input().split())) for _ in range(rows)]
-
-def read_strings(rows):
-    return [input().strip() for _ in range(rows)]
-
-
-def read_matrix(rows):
-    return [list(map(int,input().split())) for _ in range(rows)]
-
-def read_strings(rows):
-    return [input().strip() for _ in range(rows)]
-
-
-overall_res = 0
-
-def read_matrix(rows):
-    return [list(map(int,input().split())) for _ in range(rows)]
-
-def read_strings(rows):
-    return [input().strip() for _ in range(rows)]
-
-# for case_num in [1]:  # no loop over test case
-for case_num in range(int(input())):
-
-    # read line as a string
-    # strr = input().strip()
-
-    # read one line and parse each word as a string
-    # lst = input().split()
-
-    # read line as an integer
-    # k = int(input())
+x = 7
+M = 20201227
+for i in range(2,20201227+1):
+    x = (7*x) % 20201227
     
-    # read one line and parse each word as an integer
-    # lst = list(map(int,input().split()))
-
-    # read multiple rows
-    # mrr = read_matrix(k)
-    # arr = read_strings(k)
-    
-    # strr = input().strip()
-    # if strr == "EXIT":
-    #     sys.exit()
-    # entry.append(strr)
-
-    overall_res += solve_1()
-    overall_res += solve_2()
-
-    print(overall_res)
+    if x == 5764801:
+        print(i)
+    if x == 17807724:
+        print(i)
+    if x == 9789649:
+        print(9789649, i)
+        a = i
+    if x == 3647239:
+        print(3647239, i)
+        
+print(pow(3647239, a, 20201227))
