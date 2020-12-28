d60a084492cdf16702d021515348d7afb5aca3a0
hkmac <tonghuikang@outlook.com>
2020-12-29 01:35:26 +0800

    d_checker.py

--- template/d_checker.py
+++ template/d_checker.py
@@ -10,19 +10,19 @@ input = sys.stdin.readline
 # import scipy
 
 # if testing locally, print to terminal with a different color
-OFFLINE_TEST = getpass.getuser() == "hkmac"
-# OFFLINE_TEST = False  # codechef does not allow getpass
+# OFFLINE_TEST = getpass.getuser() == "hkmac"
+OFFLINE_TEST = False  # codechef does not allow getpass
 def log(*args):  
     if OFFLINE_TEST:
         print('\033[36m', *args, '\033[0m', file=sys.stderr)
 
 
-def solve_(k):
+def solve_(res):
     # your solution here
+    k = max(max(x) for x in res)
 
     arr = [x for x in range(k+1)]
     arr[0] == None
-    res = []
 
     def ceildiv(numer,denom):
         ret = -((-numer)//denom)
@@ -33,33 +33,10 @@ def solve_(k):
             return
         if top == bot:
             return
-        res.append((top,bot))
         arr[top] = ceildiv(arr[top], arr[bot])
 
-    num = k
-    cur = num
-    while num > 2:
-        next_root = max(2,math.floor(math.sqrt(num-1)))
-        for i in range(next_root+1, num):
-            operate(i,k)
-
-        operate(k, next_root)
-        if next_root != 2:
-            operate(next_root, k)
-        num = ceildiv(num, next_root)
-
-        # log(k, num, next_root, Counter(arr), arr[k])
-
-        # if num == 3:
-        #     operate(k, 2)
-        #     break
-
-    idxs = []
-    for i,a in enumerate(arr):
-        if a == 2:
-            idxs.append(i)
-    for a,b in zip(idxs, idxs[1:]):
-        operate(a, b)
+    for a,b in res:
+        operate(a,b)
 
     # log(arr)
     c = Counter(arr)
@@ -68,7 +45,7 @@ def solve_(k):
     assert(c[1] == k-1)
     assert(len(res) <= k+5)
 
-    return res
+    return "OK"
 
 
 def solve(*args):
@@ -86,8 +63,9 @@ def read_matrix(rows):
 def read_strings(rows):
     return [input().strip() for _ in range(rows)]
 
-# for case_num in [1]:  # no loop over test case
-for case_num in range(int(input())):
+print("commence")
+for case_num in range(2100):  # no loop over test case
+# for case_num in range(int(input())):
 
     # read line as a string
     # strr = input().strip()
@@ -102,32 +80,22 @@ for case_num in range(int(input())):
     # lst = list(map(int,input().split()))
 
     # read multiple rows
-    # mrr = read_matrix(k)
+    mrr = read_matrix(k)
     # arr = read_strings(k)
 
-    res = solve(k)  # please change
-    
+    res = solve(mrr)  # please change
+    print("o", end="")
+    # print(res)
     # print result
     # Google - case number required
     # print("Case #{}: {}".format(case_num+1, res))
 
     # Other platforms - no case number required
-    print(len(res))
-    for r in res:
-        print(*r)
+    # print(len(res))
+    # for r in res:
+    #     print(*r)
     # print(len(res))  # if printing length of list
     # print(*res)  # if printing a list
+    # break
+print("OK")
 
-
-# import random
-# for i in range(3,200000+1):
-#     # x = random.randint(3, 200000)
-#     solve_(i)
-#     log(i)
-
-
-# import random
-# for i in range(3,200000):
-#     x = random.randint(3, 200000)
-#     solve_(x)
-#     log(x)
