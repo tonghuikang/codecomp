c5d040b07c24516032c7fc655dba4c4893948c3a
hkmac <tonghuikang@outlook.com>
2021-01-01 23:26:43 +0800

    cpp template ok

--- cpp_conversion_course.md
+++ cpp_conversion_course.md
@@ -1,8 +1,6 @@
 # C++ conversion course
 
-
-
-
+To make `#include <bits/stdc++.h>` work
 
 ```bash
 sudo cp template/stdc++.h /Library/Developer/CommandLineTools/usr/include/c++/v1/bits/
@@ -10,3 +8,23 @@ sudo cp template/stdc++.h /Library/Developer/CommandLineTools/usr/include/c++/v1
 
 The original source is [here](https://github.com/gcc-mirror/gcc/blob/master/libstdc%2B%2B-v3/include/precompiled/stdc%2B%2B.h).
 
+
+
+### To compile the code
+
+```bash
+g++ -std=c++17 -o tmp.out -Wall -Wno-unknown-pragmas x.cpp
+```
+
+
+
+Understanding the flags
+
+- `-std=c++17`  compile with the C++17 standard. [Docs](https://gcc.gnu.org/projects/cxx-status.html#cxx17)
+- `-o tmp.out` specifies the name of the compiled code. The default would have been `a.out` [Stack Overflow](https://askubuntu.com/questions/61408/what-is-a-command-to-compile-and-run-c-programs)
+- `-Wall` enables all the warnings. [Docs](https://gcc.gnu.org/onlinedocs/gcc-4.3.2/gcc/Warning-Options.html)
+- `-Wno-unknown-pragmas` disables warning for optimizer. [Stack Overflow](https://stackoverflow.com/questions/132667/how-to-disable-pragma-warnings)
+
+
+
+(To experiment whether the optimisation really improves the result.)
--- template/x.cpp
+++ template/x.cpp
@@ -2,30 +2,20 @@
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,sse4.1,sse4.2,popcnt,abm,mmx,avx,avx2,fma,tune=native")
 
 #include <bits/stdc++.h>
-
 using namespace std;
-
-#define f first
-#define s second
-#define pb push_back
-#define mp make_pair
-#define all(v) v.begin(), v.end()
-#define sz(v) (int)v.size()
 
 // for loops
 #define REP(i, A, B) for (int i=A; i<B; ++i)  // for i in range(A,B)
-// #define REPs(i, a, s) for (int i=s; i<(a); ++i)
-#define MOO(i, a, b) for (int i=a; i<b; i++)
-#define MOOd(i,a,b) for (int i = (b)-1; i >= a; i--)
-#define M00d(i,a) for (int i = (a)-1; i >= 0; i--)
-
-// initialise arrays and set default variables - ?
+#define REPn(i, A, B) for (int i=A; i>B; --i)  // for i in range(A,B,-1)
 
 // scan to variable
-// list
-// matrix
+#define SCAN_INT(var) scanf("%d", &var);
+#define SCAN_LL(var) scanf("%lld", &var);
+#define SCAN_ARR(arr, k) REP(i, 1, n+1) scanf("%lld", &arr[i]);
+// scan matrix (tbc)
+// scan string
 
-// fast sync
+// fast io
 #define FAST ios::sync_with_stdio(0); cin.tie(0);
 
 // debug
@@ -37,59 +27,65 @@ typedef long double ld;
 typedef pair<int,int> pi;
 typedef pair<ll,ll> pll;
 typedef pair<ld,ld> pd;
-typedef complex<ld> cd;
 typedef vector<int> vi;
 typedef vector<ll> vll;
 
-// reminders
-// `1LL << j` remember the LL if you define integers larger than ...
+// constants
+const int M9 = 1e9 + 7;  // 998244353
 
-// ll MOD = 1000000007;
+// reminders
+// If the intended result is LL, make sure the first element in the operation is LL
+// e.g. instead of `1 << j`, do `1LL << j`
+
+// ------------------------------ template ends here ------------------------------
 
 const int N = 500 * 1000 + 7;
 const int P = 60;
-const int M9 = 1e9 + 7;  // 998244353
 
 int n;
-LL in[N];
-int cnt[P];
-int val[P];
+LL arr[N];
+LL cnt[P];
+LL val[P];
 
 void solve(){
 	REP(i, 0, P) cnt[i] = 0;
 	REP(i, 0, P) val[i] = (1LL << i) % M9;
 
-	scanf("%d", &n);	
+	SCAN_INT(n);
+  SCAN_ARR(arr, n);
+
 	REP(i, 1, n+1){
-		scanf("%lld", &in[i]);
     REP(j, 0, P){
-      cnt[j] += in[i] >> j & 1;
+      cnt[j] += arr[i] >> j & 1;
     }
 	}
 	
-	int ans = 0;
+	LL ans = 0;
   REP(i, 1, n+1){
-		LL exp_or = 0, exp_and = 0;
+    LL exp_or = 0;
+    LL exp_and = 0;
+
     REP(j, 0, P){
-			if(in[i] & (1LL << j)){
-				exp_or += (1LL << j) % M9 * n;
-				exp_and += (1LL << j) % M9 * cnt[j];
-			}
-			else
-				exp_or += (1LL << j) % M9 * cnt[j];
-		}
-		
-		exp_and %= M9;
-    exp_or %= M9;
+			if(arr[i] & (1LL << j)){
+				exp_or += val[j] * n;
+				exp_and += val[j] * cnt[j];
+			} else {
+				exp_or += val[j] * cnt[j];
+      }
+      // dbg(exp_or);
+      // dbg(exp_and);
+      exp_and %= M9;
+      exp_or %= M9;
+ 		}
 		ans = (ans + 1LL * exp_or * exp_and) % M9;
 	}
-	
-	printf("%d\n", ans);
+	printf("%lld\n", ans);
 }
+
 
 int main(){ FAST
 	int total_cases;
-	scanf("%d", &total_cases);
+  SCAN_INT(total_cases);
 	
 	REP(case_num, 0, total_cases){
     // printf("Case #%d: ", case_num);  // for Google and Facebook
