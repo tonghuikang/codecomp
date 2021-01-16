ca53ca313a794930deb33c61063a810afa41b61c
hkmac <tonghuikang@outlook.com>
2021-01-16 10:04:43 +0800

    add dinic

--- template/template_graphs.py
+++ template/template_graphs.py
@@ -128,8 +128,91 @@ def dijkstra_with_preprocessing(map_from_node_to_nodes_and_costs, source, target
 def floyd_warshall(map_from_node_to_nodes_and_costs, source, target, idxs=set()):
     raise NotImplementedError
 
-def max_flow(map_from_node_to_nodes_and_capcities, start, end):
-    raise NotImplementedError
+class Dinic:
+    # codeforces.com/contest/1473/submission/104332748
+    def __init__(self, N):
+        self.N = N
+        self.G = [[] for i in range(N)]
+
+    def add_edge(self, fr, to, cap):
+        forward = [to, cap, None]
+        forward[2] = backward = [fr, 0, forward]
+        self.G[fr].append(forward)
+        self.G[to].append(backward)
+
+    def add_multi_edge(self, v1, v2, cap1, cap2):
+        edge1 = [v2, cap1, None]
+        edge1[2] = edge2 = [v1, cap2, edge1]
+        self.G[v1].append(edge1)
+        self.G[v2].append(edge2)
+
+    def bfs(self, s, t):
+        self.level = level = [None]*self.N
+        deq = deque([s])
+        level[s] = 0
+        G = self.G
+        while deq:
+            v = deq.popleft()
+            lv = level[v] + 1
+            for w, cap, _ in G[v]:
+                if cap and level[w] is None:
+                    level[w] = lv
+                    deq.append(w)
+        return level[t] is not None
+
+    def dfs(self, v, t, f):
+        if v == t:
+            return f
+        level = self.level
+        for e in self.it[v]:
+            w, cap, rev = e
+            if cap and level[v] < level[w]:
+                d = self.dfs(w, t, min(f, cap))
+                if d:
+                    e[1] -= d
+                    rev[1] += d
+                    return d
+        return 0
+
+    def flow(self, s, t):
+        flow = 0
+        INF = 10**9 + 7
+        G = self.G
+        while self.bfs(s, t):
+            *self.it, = map(iter, self.G)
+            f = INF
+            while f:
+                f = self.dfs(s, t, INF)
+                flow += f
+        return flow
+
+# n = int(input())
+# a = list(map(int,input().split()))
+# b = list(map(int,input().split()))
+
+# last = [-1 for i in range(101)]
+
+# G = Dinic(n+2)
+
+# res = 0
+# for i in range(n):
+#     if b[i] > 0:
+#         res += b[i]
+#         G.add_edge(i+1,n+1,b[i])
+#     else:
+#         G.add_edge(0,i+1,-b[i])
+
+#     pre = last[a[i]]
+#     if pre!=-1:
+#         G.add_edge(pre+1,i+1,10**15)
+#     for j in range(pre+1,i):
+#         if a[i]%a[j]==0:
+#             G.add_edge(j+1,i+1,10**15)
+
+#     last[a[i]] = i
+
+# print(res-G.flow(0,n+1))
+
 
 def min_cost_flow(map_from_node_to_nodes_and_capcities, demands):
     raise NotImplementedError
