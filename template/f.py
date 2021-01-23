5e67256d790116232e9ce4ad221699cc8da80c10
hkmac <tonghuikang@outlook.com>
2021-01-23 21:45:56 +0800

    TLE and RE

--- template/f.py
+++ template/f.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import sys, getpass
-import math, random
-import functools, itertools, collections, heapq, bisect
-from collections import Counter, defaultdict, deque
+# import math, random
+# import functools, itertools, collections, heapq, bisect
+# from collections import Counter, defaultdict, deque
 input = sys.stdin.readline  # to read input quickly
 
 # available on Google, AtCoder Python3, not available on Codeforces
-# import numpy as np
+import numpy as np
 # import scipy
 
 M9 = 10**9 + 7  # 998244353
@@ -39,15 +39,33 @@ def read_strings(rows):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(n,m,arr):
     # your solution here
 
-    return ""
+    Q = np.zeros((n,n))
+    I = np.identity(n)
+    arr = set(arr)
+
+    for i in range(n):
+        if i in arr:
+            Q[i,0] = 1
+        else:
+            Q[i,i+1:i+m+1] = 1/m
+            for a in arr:
+                if i+1 <= a < i+m+1:
+                    Q[i,a] = 0
+                    Q[i,0] += 1/m
+
+    log(Q)
+    
+    X = np.linalg.inv(I-Q)
+
+    return sum(X[0])
 
 
-# for case_num in [0]:  # no loop over test case
+for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
-for case_num in range(int(input())):
+# for case_num in range(int(input())):
 
     # read line as an integer
     # k = int(input())
@@ -59,21 +77,24 @@ for case_num in range(int(input())):
     # lst = input().split()
     
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
-    # lst = list(map(int,input().split()))
+    n,m,k = list(map(int,input().split()))
+    arr = list(map(int,input().split()))
 
     # read multiple rows
     # mrr = read_matrix(k)  # and return as a list of list of int
     # arr = read_strings(k)  # and return as a list of str
 
-    res = solve()  # include input here
+    res = solve(n,m,arr)  # include input here
     
     # print result
     # Google and Facebook - case number required
     # print("Case #{}: {}".format(case_num+1, res))
 
     # Other platforms - no case number required
-    print(res)
+    if res > 10**10:
+        print(-1)
+    else:
+        print(res)
     # print(len(res))
     # print(*res)  # print a list with elements
     # for r in res:  # print each list in a different line
