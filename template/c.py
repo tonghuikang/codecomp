7064d7e5ab31e9825edd609a75c4efdef34b00f9
hkmac <tonghuikang@outlook.com>
2021-02-06 22:18:50 +0800

    C 21/100

--- template/c.py
+++ template/c.py
@@ -31,8 +31,8 @@ M9 = 10**9 + 7  # 998244353
 MAXINT = sys.maxsize
 
 # if testing locally, print to terminal with a different color
-OFFLINE_TEST = True  # quora does not allow getpass
-# OFFLINE_TEST = False  # quora does not allow getpass
+# OFFLINE_TEST = True  # quora does not allow getpass
+OFFLINE_TEST = False  # quora does not allow getpass
 def log(*args):
     if OFFLINE_TEST:
         print('\033[36m', *args, '\033[0m', file=sys.stderr)
@@ -53,16 +53,48 @@ def read_strings(rows):
 
 # ---------------------------- template ends here ----------------------------
 
+# hypothesis - checkboard is ok
 
-def solve_():
+def solve_(mrr, n):
     # your solution here
+    mrr = [(a-1,b-1,c-1,d-1) for a,b,c,d in mrr]
 
-    return ""
+    d = defaultdict(list)
+    for x1,y1,x2,y2 in mrr:
+        d[x1,y1].append((x2,y2))
+        d[x2,y2].append((x1,y1))
+    
+    res = 0
+    visited = set()
+
+    for i in range(n):
+        for j in range(n):
+            if (i,j) in visited:
+                continue
+            odds = 0
+            evens = 0
+            stack = [(i,j)]
+            visited.add((i,j))
+            while stack:
+                ci,cj = stack.pop()
+
+                if (ci+cj) % 2 == 0:
+                    evens += 1
+                else:
+                    odds += 1
+                
+                for ni,nj in d[ci,cj]:
+                    if (ni,nj) in visited:
+                        continue
+                    visited.add((ni,nj))
+                    stack.append((ni,nj))
+            res += max(odds,evens)
+    return res
 
 
-# for case_num in [0]:  # no loop over test case
+for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
-for case_num in range(int(input())):
+# for case_num in range(int(input())):
 
     # read line as an integer
     # k = int(input())
@@ -74,14 +106,14 @@ for case_num in range(int(input())):
     # lst = input().split()
     
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
+    n,k = list(map(int,input().split()))
     # lst = list(map(int,input().split()))
 
     # read multiple rows
-    # mrr = read_matrix(k)  # and return as a list of list of int
+    mrr = read_matrix(k)  # and return as a list of list of int
     # arr = read_strings(k)  # and return as a list of str
 
-    res = solve()  # include input here
+    res = solve(mrr, n)  # include input here
     
     # print result
     # Google and Facebook - case number required
