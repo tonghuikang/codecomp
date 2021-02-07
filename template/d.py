1ea43b179df4ebc3b69f680fe51796610a8a8d7b
hkmac <tonghuikang@outlook.com>
2021-02-07 23:51:49 +0800

    correction

--- template/d.py
+++ template/d.py
@@ -39,18 +39,41 @@ def read_strings(rows):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(lst):
     # your solution here
 
-    return ""
+    prev = -1
+    keep = []
+    dump = []
+    
+    for x in lst:
+        if x == prev:
+            dump.append(x)
+            keep.append(-1)
+        else:
+            keep.append(x)
+            dump.append(-1)
+        prev = x
+
+    log(dump)
+    log(keep)
+
+    res = len(keep)
+    prev = -1
+    for x in dump:
+        if prev != x:
+            res += 1
+        prev = x
+
+    return res
 
 
-# for case_num in [0]:  # no loop over test case
+for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
-for case_num in range(int(input())):
+# for case_num in range(int(input())):
 
     # read line as an integer
-    # k = int(input())
+    k = int(input())
 
     # read line as a string
     # srr = input().strip()
@@ -60,13 +83,13 @@ for case_num in range(int(input())):
     
     # read one line and parse each word as an integer
     # a,b,c = list(map(int,input().split()))
-    # lst = list(map(int,input().split()))
+    lst = list(map(int,input().split()))
 
     # read multiple rows
     # mrr = read_matrix(k)  # and return as a list of list of int
     # arr = read_strings(k)  # and return as a list of str
 
-    res = solve()  # include input here
+    res = solve(lst)  # include input here
     
     # print result
     # Google and Facebook - case number required
