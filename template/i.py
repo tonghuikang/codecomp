6fd284ae4d89f08f3fe92f2ca3ae555419043b0d
hkmac <tonghuikang@outlook.com>
2021-02-10 20:12:03 +0800

    rename

--- template/i.py
+++ template/i.py
@@ -39,88 +39,38 @@ def read_strings(rows):
 # ---------------------------- template ends here ----------------------------
 
 
-
-def solve_(n,arr):
+def solve_(mrr):
     # your solution here
+
+    # both sides the same, consider 2n numbers
+    length = len(mrr)
+    res = len(mrr)*2
+
+    c = Counter([x for x,y in mrr] + [y for x,y in mrr])
+    for v in c.values():
+        res = min(res, length*2-v)
+
+    for a,b in mrr:
+        if a == b:
+            c[a] -= 1
     
-    one_piece = sorted([x for x,s in arr if s==1])[::-1]
-    two_piece = sorted([x for x,s in arr if s==2])[::-1]
+    lst = c.most_common(4)
 
-    # log(one_piece)
-    # log(two_piece)
-
-    one_count = 0
-    two_count = 0
-    cur_cost = 0
-    res = []
-    # x <= 2y+2
-    # y <= 2x+2
-
-    for i in range(n):
-        # log(i, one_count, two_count)
-        # log(one_piece)
-        # log(two_piece)
-        if one_count < 2*two_count + 2 and two_count < 2*one_count + 2:
-            if one_piece and two_piece:
-                if one_piece[-1] < two_piece[-1]:
-                    cost = one_piece.pop()
-                    one_count += 1
-                    cur_cost += cost
-                    res.append(cur_cost)
+    for x,_ in lst:
+        for y,_ in lst:
+            if x == y:
+                continue  # considered already
+            cnt = 0
+            for a,b in mrr:
+                if (a == x and b == y) or (a == y and b == x):
                     continue
+                elif a == x or b == x or a == y or b == y:
+                    cnt += 1
                 else:
-                    cost = two_piece.pop()
-                    two_count += 1
-                    cur_cost += cost
-                    res.append(cur_cost)
-                    continue
-            elif one_piece:
-                cost = one_piece.pop()
-                one_count += 1
-                cur_cost += cost
-                res.append(cur_cost)
-                continue
-            elif two_piece:
-                cost = two_piece.pop()
-                two_count += 1
-                cur_cost += cost
-                res.append(cur_cost)
-                continue
-            else:
-                log("error")
-
-        elif one_count < 2*two_count + 2:
-            if one_piece:
-                cost = one_piece.pop()
-                one_count += 1
-                cur_cost += cost
-                res.append(cur_cost)
-                continue
-            else:
-                break
-
-        elif two_count < 2*one_count + 2:
-            if two_piece:
-                cost = two_piece.pop()
-                two_count += 1
-                cur_cost += cost
-                res.append(cur_cost)
-                continue            
-            else:
-                break
-        
-        else:
-            break
-
+                    cnt += 2
+            res = min(res, cnt)
     
-    res = res + [-1]*(n-len(res))
-
-
     return res
-
-
-def read_matrix(rows):
-    return [list(map(int,input().split())) for _ in range(rows)]
 
 
 
@@ -129,7 +79,7 @@ for case_num in [0]:  # no loop over test case
 # for case_num in range(int(input())):
 
     # read line as an integer
-    # k = int(input())
+    k = int(input())
 
     # read line as a string
     # srr = input().strip()
@@ -138,20 +88,21 @@ for case_num in [0]:  # no loop over test case
     # lst = input().split()
     
     # read one line and parse each word as an integer
-    n, = list(map(int,input().split()))
-    # srr = input().strip()
+    # a,b = list(map(int,input().split()))
     # lst = list(map(int,input().split()))
 
-    arr = read_matrix(n)
+    # read multiple rows
+    mrr = read_matrix(k)  # and return as a list of list of int
+    # arr = read_strings(k)  # and return as a list of str
 
-    res = solve(n,arr)  # include input here
+    res = solve(mrr)  # include input here
     
     # print result
     # Google and Facebook - case number required
     # print("Case #{}: {}".format(case_num+1, res))
 
     # Other platforms - no case number required
-    print("\n".join(str(x) for x in res))
+    print(res)
     # print(len(res))
     # print(*res)  # print a list with elements
     # for r in res:  # print each list in a different line
