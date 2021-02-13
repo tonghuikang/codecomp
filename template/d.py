dc9b3bf50ac948d59d148304406fdaf97aefd24e
hkmac <tonghuikang@outlook.com>
2021-02-13 21:12:58 +0800

    D WA

--- template/d.py
+++ template/d.py
@@ -39,15 +39,102 @@ def read_strings(rows):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(mrr,h,w):
     # your solution here
 
-    return ""
+    stack = [(0,0), (h-1,0), (0,w-1), (h-1,w-1)]
+    grounds = set(stack)
+    grounds_considered = set()
+
+    grounds_by_row = defaultdict(set)
+    grounds_by_col = defaultdict(set)
+
+    efficient_axis_to_consider = [(0,True),(h-1,True),(0,False),(h-1,False)]  # loc, is_row
+    efficient_axis_considered = set()
+    for loc,is_row in efficient_axis_to_consider:
+        efficient_axis_considered.add((loc,is_row))
+
+    for i,row in enumerate(mrr):
+        for j,cell in enumerate(row):
+            if cell == "#":
+                grounds.add((i,j))
+                grounds_by_row[i].add((i,j))
+                grounds_by_col[j].add((i,j))
+
+    allres = 0
+    while True:
+        while efficient_axis_to_consider:
+            loc,is_row = efficient_axis_to_consider.pop()
+            if is_row:
+                for row,col in grounds_by_row[loc]:
+                    if (row,True) not in efficient_axis_considered:
+                        efficient_axis_to_consider.append((row,True))
+                        efficient_axis_considered.add((row,True))
+                    if (col,False) not in efficient_axis_considered:
+                        efficient_axis_to_consider.append((col,False))
+                        efficient_axis_considered.add((col,False))
+                    grounds_by_col[col].remove((row,col))
+                    if len(grounds_by_col[col]) == 0:
+                        del grounds_by_col[col]
+                del grounds_by_row[loc]
+            else:
+                for row,col in grounds_by_col[loc]:
+                    if (row,True) not in efficient_axis_considered:
+                        efficient_axis_to_consider.append((row,True))
+                        efficient_axis_considered.add((row,True))
+                    if (col,False) not in efficient_axis_considered:
+                        efficient_axis_to_consider.append((col,False))
+                        efficient_axis_considered.add((col,False))
+                    grounds_by_row[row].remove((row,col))
+                    if len(grounds_by_row[row]) == 0:
+                        del grounds_by_row[row]
+                del grounds_by_col[loc]
+
+        maxlen = 0
+        nexres = None,None
+
+        for row,sett in grounds_by_row.items():
+            length = len(sett)
+            assert length > 0
+            if length > maxlen:
+                nexres = row, True
+                maxlen = length
+
+        for col,sett in grounds_by_col.items():
+            length = len(sett)
+            assert length > 0
+            if length > maxlen:
+                nexres = col, False
+                maxlen = length
+
+        # log(maxlen, nexres)
+        if maxlen == 0:  # manually add
+            break
+        
+        allres += 1
+        efficient_axis_to_consider.append(nexres)
+        efficient_axis_considered.add(nexres)
+
+    # log(efficient_axis_considered)
+
+    addn_row = 0
+    for loc in range(h):
+        if (loc,True) not in efficient_axis_considered:
+            addn_row += 1
+    
+    addn_col = 0
+    for loc in range(w):
+        if (loc,False) not in efficient_axis_considered:
+            addn_col += 1
+    
+    # log(allres, addn_row, addn_col)
+
+    return allres + min(addn_row, addn_col)
 
 
-# for case_num in [0]:  # no loop over test case
+for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
-for case_num in range(int(input())):
+# for case_num in range(int(input())):
 
     # read line as an integer
     # k = int(input())
@@ -59,14 +146,14 @@ for case_num in range(int(input())):
     # lst = input().split()
     
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
+    h,w = list(map(int,input().split()))
     # lst = list(map(int,input().split()))
 
     # read multiple rows
-    # mrr = read_matrix(k)  # and return as a list of list of int
-    # arr = read_strings(k)  # and return as a list of str
+    # mrr = read_matrix(h)  # and return as a list of list of int
+    mrr = read_strings(h)  # and return as a list of str
 
-    res = solve()  # include input here
+    res = solve(mrr,h,w)  # include input here
     
     # print result
     # Google and Facebook - case number required
