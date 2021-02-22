3f652ff7f784f088be8b0f69f78de54a3c997611
hkmac <tonghuikang@outlook.com>
2021-02-23 03:43:44 +0800

    H ok

--- template/h.py
+++ template/h.py
@@ -1,24 +1,140 @@
 #!/usr/bin/env python3
-## For the attempt to be the first to solve the easiest AtCoder questions
-
-import heapq as hq
+import sys, getpass
 import math, random
-import functools, itertools, collections
+import functools, itertools, collections, heapq, bisect
 from collections import Counter, defaultdict, deque
+input = sys.stdin.readline  # to read input quickly
 
-# read line as an integer
-# k = int(input())
+# available on Google, AtCoder Python3, not available on Codeforces
+# import numpy as np
+# import scipy
 
-# read line as a string
-# srr = input()
+M9 = 10**9 + 7  # 998244353
+# d4 = [(1,0),(0,1),(-1,0),(0,-1)]
+# d8 = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
+# d6 = [(2,0),(1,1),(-1,1),(-2,0),(-1,-1),(1,-1)]  # hexagonal layout
+MAXINT = sys.maxsize
 
-# read one line and parse each word as a string
-# lst = input().split()
+# if testing locally, print to terminal with a different color
+OFFLINE_TEST = getpass.getuser() == "hkmac"
+# OFFLINE_TEST = False  # codechef does not allow getpass
+def log(*args):
+    if OFFLINE_TEST:
+        print('\033[36m', *args, '\033[0m', file=sys.stderr)
 
-# read one line and parse each word as an integer
-a,b = list(map(int,input().split()))
-# lst = list(map(int,input().split()))
+def solve(*args):
+    # screen input
+    if OFFLINE_TEST:
+        log("----- solving ------")
+        log(*args)
+        log("----- ------- ------")
+    return solve_(*args)
 
-# include logic here
+def read_matrix(rows):
+    return [list(map(int,input().split())) for _ in range(rows)]
 
-print(a+b)
+def read_strings(rows):
+    return [input().strip() for _ in range(rows)]
+
+# ---------------------------- template ends here ----------------------------
+
+
+class Dinic:
+    # codeforces.com/contest/1473/submission/104332748
+    def __init__(self, N):
+        self.N = N
+        self.G = [[] for i in range(N)]
+
+    def add_edge(self, fr, to, cap):
+        forward = [to, cap, None]
+        forward[2] = backward = [fr, 0, forward]
+        self.G[fr].append(forward)
+        self.G[to].append(backward)
+
+    def add_multi_edge(self, v1, v2, cap1, cap2):
+        edge1 = [v2, cap1, None]
+        edge1[2] = edge2 = [v1, cap2, edge1]
+        self.G[v1].append(edge1)
+        self.G[v2].append(edge2)
+
+    def bfs(self, s, t):
+        self.level = level = [None]*self.N
+        deq = deque([s])
+        level[s] = 0
+        G = self.G
+        while deq:
+            v = deq.popleft()
+            lv = level[v] + 1
+            for w, cap, _ in G[v]:
+                if cap and level[w] is None:
+                    level[w] = lv
+                    deq.append(w)
+        return level[t] is not None
+
+    def dfs(self, v, t, f):
+        if v == t:
+            return f
+        level = self.level
+        for e in self.it[v]:
+            w, cap, rev = e
+            if cap and level[v] < level[w]:
+                d = self.dfs(w, t, min(f, cap))
+                if d:
+                    e[1] -= d
+                    rev[1] += d
+                    return d
+        return 0
+
+    def flow(self, s, t):
+        flow = 0
+        INF = 10**9 + 7
+        G = self.G
+        while self.bfs(s, t):
+            *self.it, = map(iter, self.G)
+            f = INF
+            while f:
+                f = self.dfs(s, t, INF)
+                flow += f
+        return flow
+
+
+def solve_(mrr, n):
+    # your solution here
+    G = Dinic(n)
+    for a,b in mrr:
+        G.add_edge(a,b,1)
+    
+    return G.flow(0,n-1) - 1
+
+
+for case_num in [0]:  # no loop over test case
+# for case_num in range(100):  # if the number of test cases is specified
+# for case_num in range(int(input())):
+
+    # read line as a string
+    # srr = input().strip()
+
+    # read one line and parse each word as a string
+    # lst = input().split()
+    
+    # read one line and parse each word as an integer
+    n,k = list(map(int,input().split()))
+    # lst = list(map(int,input().split()))
+
+    # read multiple rows
+    mrr = read_matrix(k)  # and return as a list of list of int
+    # arr = read_strings(k)  # and return as a list of str
+
+    res = solve(mrr, n)  # include input here
+    
+    # print result
+    # Google and Facebook - case number required
+    # print("Case #{}: {}".format(case_num+1, res))
+
+    # Other platforms - no case number required
+    # print("\n".join("{}".format(x) for x in res))
+    # print(len(res))
+    print(res)  # print a list with elements
+    # for r in res:  # print each list in a different line
+        # print(res)
+        # print(*res)
