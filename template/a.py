9cf2ceee9e41130f9133e9e670c585203cd6f33b
hkmac <tonghuikang@outlook.com>
2021-03-07 21:11:47 +0800

    panggang

--- template/a.py
+++ template/a.py
@@ -38,11 +38,61 @@ def read_strings(rows):
 
 # ---------------------------- template ends here ----------------------------
 
+def isSubsequence(s, t):
+    t,s = s,t
+    t = iter(t)
+    return all(c in t for c in s)
 
-def solve_():
+
+def solve_(arr,brr,crr,k):
     # your solution here
 
-    return ""
+    def check(xrr):
+        return isSubsequence(arr+arr, xrr) and isSubsequence(brr+brr, xrr) and isSubsequence(crr+crr, xrr)
+
+    candidate = k*"0" + "1" + k*"0"
+    if check(candidate):
+        return candidate
+
+    candidate = k*"1" + "0" + k*"1"
+    if check(candidate):
+        return candidate
+
+    candidate = k*"1" + "00" + (k-1)*"1"
+    if check(candidate):
+        return candidate
+
+    candidate = (k-1)*"1" + "00" + k*"1"
+    if check(candidate):
+        return candidate
+
+    candidate = k*"0" + "11" + (k-1)*"0"
+    if check(candidate):
+        return candidate
+
+    candidate = (k-1)*"0" + "11" + k*"0"
+    if check(candidate):
+        return candidate
+
+    candidate = k*"0" + k*"1" + "0"
+    if check(candidate):
+        return candidate
+
+    candidate = k*"1" + k*"0" + "1"
+    if check(candidate):
+        return candidate
+
+    candidate = "1" + k*"0" + k*"1"
+    if check(candidate):
+        return candidate
+
+    candidate = "0" + k*"1" + k*"0"
+    if check(candidate):
+        return candidate
+
+    log("error")
+
+    return arr + arr[0]
 
 
 # for case_num in [0]:  # no loop over test case
@@ -50,7 +100,7 @@ def solve_():
 for case_num in range(int(input())):
 
     # read line as an integer
-    # k = int(input())
+    k = int(input())
 
     # read line as a string
     # srr = input().strip()
@@ -60,13 +110,15 @@ for case_num in range(int(input())):
     
     # read one line and parse each word as an integer
     # a,b,c = list(map(int,input().split()))
-    # lst = list(map(int,input().split()))
+    arr = input().strip()
+    brr = input().strip()
+    crr = input().strip()
 
     # read multiple rows
     # mrr = read_matrix(k)  # and return as a list of list of int
     # arr = read_strings(k)  # and return as a list of str
 
-    res = solve()  # include input here
+    res = solve(arr,brr,crr,k)  # include input here
     
     # print result
     # Google and Facebook - case number required
