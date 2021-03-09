091c72bf68898e3e114bf3071568de5f29a3c642
hkmac <tonghuikang@outlook.com>
2021-03-10 01:22:26 +0800

    add kotlin template

--- template/run_kt.sh
+++ template/run_kt.sh
@@ -1,9 +1,9 @@
 echo "-------- compiling --------"
-g++ -std=c++17 -o tmp.out -Wall -Wno-unknown-pragmas $1.cpp
+kotlinc $1.kt -include-runtime -d $1.jar
 for TEST_CASE in $1*;
 	do
 	[[ $TEST_CASE == *.* ]] && continue; 
     echo "-------- running test case" $TEST_CASE "--------"
-	time ./tmp.out < $TEST_CASE
+	time java -jar $1.jar < $TEST_CASE
 done
-rm tmp.out
+rm $1.jar
