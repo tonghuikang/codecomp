38fcc1d9b64849f200ed8319e7a037ad8677a21b
hkmac <tonghuikang@outlook.com>
2021-03-09 23:34:46 +0800

    panggang

--- template/h.py
+++ template/h.py
@@ -39,8 +39,10 @@ def read_strings(rows):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(a,b):
     # your solution here
+
+    pass
 
     return ""
 
@@ -59,14 +61,14 @@ for case_num in range(int(input())):
     # lst = input().split()
     
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
+    a,b = list(map(int,input().split()))
     # lst = list(map(int,input().split()))
 
     # read multiple rows
     # mrr = read_matrix(k)  # and return as a list of list of int
     # arr = read_strings(k)  # and return as a list of str
 
-    res = solve()  # include input here
+    res = solve(a,b)  # include input here
     
     # print result
     # Google and Facebook - case number required
