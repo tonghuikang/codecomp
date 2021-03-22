0723c5aacaa953e56555d1b1870e702574d7858b
hkmac <tonghuikang@outlook.com>
2021-03-23 00:31:09 +0800

    run prof code locally

--- template/cbow.py
+++ template/cbow.py
@@ -5,7 +5,7 @@
 # 
 # ### Imports
 
-# In[1]:
+# In[18]:
 
 
 import torch
@@ -15,12 +15,13 @@ import torch.optim as optim
 import torch.nn.functional as F
 import functools
 import matplotlib.pyplot as plt
-CUDA = torch.cuda.is_available()
+# CUDA = torch.cuda.is_available()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 # ### Step 1. Produce some data based on a given text for training our CBoW model    
 
-# In[2]:
+# In[19]:
 
 
 def text_to_train(text, context_window):
@@ -35,7 +36,7 @@ def text_to_train(text, context_window):
     return data
 
 
-# In[3]:
+# In[20]:
 
 
 def create_text():
@@ -59,14 +60,14 @@ def create_text():
     return text
 
 
-# In[4]:
+# In[21]:
 
 
 text = create_text()
 print(text)
 
 
-# In[5]:
+# In[22]:
 
 
 def generate_data(text, context_window):
@@ -84,48 +85,48 @@ def generate_data(text, context_window):
     return vocab, data, word2index, index2word
 
 
-# In[6]:
+# In[23]:
 
 
 vocab, data, word2index, index2word = generate_data(text, context_window = 2)
 
 
-# In[7]:
+# In[24]:
 
 
 print(vocab)
 
 
-# In[8]:
+# In[25]:
 
 
 print(word2index)
 
 
-# In[9]:
+# In[26]:
 
 
 print(index2word)
 
 
-# In[10]:
+# In[27]:
 
 
 print(data)
 
 
-# In[11]:
+# In[28]:
 
 
 def words_to_tensor(words: list, w2i: dict, dtype = torch.FloatTensor):
     tensor =  dtype([w2i[word] for word in words])
-    tensor = tensor.cuda()
+    tensor = tensor.to(device)
     return Variable(tensor)
 
 
 # ### Step 2. Create a CBoW model and train
 
-# In[12]:
+# In[29]:
 
 
 class CBOW(nn.Module):
@@ -143,27 +144,27 @@ class CBOW(nn.Module):
         return out
 
 
-# In[13]:
+# In[30]:
 
 
 # Create model and pass to CUDA
 model = CBOW(context_size = 2, embedding_size = 20, vocab_size = len(vocab))
-model = model.cuda()
+model = model.to(device)
 model.train()
 
 
-# In[ ]:
+# In[42]:
 
 
 # Define training parameters
 learning_rate = 0.001
-epochs = 1000
+epochs = 20
 torch.manual_seed(28)
 loss_func = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=learning_rate)
 
 
-# In[ ]:
+# In[43]:
 
 
 def get_prediction(context, model, word2index, index2word):
@@ -181,7 +182,7 @@ def get_prediction(context, model, word2index, index2word):
     return index2word[index.item()]
 
 
-# In[ ]:
+# In[44]:
 
 
 def check_accuracy(model, data, word2index, index2word):
@@ -196,7 +197,7 @@ def check_accuracy(model, data, word2index, index2word):
     return correct/len(data)
 
 
-# In[ ]:
+# In[45]:
 
 
 losses = []
@@ -226,7 +227,7 @@ for epoch in range(epochs):
         total_loss += loss.data.item()
     
     # Display
-    if epoch % 10 == 0:
+    if epoch % 2 == 0:
         accuracy = check_accuracy(model, data, word2index, index2word)
         print("Accuracy after epoch {} is {}".format(epoch, accuracy))
         accuracies.append(accuracy)
@@ -235,7 +236,7 @@ for epoch in range(epochs):
 
 # ### 3. Visualization
 
-# In[ ]:
+# In[46]:
 
 
 # Display losses over time
@@ -244,7 +245,7 @@ plt.plot(losses)
 plt.show()
 
 
-# In[ ]:
+# In[47]:
 
 
 # Display accuracy over time
@@ -253,7 +254,7 @@ plt.plot(accuracies)
 plt.show()
 
 
-# In[ ]:
+# In[41]:
 
 
 word1 = words_to_tensor(["boys"], word2index, dtype = torch.LongTensor)
@@ -272,11 +273,17 @@ print(torch.dot(w1, w2).item())
 print(torch.dot(w1, w3).item())
 
 
-# In[14]:
+# In[ ]:
 
 
 if __name__ == "__main__":
-    get_ipython().system('jupyter nbconvert --to script lung_dataset.ipynb')
+    get_ipython().system('jupyter nbconvert --to script cbow.ipynb')
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
