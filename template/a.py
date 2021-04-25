b5519d0a16fd859cfb60cec2c1304104e3615064
hkmac <tonghuikang@outlook.com>
2021-04-26 01:41:43 +0800

    give up lol

--- template/a.py
+++ template/a.py
@@ -45,52 +45,55 @@ def take_min(angle):
 def solve_(aa,bb,cc):
     aa,bb,cc = sorted([aa,bb,cc])
     # your solution here
-    res_arr = []
+    # res_arr = set()
 
     # for z1,z2,z3 in itertools.product([-1,1], repeat=3):
-    if True:
-        for a,b,c in itertools.permutations([aa,bb,cc]):
-            d,e,f = abs(a-b), abs(b-c), abs(a-c)
-            d,e,f = take_min(d), take_min(e), take_min(f)
-            # d = (d*z1)%M
-            # e = (e*z2)%M
-            # f = (f*z3)%M
-            for p in range(12*60):  # number of seconds elapsed
-                # if p == 62:
-                #     log(d, p, (d + p*M) % 719)
-                if (d + p*M) % 719 != 0 and (d - p*M) % 719 != 0:
-                    continue
-                x = ((d + p*M)//719)%(12*60*60*10**9)
-                diff1 = 720*x - p*M - (12*x - (p//60)*M)
-                diff2 = 12*x - (p//60)*M - x
-                diff1 = take_min(abs(diff1))
-                diff2 = take_min(abs(diff2))
-                if sorted([diff1,diff2]) == sorted([e,f]):
-                    # log("ok", x)
+    # if True:
+    #     for a,b,c in itertools.permutations([aa,bb,cc]):
+    #         d,e,f = abs(a-b), abs(b-c), abs(a-c)
+    #         d,e,f = take_min(d), take_min(e), take_min(f)
+    #         d = (d*z1)%M
+    #         e = (e*z2)%M
+    #         f = (f*z3)%M
+    #         for p in range(12*60):  # number of seconds elapsed
+    #             # if p == 62:
+    #             #     log(d, p, (d + p*M) % 719)
+    #             if (d + p*M) % 719 != 0 and (d - p*M) % 719 != 0:
+    #                 continue
+    #             x = ((d + p*M)//719)%(12*60*60*10**9)
+    #             diff1 = 720*x - p*M - (12*x - (p//60)*M)
+    #             diff2 = 12*x - (p//60)*M - x
+    #             diff1 = take_min(abs(diff1))
+    #             diff2 = take_min(abs(diff2))
+    #             if sorted([diff1,diff2]) == sorted([e,f]):
+    #                 # log("ok", x)
 
-                    # log("Case #{}: {} {} {} {}".format(case_num, 
-                    #     (x//(60*60*10**9)),
-                    #     (x//(60*10**9))%60,
-                    #     (x//(10**9))%60,
-                    #     (x//(1))%(10**9)
-                    # ))
-                    res_arr.append(x)
+    #                 # log("Case #{}: {} {} {} {}".format(case_num, 
+    #                 #     (x//(60*60*10**9)),
+    #                 #     (x//(60*10**9))%60,
+    #                 #     (x//(10**9))%60,
+    #                 #     (x//(1))%(10**9)
+    #                 # ))
+    #                 res_arr.add(x)
 
-    LARGE = 12*60*60*10**9
-    new_arr = []
-    for x in res_arr:
-        new_arr.append(x)
-        if x != 0:
-            new_arr.append(LARGE-x)
+    # LARGE = 12*60*60*10**9
+    # new_arr = []
+    # for x in res_arr:
+    #     new_arr.append(x)
+    #     if x != 0:
+    #         new_arr.append(LARGE-x)
             
-    log(new_arr)
+    # log(new_arr)
 
-    for nsecs in new_arr:
+    for nsecs in range(0,12*60*60):
+        nsecs = nsecs*10**9
         p,q,r = nsecs, (720//60*nsecs)%(60*720*10**9), (720*nsecs)%(60*720*10**9) 
         p,q,r = sorted([p,q,r])
-        if p-aa == q-bb == r-cc:
-            return nsecs
+        for a,b,c in itertools.permutations([aa,bb,cc]):
+            if (p-a)%M == (q-b)%M == (r-c)%M:
+                return nsecs
     
+    assert False
     return 0
 
 
@@ -119,7 +122,7 @@ for case_num in range(int(input())):
     
     # print result
     # Google and Facebook - case number required
-    print("Case #{}: {} {} {} {}".format(case_num, 
+    print("Case #{}: {} {} {} {}".format(case_num + 1, 
         (x//(60*60*10**9)),
         (x//(60*10**9))%60,
         (x//(10**9))%60,
