80bcd534ee903c08a06c1168cf1072c75c751348
hkmac <tonghuikang@outlook.com>
2021-04-30 00:49:41 +0800

    still TLE

--- template/d.py
+++ template/d.py
@@ -1,120 +1,67 @@
 #!/usr/bin/env python3
 import sys, getpass
-import math, random
-import functools, itertools, collections, heapq, bisect
-from collections import Counter, defaultdict, deque
 input = sys.stdin.readline  # to read input quickly
 
-# available on Google, AtCoder Python3, not available on Codeforces
-# import numpy as np
-# import scipy
-
-M9 = 10**9 + 7  # 998244353
-# d4 = [(1,0),(0,1),(-1,0),(0,-1)]
-# d8 = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
-# d6 = [(2,0),(1,1),(-1,1),(-2,0),(-1,-1),(1,-1)]  # hexagonal layout
-MAXINT = sys.maxsize
-
-# if testing locally, print to terminal with a different color
-OFFLINE_TEST = getpass.getuser() == "hkmac"
-# OFFLINE_TEST = False  # codechef does not allow getpass
-def log(*args):
-    if OFFLINE_TEST:
-        print('\033[36m', *args, '\033[0m', file=sys.stderr)
-
-def solve(*args):
-    # screen input
-    if OFFLINE_TEST:
-        log("----- solving ------")
-        log(*args)
-        log("----- ------- ------")
-    return solve_(*args)
-
-def read_matrix(rows):
-    return [list(map(int,input().split())) for _ in range(rows)]
-
-def read_strings(rows):
-    return [input().strip() for _ in range(rows)]
 
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_(arr, brr, k):
-    # your solution here
-
-    mult = [a*b for a,b in zip(arr,brr)]
-    # consider all possible centres
-    log(sum(mult))
-
-    # odd centres
-    max_addres = 0
-    for c in range(k):
-        curres = 0
-        for i in range(1, k):
-            left = c-i
-            righ = c+i
-            if left < 0 or righ >= k:
-                break
-            old = mult[left] + mult[righ]
-            new = arr[righ]*brr[left] + arr[left]*brr[righ]
-            # log(left, righ)
-            curres += new - old
-            max_addres = max(max_addres, curres)
-        
-    # log("x")
-    for c in range(k):
-        curres = 0
-        # log()
-        # limit = min(c+1, k-c)
-        # log(limit)
-        for i in range(1, k):
-            left = c-i
-            righ = c+i-1
-            if left < 0 or righ >= k:
-                break
-            old = mult[left] + mult[righ]
-            new = arr[righ]*brr[left] + arr[left]*brr[righ]
-            # log(left, righ)
-            curres += new - old
-            max_addres = max(max_addres, curres)
 
 
-    return sum(mult) + max_addres
 
+# read line as an integer
+k = int(input())
 
-for case_num in [0]:  # no loop over test case
-# for case_num in range(100):  # if the number of test cases is specified
-# for case_num in range(int(input())):
+# read line as a string
+# srr = input().strip()
 
+# read one line and parse each word as a string
+# lst = input().split()
 
-    # read line as an integer
-    k = int(input())
+# read one line and parse each word as an integer
+# a,b,c = list(map(int,input().split()))
+arr = list(map(int,input().split()))
+brr = list(map(int,input().split()))
 
-    # read line as a string
-    # srr = input().strip()
+# read multiple rows
+# mrr = read_matrix(k)  # and return as a list of list of int
+# arr = read_strings(k)  # and return as a list of str
 
-    # read one line and parse each word as a string
-    # lst = input().split()
+mult = [a*b for a,b in zip(arr,brr)]
+# consider all possible centres
+# log(sum(mult))
+
+# odd centres
+max_addres = 0
+for c in range(k):
+    curres = 0
+    left = c
+    righ = c
+    while True:
+        left -= 1
+        righ += 1
+        if left < 0 or righ >= k:
+            break
+        old = mult[left] + mult[righ]
+        new = arr[righ]*brr[left] + arr[left]*brr[righ]
+        # log(left, righ)
+        curres += new - old
+        max_addres = max(max_addres, curres)
     
-    # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
-    arr = list(map(int,input().split()))
-    brr = list(map(int,input().split()))
+# log("x")
+for c in range(k):
+    curres = 0
+    left = c+1
+    righ = c
+    while True:
+        left -= 1
+        righ += 1
+        if left < 0 or righ >= k:
+            break
+        old = mult[left] + mult[righ]
+        new = arr[righ]*brr[left] + arr[left]*brr[righ]
+        # log(left, righ)
+        curres += new - old
+        max_addres = max(max_addres, curres)
 
-    # read multiple rows
-    # mrr = read_matrix(k)  # and return as a list of list of int
-    # arr = read_strings(k)  # and return as a list of str
-
-    res = solve(arr, brr, k)  # include input here
-    
-    # print result
-    # Google and Facebook - case number required
-    # print("Case #{}: {}".format(case_num+1, res))
-
-    # Other platforms - no case number required
-    print(res)
-    # print(len(res))
-    # print(*res)  # print a list with elements
-    # for r in res:  # print each list in a different line
-        # print(res)
-        # print(*res)
+print(sum(mult) + max_addres)
