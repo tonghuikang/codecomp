1bf6b43aa6f02e18dea4ddc465affb16bf63914e
hkmac <tonghuikang@outlook.com>
2021-05-01 00:04:10 +0800

    attempt fix TLE

--- template/d.py
+++ template/d.py
@@ -1,23 +1,11 @@
 #!/usr/bin/env python3
 import sys, getpass
-import math, random
-import functools, itertools, collections, heapq, bisect
 from collections import Counter, defaultdict, deque
 input = sys.stdin.readline  # to read input quickly
 # sys.setrecursionlimit(10**6 + 5)
 
-# available on Google, AtCoder Python3, not available on Codeforces
-# import numpy as np
-# import scipy
-
-M9 = 10**9 + 7  # 998244353
-# d4 = [(1,0),(0,1),(-1,0),(0,-1)]
-# d8 = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
-# d6 = [(2,0),(1,1),(-1,1),(-2,0),(-1,-1),(1,-1)]  # hexagonal layout
 MAXINT = sys.maxsize
 
-# if testing locally, print to terminal with a different color
-# OFFLINE_TEST = getpass.getuser() == "hkmac"
 OFFLINE_TEST = False  # codechef does not allow getpass
 def log(*args):
     if OFFLINE_TEST:
@@ -34,26 +22,22 @@ def solve(*args):
 def read_matrix(rows):
     return [list(map(int,input().split())) for _ in range(rows)]
 
-def read_strings(rows):
-    return [input().strip() for _ in range(rows)]
-
 # ---------------------------- template ends here ----------------------------
 
 
 def solve_(krr,mrr,root,n):
     # your solution here
-    root -= 1
     default = krr[0]
     specials = set([x for x in krr])
 
-    g = defaultdict(set)
+    g = [set() for _ in range(n)]
     for a,b in mrr:
         g[a].add(b)
         g[b].add(a)
 
     distance = {}  # distance from root
     distance[root] = 0
-    children = defaultdict(set)
+    children = [set() for _ in range(n)]
     parent = {}
 
     stack = [root]
@@ -67,16 +51,13 @@ def solve_(krr,mrr,root,n):
             parent[nex] = cur
             distance[nex] = distance[cur] + 1
 
-    tagged_nodes_item = {i:-1 for i in range(n)}
+    tagged_nodes_item = [-1 for _ in range(n)]
     for k in specials:
         tagged_nodes_item[k] = k
 
-    leaves = set()
-    for i in range(n):
-        if i not in children:
-            leaves.add(i)
+    leaves = set(i for i in range(n) if not children[i])
 
-    # print(leaves)
+    # print(children)
     earliest = {}
     earliest[root] = 0
 
@@ -146,6 +127,7 @@ for case_num in range(int(input())):
     # read multiple rows
     mrr = read_matrix(n-1)  # and return as a list of list of int
     mrr = [((a-1),(b-1)) for a,b in mrr]
+    a -= 1
     # arr = read_strings(k)  # and return as a list of str
 
     res, res2 = solve(krr,mrr,a,n)  # include input here
@@ -155,8 +137,7 @@ for case_num in range(int(input())):
     # print("Case #{}: {}".format(case_num+1, res))
 
     # Other platforms - no case number required
-    print(" ".join(str(x) for x in res))
-    print(" ".join(str(x) for x in res2))
+    print(" ".join(str(x) for x in res) + "\n" + " ".join(str(x) for x in res2))
     # print(len(res))
     # print(*res)  # print a list with elements
     # for r in res:  # print each list in a different line
