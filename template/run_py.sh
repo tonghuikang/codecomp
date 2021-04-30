7858ee87a45c7a45eb720425dfd04d373861adad
hkmac <tonghuikang@outlook.com>
2021-04-30 18:33:19 +0800

    pypy

--- template/run_py.sh
+++ template/run_py.sh
@@ -2,5 +2,5 @@ for TEST_CASE in $1*;
 	do
 	[[ $TEST_CASE == *.* ]] && continue; 
     echo "-------- running test case" $TEST_CASE "--------";
-	time python3 $1.py < $TEST_CASE
+	time pypy3 $1.py < $TEST_CASE
 done
