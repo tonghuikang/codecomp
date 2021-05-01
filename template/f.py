fe4ca600078d20881e8c861438415e5cfadf93d0
hkmac <tonghuikang@outlook.com>
2021-05-01 21:30:48 +0800

    f check feasible

--- template/f.py
+++ template/f.py
@@ -47,15 +47,30 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(n,arr):
     # your solution here
+    ptr = [0 for _ in range(18)]
+
+    for x in arr:
+        cur = 0
+        while x:
+            if x&1:
+                ptr[cur] += 1
+            x = x >> 1
+            cur += 1
     
+    log(ptr)
+    for x in ptr:
+        if x == n//2:
+            return -1
+
+    assert False
     return ""
 
 
-# for case_num in [0]:  # no loop over test case
+for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
-for case_num in range(int(input())):
+# for case_num in range(int(input())):
 
     # read line as an integer
     # k = int(input())
@@ -67,8 +82,8 @@ for case_num in range(int(input())):
     # lst = input().split()
     
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
-    # lst = list(map(int,input().split()))
+    n,m = list(map(int,input().split()))
+    arr = list(map(int,input().split()))
     # lst = minus_one(lst)
 
     # read multiple rows
@@ -76,7 +91,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(n,arr)  # include input here
 
     # print length if applicable
     # print(len(res))
