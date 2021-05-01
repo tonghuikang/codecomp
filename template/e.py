c108259f109a7e2dbf45e05b55a959e323dbf2fd
hkmac <tonghuikang@outlook.com>
2021-05-01 20:59:55 +0800

    e ea

--- template/e.py
+++ template/e.py
@@ -39,15 +39,82 @@ def read_strings(rows):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def dijkstra(list_of_indexes_and_costs, start):  # is it possible to do dijkstra directly?
+    # leetcode.com/problems/path-with-maximum-probability/
+    # leetcode.com/problems/network-delay-time/
+    length = len(list_of_indexes_and_costs)
+    visited = [False]*length
+    weights = [MAXINT]*length
+    path = [None]*length
+    queue = []
+    weights[start] = 0
+    heapq.heappush(queue, (0, start))
+    while queue:
+        g, u = heapq.heappop(queue)
+        visited[u] = True
+        for v, w in list_of_indexes_and_costs[u]:
+            if not visited[v]:
+                f = g + w
+                if f < weights[v]:
+                    weights[v] = f
+                    path[v] = u
+                    heapq.heappush(queue, (f, v))
+    return path, weights
+
+
+def dijkstra_with_preprocessing(map_from_node_to_nodes_and_costs, source, target, idxs=set()):
+    # leetcode.com/problems/path-with-maximum-probability/
+    # leetcode.com/problems/network-delay-time/
+    d = map_from_node_to_nodes_and_costs
+
+    if target not in d:  # destination may not have outgoing paths
+        d[target] = []
+    if source not in d:
+        return MAXINT
+    
+    # assign indexes
+    if idxs:
+        idxs = {k:i for i,k in enumerate(idxs)}
+    else:
+        idxs = {k:i for i,k in enumerate(d.keys())}
+
+    # populate list of indexes and costs
+    list_of_indexes_and_costs = [[] for _ in range(len(idxs))]
+    for e,vrr in d.items():
+        for v,cost in vrr:
+            list_of_indexes_and_costs[idxs[e]].append((idxs[v],cost))
+
+    _, costs = dijkstra(list_of_indexes_and_costs, idxs[source])
+    return costs[idxs[target]]
+
+
+def solve_(hrr, vrr, h, v):
     # your solution here
+    g = defaultdict(list)
 
-    return ""
+    for i in range(h):
+        for j in range(v-1):
+            g[i,j].append(((i,j+1), hrr[i][j]))
+            g[i,j+1].append(((i,j), hrr[i][j]))
+
+    for i in range(h-1):
+        for j in range(v):
+            g[i,j].append(((i+1,j), vrr[i][j]))
+            g[i+1,j].append(((i,j), vrr[i][j]))
+            g[i+1,j,1].append(((i,j,1), 1))
+
+    for i in range(h):
+        for j in range(v):
+            g[i,j].append(((i,j,1), 0.5))
+            g[i,j,1].append(((i,j), 0.5))
 
 
-# for case_num in [0]:  # no loop over test case
+    return dijkstra_with_preprocessing(g,(0,0),(h-1,v-1))
+
+
+for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
-for case_num in range(int(input())):
+# for case_num in range(int(input())):
 
     # read line as an integer
     # k = int(input())
@@ -59,14 +126,16 @@ for case_num in range(int(input())):
     # lst = input().split()
     
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
+    n,m = list(map(int,input().split()))
     # lst = list(map(int,input().split()))
 
     # read multiple rows
-    # mrr = read_matrix(k)  # and return as a list of list of int
+    hrr = read_matrix(n)  # and return as a list of list of int
+    vrr = read_matrix(n-1)  # and return as a list of list of int
     # arr = read_strings(k)  # and return as a list of str
 
-    res = solve()  # include input here
+    res = solve(hrr,vrr,n,m)  # include input here
+    res = int(res)
     
     # print result
     # Google and Facebook - case number required
