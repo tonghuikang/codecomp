fba7756e47eab2ec0017ab86696d040fb7c2bbe9
hkmac <tonghuikang@outlook.com>
2021-05-09 01:16:02 +0800

    double divmod

--- template/a.py
+++ template/a.py
@@ -48,10 +48,11 @@ class ThreeDChessRooks:
 #         print("single_match res", res)
         
         double_match = Counter()
+        d = C+1
         for x,y,z in pool:
-            double_match[x,y,-1] += 1
-            double_match[-1,y,z] += 1
-            double_match[x,-1,z] += 1
+            double_match[x *d*d + y*d + C] += 1
+            double_match[C *d*d + y*d + z] += 1
+            double_match[x *d*d + C*d + z] += 1
         
         for k,v in double_match.items():
             res -= v*(v-1)
@@ -61,21 +62,23 @@ class ThreeDChessRooks:
         c = Counter(pool)
     
         for k,v in double_match.items():
-            x,y,z = k
+            x,f = divmod(k,d*d)
+            y,z = divmod(f,d)
+#             x,y,z = k
             
-            if x == -1:
+            if x == C:
                 if (0,y,z) in c and (1,y,z) in c:
                     res -= c[0,y,z]*c[1,y,z]
                 if (C-1,y,z) in c and (C-2,y,z) in c:
                     res -= c[C-1,y,z]*c[C-2,y,z]
                     
-            if y == -1:
+            if y == C:
                 if (x,0,z) in c and (x,1,z) in c:
                     res -= c[x,0,z]*c[x,1,z]
                 if (x,C-1,z) in c and (x,C-2,z) in c:
                     res -= c[x,C-1,z]*c[x,C-2,z]
                     
-            if z == -1:
+            if z == C:
                 if (x,y,0) in c and (x,y,1) in c:
                     res -= c[x,y,0]*c[x,y,1]
                 if (x,y,C-1) in c and (x,y,C-2) in c:
