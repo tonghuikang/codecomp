052e7f86520eca89017e403ce2e0aed6e9f38249
hkmac <tonghuikang@outlook.com>
2021-05-09 01:55:01 +0800

    srm-805

--- template/run_cpp.sh
+++
@@ -1,9 +0,0 @@
-echo "-------- compiling --------"
-g++ -std=c++17 -o tmp.out -Wall -Wno-unknown-pragmas $1.cpp
-for TEST_CASE in $1*;
-	do
-	[[ $TEST_CASE == *.* ]] && continue; 
-    echo "-------- running test case" $TEST_CASE "--------"
-	time ./tmp.out < $TEST_CASE
-done
-rm tmp.out
--- template/run_py.sh
+++
@@ -1,6 +0,0 @@
-for TEST_CASE in $1*;
-	do
-	[[ $TEST_CASE == *.* ]] && continue; 
-    echo "-------- running test case" $TEST_CASE "--------";
-	time pypy3 $1.py < $TEST_CASE
-done
