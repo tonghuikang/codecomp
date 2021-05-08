80ae5ba8d147331936a933be03e644fb6f3d3225
hkmac <tonghuikang@outlook.com>
2021-05-09 05:54:41 +0800

    sample_gen_cf.py
    update run_py.sh

--- template/run_py.sh
+++ template/run_py.sh
@@ -1,6 +1,21 @@
+# python script $1.py
+# input $1*
+# output $1*.out (optional)
+# files $1* with other extensions are ignored
+
+[[ ! -f $1.py ]] && echo "$1.py not found" && exit 1; 
+
 for TEST_CASE in $1*;
 	do
 	[[ $TEST_CASE == *.* ]] && continue; 
     echo "-------- running test case" $TEST_CASE "--------";
 	time pypy3 $1.py < $TEST_CASE
+
+	# compare with $TEST_CASE.out if exist
+	[[ ! -f $TEST_CASE.out ]] && echo "No output to compare" && continue;
+
+	pypy3 $1.py < $TEST_CASE > tmp.out 2>/dev/null
+	diff $TEST_CASE.out tmp.out || echo "DIFFERENCE FOUND IN $TEST_CASE"
+	rm tmp.out
+
 done
