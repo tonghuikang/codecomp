5089afa19074a84dbdf11e48856f235d4d56b8b3
hkmac <tonghuikang@outlook.com>
2021-05-09 00:53:08 +0800

    check

--- template/a.py
+++ template/a.py
@@ -1,24 +1,64 @@
+from collections import Counter
+
 class ThreeDChessRooks:
     def count(self, C, R, XP, YP, ZP, seed):
         
-        X = [0 for _ in range(R)]
-        Y = [0 for _ in range(R)]
-        Z = [0 for _ in range(R)]
+        pool = set()
         
-        for i in range(len(XP)):
-            X[i] = XP[i]
-            Y[i] = YP[i]
-            Z[i] = ZP[i]
+        for x,y,z in zip(XP, YP, ZP):
+            pool.add((x,y,z))
 
         state = seed
         for i in range(len(XP), R):
             state = (state * 1103515245 + 12345) % 2147483648
-            X[i] = state%C
+            x = state%C
             state = (state * 1103515245 + 12345) % 2147483648
-            Y[i] = state%C
+            y = state%C
             state = (state * 1103515245 + 12345) % 2147483648
-            Z[i] = state%C
+            z = state%C
+            pool.add((x,y,z))
             
-        print(X)
-        print(Y)
-        print(Z)
+#         print(pool)
+        res = 0
+
+        single_match = Counter()
+        
+        for x,y,z in pool:
+            single_match[x,-1,-1] += 1
+            single_match[-1,y,-1] += 1
+            single_match[-1,-1,z] += 1
+        
+        for k,v in single_match.items():
+            res += v*(v-1)
+        
+        double_match = Counter()
+        for x,y,z in pool:
+            double_match[x,y,-1] += 1
+            double_match[-1,y,z] += 1
+            double_match[x,-1,z] += 1
+        
+        for k,v in double_match.items():
+            res -= v*(v-1)
+
+        for k,v in double_match.items():
+            x,y,z = k
+            
+            if x == -1:
+                if (0,y,z) in pool and (1,y,z) in pool:
+                    res -= 1
+                if (C-1,y,z) in pool and (C-2,y,z) in pool:
+                    res -= 1
+                    
+            if y == -1:
+                if (x,0,z) in pool and (x,1,z) in pool:
+                    res -= 1
+                if (x,C-1,z) in pool and (x,C-2,z) in pool:
+                    res -= 1
+                    
+            if z == -1:
+                if (x,y,0) in pool and (x,y,1) in pool:
+                    res -= 1
+                if (x,y,C-1) in pool and (x,y,C-2) in pool:
+                    res -= 1
+        
+        return res        
