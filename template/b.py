5b2883188f8e42c5b1f1d6ead42b31998c12456c
hkmac <tonghuikang@outlook.com>
2021-05-22 20:03:07 +0800

    cases

--- template/b.py
+++ template/b.py
@@ -47,21 +47,23 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(srr):
     # your solution here
     
-    return ""
+    mapp = {"0":"0", "1":"1", "6":"9", "8":"8", "9":"6"}
+
+    return "".join(mapp[x] for x in srr)[::-1]
 
 
-# for case_num in [0]:  # no loop over test case
+for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
-for case_num in range(int(input())):
+# for case_num in range(int(input())):
 
     # read line as an integer
     # k = int(input())
 
     # read line as a string
-    # srr = input().strip()
+    srr = input().strip()
 
     # read one line and parse each word as a string
     # lst = input().split()
@@ -76,7 +78,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(srr)  # include input here
 
     # print length if applicable
     # print(len(res))
