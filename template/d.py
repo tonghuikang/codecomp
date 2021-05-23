37556e9041a319623e855c261425d9d7bc9f87e9
hkmac <tonghuikang@outlook.com>
2021-05-23 21:32:44 +0800

    hash

--- template/d.py
+++ template/d.py
@@ -47,10 +47,62 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
+
+# https://medium.com/@CalvinChankf/solving-basic-calculator-i-ii-iii-on-leetcode-74d926732437
+def calculate(s):
+        """
+        Time    O(n^2) because we have to find the correspondign closing parenthesis for recursion
+        Space   O(n)
+        204 ms, faster than 7.41%
+        """
+        if len(s) == 0:
+            return 0
+        stack = []
+        sign = '+'
+        num = 0
+        i = 0
+        while i < len(s):
+            c = s[i]
+            if c.isdigit():
+                num = num*10+int(c)
+
+            if c == '(':
+                # find the corresponding ")"
+                pCnt = 0
+                end = 0
+                clone = s[i:]
+                while end < len(clone):
+                    if clone[end] == '(':
+                        pCnt += 1
+                    elif clone[end] == ')':
+                        pCnt -= 1
+                        if pCnt == 0:
+                            break
+                    end += 1
+                # do recursion to calculate the sum within the next (...)
+                num = calculate(s[i+1:i+end])
+                i += end
+
+            if i + 1 == len(s) or (c == '+' or c == '-' or c == '*' or c == '#'):
+                if sign == '+':
+                    stack.append(num)
+                # elif sign == '-':
+                #     stack.append(-num)
+                elif sign == '*':
+                    stack[-1] = stack[-1]*num
+                elif sign == '#':
+                    stack[-1] = hash((stack[-1],num))%65537
+                    # stack[-1] = stack[-1]num
+                sign = c
+                num = 0
+            i += 1
+
+        return sum(stack)
+
 def solve_(arr):
     # your solution here
     
-    operators = ["*", "+", "-", "^", "&"]
+    operators = ["*", "+", "-", "^", "&", "+1-", "*2-", "*0.37-0.23*"]
 
     groups = [list(range(len(arr)))]
     # log(groups)
@@ -60,12 +112,13 @@ def solve_(arr):
         for group in groups:
             new_groups_div = defaultdict(list)
             for idx in group:
-                eva = arr[idx].replace("#", op)
-                val = eval(eva)
+                eva = arr[idx].replace("#", "#")
+                val = calculate(eva)
                 new_groups_div[val].append(idx)
             new_groups.extend(list(new_groups_div.values()))
             # log(new_groups)
         groups = new_groups
+        break
 
     groups.sort()
     res = [-1 for _ in range(len(arr))]
@@ -75,6 +128,11 @@ def solve_(arr):
 
     return res
 
+
+# def solve_proper(arr):
+
+#     stack = []
+#     for 
 
 # for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
