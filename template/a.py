2e6d071e14c00fe6b9675b836732cabfb1dcd8d9
hkmac <tonghuikang@outlook.com>
2021-05-24 23:20:12 +0800

    fastio maybe

--- template/a.py
+++ template/a.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 import sys
 import getpass  # not available on codechef
-import math, random
-import functools, itertools, collections, heapq, bisect
-from collections import Counter, defaultdict, deque
-input = sys.stdin.readline  # to read input quickly
+# import math, random
+# import functools, itertools, collections, heapq, bisect
+from collections import defaultdict
+# input = sys.stdin.readline  # to read input quickly
+import os
+import sys
+from io import BytesIO, IOBase
 
 # available on Google, AtCoder Python3, not available on Codeforces
 # import numpy as np
 # import scipy
+BUFSIZE = 8192
+
 
 M9 = 10**9 + 7  # 998244353
 yes, no = "YES", "NO"
@@ -18,8 +23,8 @@ yes, no = "YES", "NO"
 MAXINT = sys.maxsize
 
 # if testing locally, print to terminal with a different color
-OFFLINE_TEST = getpass.getuser() == "hkmac"
-# OFFLINE_TEST = False  # codechef does not allow getpass
+# OFFLINE_TEST = getpass.getuser() == "hkmac"
+OFFLINE_TEST = False  # codechef does not allow getpass
 def log(*args):
     if OFFLINE_TEST:
         print('\033[36m', *args, '\033[0m', file=sys.stderr)
@@ -48,6 +53,53 @@ def minus_one_matrix(mrr):
 
 
 
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
 
 
 # minmax
@@ -57,8 +109,8 @@ def solve_(minrr, maxrr, edges):
 
     g = defaultdict(list)
     for a,b in edges:
-        g[a].append(b)
-        g[b].append(a)
+        g[a-1].append(b-1)
+        g[b-1].append(a-1)
     
     # maxres = 0
 
@@ -83,10 +135,11 @@ def solve_(minrr, maxrr, edges):
                 
     #     maxres = max(maxres, curres)
 
+    # return maxres
+
     entered = set([0])
     exiting = set()
-    prev = {}
-    prev[0] = -1
+    prev = [-1]*len(arr)
     resmincur = [0]*len(arr)
     resmaxcur = [0]*len(arr)
 
@@ -142,7 +195,7 @@ for case_num in range(int(input())):
     arr, brr = zip(*mrr)
     del mrr
     edges = read_matrix(k-1)  # and return as a list of list of int
-    edges = minus_one_matrix(edges)
+    # edges = minus_one_matrix(edges)
 
     res = solve(arr, brr, edges)  # include input here
 
