881cab8a0de8055ecef170969d43893e3848265e
hkmac <tonghuikang@outlook.com>
2021-05-29 20:52:31 +0800

    ckpt

--- template/c.py
+++ template/c.py
@@ -47,10 +47,82 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(lst, k):
+    if lst == sorted(lst):
+        return []
+
     # your solution here
-    
-    return ""
+    lst = [x-1 for x in lst]
+    locs = {i:x for i,x in enumerate(lst)}
+    log(locs)
+
+    required = set([0, k-1])
+    cureven = True
+    res = []
+    settled = set([-1, k])
+
+    while required:
+        log(lst, res)
+
+        for val in required:
+
+            if locs[val] == val:
+                required.remove(val)
+                settled.add(val)
+                if val-1 not in settled:
+                    required.add(val)
+                if val+1 not in settled:
+                    required.add(val)
+                break
+
+            if cureven:
+                if locs[val] > val:  # move left
+                    pos = locs[val]-1
+                    if pos%2 == 0:  # needs to be even to move
+                        log("move left, even", pos, val)
+                        for i in range(pos+1, val, -1):
+                            lst[i], lst[i-1] = lst[i-1], lst[i]
+                            cureven = not cureven
+                            res.append(i)
+                        
+                        required.remove(val)
+                        settled.add(val)
+                        locs = {i:x for i,x in enumerate(lst)}
+                        if val-1 not in settled:
+                            required.add(val)
+                        if val+1 not in settled:
+                            required.add(val)
+                        break
+                    else:
+                        pass
+                
+                else:  # move right
+                    pos = locs[val]
+                    if pos%2 == 0:  # needs to be even to move
+                        # move
+                        for i in range(pos, val):
+                            lst[i], lst[i+1] = lst[i+1], lst[i]
+                            cureven = not cureven
+                            res.append(i)
+
+                        locs = {i:x for i,x in enumerate(lst)}
+                        required.remove(val)
+                        settled.add(val)
+                        if val-1 not in settled:
+                            required.add(val)
+                        if val+1 not in settled:
+                            required.add(val)
+                        break
+                    
+                    else:
+                        pass
+        
+                        
+
+        else:
+            break
+
+    return res
 
 
 # for case_num in [0]:  # no loop over test case
@@ -58,7 +130,7 @@ def solve_():
 for case_num in range(int(input())):
 
     # read line as an integer
-    # k = int(input())
+    k = int(input())
 
     # read line as a string
     # srr = input().strip()
@@ -68,7 +140,7 @@ for case_num in range(int(input())):
     
     # read one line and parse each word as an integer
     # a,b,c = list(map(int,input().split()))
-    # lst = list(map(int,input().split()))
+    lst = list(map(int,input().split()))
     # lst = minus_one(lst)
 
     # read multiple rows
@@ -76,13 +148,13 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(lst, k)  # include input here
 
     # print length if applicable
-    # print(len(res))
+    print(len(res))
 
     # parse result
-    # res = " ".join(str(x) for x in res)
+    res = " ".join(str(x) for x in res)
     # res = "\n".join(str(x) for x in res)
     # res = "\n".join(" ".join(str(x) for x in row) for row in res)
 
