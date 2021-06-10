f5ef0cbe0ad412b81a3b1e301443ae2aae68b315
hkmac <tonghuikang@outlook.com>
2021-06-10 23:21:57 +0800

    fix a case

--- template/d.py
+++ template/d.py
@@ -46,11 +46,120 @@ def minus_one_matrix(mrr):
 
 # ---------------------------- template ends here ----------------------------
 
+def get_largest_prime_factors(num):
+    # get largest prime factor for each number
+    # you can use this to obtain primes
+    largest_prime_factors = [1] * num
+    for i in range(2, num):
+        if largest_prime_factors[i] > 1:  # not prime
+            continue
+        for j in range(i, num, i):
+            largest_prime_factors[j] = i
+    return largest_prime_factors
 
-def solve_():
-    # your solution here
+LARGE = int(10**5)
+
+largest_prime_factors = get_largest_prime_factors(int(10**5))
+primes = [x for i,x in enumerate(largest_prime_factors[2:], start=2) if x == i]
+
+
+def is_prime(n):
+    # primality test (not tested)
+    # https://github.com/not522/ac-library-python/blob/master/atcoder/_math.py
+    # http://ceur-ws.org/Vol-1326/020-Forisek.pdf
+    # untested
+    if n <= 1:
+        return False
+    if n > 2**32:
+        primitives = set([2, 325, 9375, 28178, 450775, 9780504, 1795265022])
+        if n == 2:
+            return True
+    else:
+        primitives = set([2, 7, 61])
+        if n in primitives:
+            return True
     
-    return ""
+    if n % 2 == 0:
+        return False
+
+    d = n - 1
+    while d % 2 == 0:
+        d //= 2
+
+    for a in primitives:
+        t = d
+        y = pow(a, t, n)
+        while t != n - 1 and y != 1 and y != n - 1:
+            y = y * y % n
+            t <<= 1
+        if y != n - 1 and t % 2 == 0:
+            return False
+    return True
+
+
+def get_prime_factors_with_precomp_largest_factors(num, largest_prime_factors=largest_prime_factors):
+    factors = []
+
+    if num >= int(10**4.6):
+        if is_prime(num):
+            return [num]
+        # brute force first 1000
+        for p in primes:
+            while num%p == 0:
+                factors.append(p)
+                num = num//p
+                # print(num, p)
+            if num <= LARGE:
+                break
+        else:
+            if is_prime(num):
+                return factors + [num]
+
+    # factorise into prime factors given precomputed largest_prime_factors
+    lf = largest_prime_factors[num]
+    while lf != num:
+        factors.append(lf)
+        num //= lf
+        lf = largest_prime_factors[num]
+    if num > 1:
+        factors.append(num)
+    return factors
+
+
+def solve_(a,b,k):
+    a,b = sorted([a,b])
+    
+    if a == b == 1:
+        return no
+
+    if a == 1:
+        f = len(get_prime_factors_with_precomp_largest_factors(b))
+        if 1 <= k <= f:
+            return yes
+        return no
+
+    if a == b:
+        f = len(get_prime_factors_with_precomp_largest_factors(b))
+        if 2 <= k <= 2*f:
+            return yes
+        return no
+        
+    gcd = math.gcd(a,b)    
+    p = a//gcd
+    q = b//gcd
+
+    x = len(get_prime_factors_with_precomp_largest_factors(gcd))
+    y = len(get_prime_factors_with_precomp_largest_factors(p))
+    z = len(get_prime_factors_with_precomp_largest_factors(q))
+
+    if k == 2:
+        if p == 1:
+            if z == 1:
+                return no
+
+    if 1 <= k <= 2*x + y + z:
+        return yes
+    return no
 
 
 # for case_num in [0]:  # no loop over test case
@@ -67,7 +176,7 @@ for case_num in range(int(input())):
     # lst = input().split()
     
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
+    a,b,k = list(map(int,input().split()))
     # lst = list(map(int,input().split()))
     # lst = minus_one(lst)
 
@@ -76,7 +185,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(a,b,k)  # include input here
 
     # print length if applicable
     # print(len(res))
