2965d68c2fb34f019057dcc57cebafb8ebdcfce9
hkmac <tonghuikang@outlook.com>
2021-06-10 23:41:02 +0800

    accepted for fuck

--- template/d.py
+++ template/d.py
@@ -63,68 +63,76 @@ largest_prime_factors = get_largest_prime_factors(int(10**5))
 primes = [x for i,x in enumerate(largest_prime_factors[2:], start=2) if x == i]
 
 
-def is_prime(n):
-    # primality test (not tested)
-    # https://github.com/not522/ac-library-python/blob/master/atcoder/_math.py
-    # http://ceur-ws.org/Vol-1326/020-Forisek.pdf
-    # untested
-    if n <= 1:
-        return False
-    if n > 2**32:
-        primitives = set([2, 325, 9375, 28178, 450775, 9780504, 1795265022])
-        if n == 2:
-            return True
-    else:
-        primitives = set([2, 7, 61])
-        if n in primitives:
-            return True
+# def is_prime(n):
+#     # primality test (not tested)
+#     # https://github.com/not522/ac-library-python/blob/master/atcoder/_math.py
+#     # http://ceur-ws.org/Vol-1326/020-Forisek.pdf
+#     # untested
+#     if n <= 1:
+#         return False
+#     if n > 2**32:
+#         primitives = set([2, 325, 9375, 28178, 450775, 9780504, 1795265022])
+#         if n == 2:
+#             return True
+#     else:
+#         primitives = set([2, 7, 61])
+#         if n in primitives:
+#             return True
     
-    if n % 2 == 0:
-        return False
+#     if n % 2 == 0:
+#         return False
 
-    d = n - 1
-    while d % 2 == 0:
-        d //= 2
+#     d = n - 1
+#     while d % 2 == 0:
+#         d //= 2
 
-    for a in primitives:
-        t = d
-        y = pow(a, t, n)
-        while t != n - 1 and y != 1 and y != n - 1:
-            y = y * y % n
-            t <<= 1
-        if y != n - 1 and t % 2 == 0:
-            return False
-    return True
+#     for a in primitives:
+#         t = d
+#         y = pow(a, t, n)
+#         while t != n - 1 and y != 1 and y != n - 1:
+#             y = y * y % n
+#             t <<= 1
+#         if y != n - 1 and t % 2 == 0:
+#             return False
+#     return True
+# log(primes[:10])
 
-
-def get_prime_factors_with_precomp_largest_factors(num, largest_prime_factors=largest_prime_factors):
+def get_prime_factors_with_precomp_largest_factors(num):
+    if num == 1:
+        return []
     factors = []
 
-    if num >= int(10**4.6):
-        if is_prime(num):
-            return [num]
-        # brute force first 1000
-        for p in primes:
-            while num%p == 0:
-                factors.append(p)
-                num = num//p
-                # print(num, p)
-            if num <= LARGE:
-                break
-        else:
-            if is_prime(num):
-                return factors + [num]
-
-    # factorise into prime factors given precomputed largest_prime_factors
-    lf = largest_prime_factors[num]
-    while lf != num:
-        factors.append(lf)
-        num //= lf
-        lf = largest_prime_factors[num]
+    # if num >= int(10**4.6):
+    #     if is_prime(num):
+    #         return [num]
+    #     # brute force first 1000
+    for p in primes:
+        while num%p == 0:
+            factors.append(p)
+            num = num//p
+            # print(num, p)
     if num > 1:
-        factors.append(num)
+        return factors + [num]
     return factors
 
+    # # factorise into prime factors given precomputed largest_prime_factors
+    # lf = largest_prime_factors[num]
+    # while lf != num:
+    #     factors.append(lf)
+    #     num //= lf
+    #     lf = largest_prime_factors[num]
+    # if num > 1:
+    #     factors.append(num)
+    # return factors
+
+# log("check")
+# log(primes[-19:])
+
+# for i in range(1,100):
+#     log(get_prime_factors_with_precomp_largest_factors(i))
+
+# for i in range(10**9-100,10**9):
+#     log(get_prime_factors_with_precomp_largest_factors(i))
 
 def solve_(a,b,k):
     a,b = sorted([a,b])
@@ -152,10 +160,16 @@ def solve_(a,b,k):
     y = len(get_prime_factors_with_precomp_largest_factors(p))
     z = len(get_prime_factors_with_precomp_largest_factors(q))
 
-    if k == 2:
-        if p == 1:
-            if z == 1:
-                return no
+    # log(get_prime_factors_with_precomp_largest_factors(gcd))
+    # log(x,y,z,p,q)
+
+    if k == 1:
+        if p != 1:
+            return no
+    # if k == 2:
+    #     if p == 1:
+    #         if z == 1:
+    #             return no
 
     if 1 <= k <= 2*x + y + z:
         return yes
