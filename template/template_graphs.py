1d7dd895df8858886f7bac16550c68d6b97e468b
hkmac <tonghuikang@outlook.com>
2021-06-14 03:39:47 +0800

    add scc

--- template/template_graphs.py
+++ template/template_graphs.py
@@ -61,6 +61,33 @@ def count_connected_components_undirected(map_from_node_to_nodes, total_elements
     return components + total_elements - len(map_from_node_to_nodes)
 
 
+def find_strongly_connected_components(graph):
+    # https://github.com/cheran-senthil/PyRival/blob/master/pyrival/graphs/scc.py
+    # input - list of node to nodes?
+    SCC, S, P = [], [], []
+    depth = [0] * len(graph)
+ 
+    stack = list(range(len(graph)))
+    while stack:
+        node = stack.pop()
+        if node < 0:
+            d = depth[~node] - 1
+            if P[-1] > d:
+                SCC.append(S[d:])
+                del S[d:], P[-1]
+                for node in SCC[-1]:
+                    depth[node] = -1
+        elif depth[node] > 0:
+            while P[-1] > depth[node]:
+                P.pop()
+        elif depth[node] == 0:
+            S.append(node)
+            P.append(len(S))
+            depth[node] = len(S)
+            stack.append(~node)
+            stack += graph[node]
+    return SCC[::-1]
+
 # ------------------------ shortest path ------------------------
 
 
