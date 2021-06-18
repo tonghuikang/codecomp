612a00b6af0bfeb5198dd8f3853604ffa8fa99ec
hkmac <tonghuikang@outlook.com>
2021-06-18 23:51:19 +0800

    test e1

--- template/e1.py
+++ template/e1.py
@@ -47,27 +47,35 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(srr, k):
     # your solution here
     
-    return ""
+    abc = {x:i for i,x in enumerate("abcdefghijklmnopqrstuvwxyz")}
+
+    for i in range(1,len(srr)):
+        if srr[:i]+srr[:i] < srr[i:]:
+            break
+    else:
+        i = len(srr)
+    
+    return (srr[:i]*(k//i + 1))[:k]
 
 
-# for case_num in [0]:  # no loop over test case
+for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
-for case_num in range(int(input())):
+# for case_num in range(int(input())):
 
     # read line as an integer
     # k = int(input())
 
+    n,k = list(map(int,input().split()))
     # read line as a string
-    # srr = input().strip()
+    srr = input().strip()
 
     # read one line and parse each word as a string
     # lst = input().split()
     
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
     # lst = list(map(int,input().split()))
     # lst = minus_one(lst)
 
@@ -76,7 +84,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(srr, k)  # include input here
 
     # print length if applicable
     # print(len(res))
