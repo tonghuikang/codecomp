652934d379573c58e8492fa7dc660babf178859d
hkmac <tonghuikang@outlook.com>
2021-06-19 01:40:34 +0800

    add z algorithm

--- template/template_arrays.py
+++ template/template_arrays.py
@@ -131,3 +131,25 @@ def gathering_cost(xpos):
         right_cost -= (n-i-1)*(nex - prev)
         cost_arr.append(left_cost + right_cost)
     return cost_arr
+
+
+def z_function(S):
+    # https://github.com/cheran-senthil/PyRival/blob/master/pyrival/strings/z_algorithm.py
+    # https://cp-algorithms.com/string/z-function.html
+    n = len(S)
+    Z = [0] * n
+    l = r = 0
+
+    for i in range(1, n):
+        z = Z[i - l]
+        if i + z >= r:
+            z = max(r - i, 0)
+            while i + z < n and S[z] == S[i + z]:
+                z += 1
+
+            l, r = i, i + z
+
+        Z[i] = z
+
+    Z[0] = n
+    return Z
