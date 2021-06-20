249745997a3e422033557c145fe3b8b035a8c54a
hkmac <tonghuikang@outlook.com>
2021-06-20 18:14:35 +0800

    cases

--- template/a.py
+++ template/a.py
@@ -47,10 +47,22 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(a,b,c):
     # your solution here
     
-    return ""
+    size = c//b
+
+    x = (a - size)*size
+    y = (size*(size-1))//2
+
+    if size > a:
+        log("cur")
+        return a*(a-1)//2
+
+    log(size, x,y)
+
+    return x+y
+
 
 
 # for case_num in [0]:  # no loop over test case
@@ -67,7 +79,7 @@ for case_num in range(int(input())):
     # lst = input().split()
     
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
+    a,b,c = list(map(int,input().split()))
     # lst = list(map(int,input().split()))
     # lst = minus_one(lst)
 
@@ -76,7 +88,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(a,b,c)  # include input here
 
     # print length if applicable
     # print(len(res))
