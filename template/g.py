b100b1077d81ac656f4cb1a0dc11e630f7ac3bb2
hkmac <tonghuikang@outlook.com>
2021-07-11 00:39:10 +0800

    TLe

--- template/g.py
+++ template/g.py
@@ -1,14 +1,69 @@
 #!/usr/bin/env python3
-import sys
+# import sys
 import getpass  # not available on codechef
-import math, random
-import functools, itertools, collections, heapq, bisect
 from collections import Counter, defaultdict, deque
-input = sys.stdin.readline  # to read input quickly
+# input = sys.stdin.readline  # to read input quickly
 
 # available on Google, AtCoder Python3, not available on Codeforces
 # import numpy as np
 # import scipy
+
+# ------------------------------warmup----------------------------
+import os
+import sys
+from io import BytesIO, IOBase
+
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+
 
 M9 = 10**9 + 7  # 998244353
 yes, no = "YES", "NO"
@@ -69,7 +124,6 @@ def bootstrap(f, stack=[]):
 
 
 
-
 def solve_(n, edges):
     # your solution here
 
@@ -94,6 +148,7 @@ def solve_(n, edges):
                 continue
             if nex in visitable:
                 duals.add(nex)
+                continue
             dfs(nex)
         parents.remove(cur)
 
@@ -118,9 +173,9 @@ def solve_(n, edges):
             cycles.add(nex)
             stack.append(nex)
 
-    log(visitable)
-    log(cycles)
-    log(duals)
+    # log(visitable)
+    # log(cycles)
+    # log(duals)
 
     res = [0]*n
     for i in range(n):
@@ -137,6 +192,7 @@ def solve_(n, edges):
 
 
 
+res_lst = []
 # for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
 for case_num in range(int(input())):
@@ -169,8 +225,8 @@ for case_num in range(int(input())):
     res = " ".join(str(x) for x in res)
     # res = "\n".join(str(x) for x in res)
     # res = "\n".join(" ".join(str(x) for x in row) for row in res)
-
+    res_lst.append(res)
     # print result
     # print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
 
-    print(res)
+print("\n".join(res_lst))
