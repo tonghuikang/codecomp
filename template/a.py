762db4608d7dbd7a8a47bab230190db5a2bef688
hkmac <tonghuikang@outlook.com>
2021-07-11 13:08:17 +0800

    submit a

--- template/a.py
+++ template/a.py
@@ -47,10 +47,24 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(a,b,c,d,f,g,h,i):
     # your solution here
+
+    p = (a + i)
+    q = (b + h)
+    r = (c + g)
+    s = (d + f)
     
-    return ""
+    cntr = Counter([x//2 for x in [p,q,r,s] if x%2 == 0])
+
+    res = max(cntr.values())
+
+    res += b == (a + c)/2
+    res += f == (c + i)/2
+    res += h == (g + i)/2
+    res += d == (a + g)/2
+
+    return res
 
 
 # for case_num in [0]:  # no loop over test case
@@ -67,7 +81,9 @@ for case_num in range(int(input())):
     # lst = input().split()
     
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
+    a,b,c = list(map(int,input().split()))
+    d,f = list(map(int,input().split()))
+    g,h,i = list(map(int,input().split()))
     # lst = list(map(int,input().split()))
     # lst = minus_one(lst)
 
@@ -76,7 +92,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(a,b,c,d,f,g,h,i)  # include input here
 
     # print length if applicable
     # print(len(res))
@@ -87,6 +103,6 @@ for case_num in range(int(input())):
     # res = "\n".join(" ".join(str(x) for x in row) for row in res)
 
     # print result
-    # print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
+    print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
 
-    print(res)
+    # print(res)
