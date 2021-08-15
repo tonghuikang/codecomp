c2c3418d2c2619a9a611680c13290a14899ee84d
hkmac <tonghuikang@outlook.com>
2021-08-15 23:04:26 +0800

    cp

---
+++ template/d2.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python3
+import sys
+import getpass  # not available on codechef
+import math, random
+import functools, itertools, collections, heapq, bisect
+from collections import Counter, defaultdict, deque
+input = sys.stdin.readline  # to read input quickly
+
+# available on Google, AtCoder Python3, not available on Codeforces
+# import numpy as np
+# import scipy
+
+M9 = 10**9 + 7  # 998244353
+yes, no = "YES", "NO"
+# d4 = [(1,0),(0,1),(-1,0),(0,-1)]
+# d8 = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
+# d6 = [(2,0),(1,1),(-1,1),(-2,0),(-1,-1),(1,-1)]  # hexagonal layout
+MAXINT = sys.maxsize
+
+# if testing locally, print to terminal with a different color
+OFFLINE_TEST = getpass.getuser() == "hkmac"
+# OFFLINE_TEST = False  # codechef does not allow getpass
+def log(*args):
+    if OFFLINE_TEST:
+        print('\033[36m', *args, '\033[0m', file=sys.stderr)
+
+def solve(*args):
+    # screen input
+    if OFFLINE_TEST:
+        log("----- solving ------")
+        log(*args)
+        log("----- ------- ------")
+    return solve_(*args)
+
+def read_matrix(rows):
+    return [list(map(int,input().split())) for _ in range(rows)]
+
+def read_strings(rows):
+    return [input().strip() for _ in range(rows)]
+
+def minus_one(arr):
+    return [x-1 for x in arr]
+
+def minus_one_matrix(mrr):
+    return [[x-1 for x in row] for row in mrr]
+
+# ---------------------------- template ends here ----------------------------
+
+class DisjointSet:
+    # github.com/not522/ac-library-python/blob/master/atcoder/dsu.py
+
+    def __init__(self, n: int = 0) -> None:
+        if n > 0:  # constant size DSU
+            self.parent_or_size = [-1]*n
+        else:
+            self.parent_or_size = defaultdict(lambda: -1)
+
+    def union(self, a: int, b: int) -> int:
+        x = self.find(a)
+        y = self.find(b)
+
+        if x == y:
+            return x
+
+        if -self.parent_or_size[x] < -self.parent_or_size[y]:
+            x, y = y, x
+
+        self.parent_or_size[x] += self.parent_or_size[y]
+        self.parent_or_size[y] = x
+
+        return x
+
+    def find(self, a: int) -> int:
+        parent = self.parent_or_size[a]
+        while parent >= 0:
+            if self.parent_or_size[parent] < 0:
+                return parent
+            self.parent_or_size[a], a, parent = (
+                self.parent_or_size[parent],
+                self.parent_or_size[parent],
+                self.parent_or_size[self.parent_or_size[parent]]
+            )
+        return a
+
+    def size(self, a: int) -> int:
+        return -self.parent_or_size[self.leader(a)]
+
+
+
+
+# if can add, just add
+def solve_(n, arr, brr):
+    # your solution here
+    
+    edges = []
+    for i in range(1,n+1):
+        for j in range(i+1,n+1):
+            edges.append((i,j))
+            
+    random.shuffle(edges)
+
+
+    t1 = DisjointSet()
+    t2 = DisjointSet()
+
+    for a,b in arr:
+        t1.union(a,b)
+
+    for a,b in brr:
+        t2.union(a,b)
+
+    res = []
+
+    for a,b in edges:
+        if t1.find(a) == t1.find(b) or t2.find(a) == t2.find(b):
+            continue
+        t1.union(a,b)
+        t2.union(a,b)
+        res.append((a,b))
+
+    return res
+
+
+for case_num in [0]:  # no loop over test case
+# for case_num in range(100):  # if the number of test cases is specified
+# for case_num in range(int(input())):
+
+    # read line as an integer
+    # k = int(input())
+
+    # read line as a string
+    # srr = input().strip()
+
+    # read one line and parse each word as a string
+    # lst = input().split()
+    
+    # read one line and parse each word as an integer
+    n,m1,m2 = list(map(int,input().split()))
+    # lst = list(map(int,input().split()))
+    # lst = minus_one(lst)
+
+    # read multiple rows
+    # arr = read_strings(k)  # and return as a list of str
+    arr = read_matrix(m1)  # and return as a list of list of int
+    brr = read_matrix(m2)  # and return as a list of list of int
+    # mrr = minus_one_matrix(mrr)
+
+    res = solve(n, arr, brr)  # include input here
+
+    # print length if applicable
+    print(len(res))
+
+    # parse result
+    # res = " ".join(str(x) for x in res)
+    # res = "\n".join(str(x) for x in res)
+    res = "\n".join(" ".join(str(x) for x in row) for row in res)
+
+    # print result
+    # print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
+
+    print(res)
