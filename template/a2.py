d993a8e59615ef7da806e3b14324a8888949c529
hkmac <tonghuikang@outlook.com>
2021-08-28 15:48:44 +0800

    a2

--- template/a2.py
+++ template/a2.py
@@ -17,6 +17,9 @@ yes, no = "YES", "NO"
 # d6 = [(2,0),(1,1),(-1,1),(-2,0),(-1,-1),(1,-1)]  # hexagonal layout
 MAXINT = sys.maxsize
 
+abc = "abcdefghijklmnopqrstuvwxyz".upper()
+abc_map = {c:i for i,c in enumerate(abc)}
+
 # if testing locally, print to terminal with a different color
 OFFLINE_TEST = getpass.getuser() == "hkmac"
 # OFFLINE_TEST = False  # codechef does not allow getpass
@@ -46,10 +49,58 @@ def minus_one_matrix(mrr):
 
 # ---------------------------- template ends here ----------------------------
 
+def dijkstra(list_of_indexes_and_costs, start):
+    # short path with nonnegative edge costs
+    # leetcode.com/problems/path-with-maximum-probability/
+    # leetcode.com/problems/network-delay-time/
+    length = len(list_of_indexes_and_costs)
+    visited = [False]*length
+    weights = [MAXINT]*length
+    path = [None]*length
+    queue = []
+    weights[start] = 0
+    heapq.heappush(queue, (0, start))
+    while queue:
+        g, u = heapq.heappop(queue)
+        visited[u] = True
+        for v, w in list_of_indexes_and_costs[u]:
+            if not visited[v]:
+                f = g + w
+                if f < weights[v]:
+                    weights[v] = f
+                    path[v] = u
+                    heapq.heappush(queue, (f, v))
+    return path, weights
 
-def solve_():
+
+def solve_(srr, qrr):
     # your solution here
-    
+    srr = [abc_map[c] for c in srr]
+    crr = [0 for _ in range(26)]
+
+    for c in srr:
+        crr[c] += 1
+
+    g = [[] for _ in range(26)]
+    for a,b in qrr:
+        a,b = abc_map[a], abc_map[b]
+        # g[a].append((b,1))
+        g[b].append((a,1))
+
+    minres = MAXINT
+    for i in range(26):
+        _, weights = dijkstra(g, i)
+        res = sum(a*b for a,b in zip(weights, crr))
+        minres = min(minres, res)
+
+    if minres > 10**8:
+        return -1
+
+    return minres
+
+
+
+
     return ""
 
 
@@ -58,25 +109,25 @@ def solve_():
 for case_num in range(int(input())):
 
     # read line as an integer
-    # k = int(input())
 
     # read line as a string
-    # srr = input().strip()
+    srr = input().strip()
 
     # read one line and parse each word as a string
     # lst = input().split()
-    
+
     # read one line and parse each word as an integer
     # a,b,c = list(map(int,input().split()))
     # lst = list(map(int,input().split()))
     # lst = minus_one(lst)
 
     # read multiple rows
-    # arr = read_strings(k)  # and return as a list of str
+    k = int(input())
+    arr = read_strings(k)  # and return as a list of str
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(srr, arr)  # include input here
 
     # print length if applicable
     # print(len(res))
@@ -87,6 +138,6 @@ for case_num in range(int(input())):
     # res = "\n".join(" ".join(str(x) for x in row) for row in res)
 
     # print result
-    # print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
+    print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
 
-    print(res)
+    # print(res)
