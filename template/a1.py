09af048aa2b02875e30601c4ab594f6d3181cc69
hkmac <tonghuikang@outlook.com>
2021-08-28 15:37:04 +0800

    a1

--- template/a1.py
+++ template/a1.py
@@ -47,10 +47,39 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(srr):
     # your solution here
-    
-    return ""
+
+    srr = list(srr)
+    c = Counter(srr)
+    log(c)
+
+    vowels = set(list("aeiou".upper()))
+
+    maxvol = 0
+    cntvol = 0
+    for k in vowels:
+        v = c[k]
+        maxvol = max(maxvol, v)
+        cntvol += v
+
+    maxcon = 0
+    cntcon = 0
+    for k in c:
+        if k in vowels:
+            continue
+        v = c[k]
+        maxcon = max(maxcon, v)
+        cntcon += v
+
+    log(maxcon, maxvol, cntcon, cntvol)
+
+    minres = min(
+        cntvol + (cntcon-maxcon)*2,
+        cntcon + (cntvol-maxvol)*2
+    )
+
+    return minres
 
 
 # for case_num in [0]:  # no loop over test case
@@ -61,11 +90,11 @@ for case_num in range(int(input())):
     # k = int(input())
 
     # read line as a string
-    # srr = input().strip()
+    srr = input().strip()
 
     # read one line and parse each word as a string
     # lst = input().split()
-    
+
     # read one line and parse each word as an integer
     # a,b,c = list(map(int,input().split()))
     # lst = list(map(int,input().split()))
@@ -76,7 +105,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(srr)  # include input here
 
     # print length if applicable
     # print(len(res))
@@ -87,6 +116,6 @@ for case_num in range(int(input())):
     # res = "\n".join(" ".join(str(x) for x in row) for row in res)
 
     # print result
-    # print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
+    print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
 
-    print(res)
+    # print(res)
