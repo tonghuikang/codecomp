fbb33fa2d627cc85e098e293f49cbf1721295927
hkmac <tonghuikang@outlook.com>
2021-09-04 20:09:21 +0800

    abc

--- template/b.py
+++ template/b.py
@@ -47,25 +47,29 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(a,b,c):
     # your solution here
-    
-    return ""
+
+    sett = set(["ABC", "ARC", "AGC", "AHC"]) - set([a,b,c])
+
+    return list(sett)[0]
 
 
-# for case_num in [0]:  # no loop over test case
+for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
-for case_num in range(int(input())):
+# for case_num in range(int(input())):
 
     # read line as an integer
     # k = int(input())
 
     # read line as a string
-    # srr = input().strip()
+    a = input().strip()
+    b = input().strip()
+    c = input().strip()
 
     # read one line and parse each word as a string
-    # lst = input().split()
-    
+    # a,b,c = input().split()
+
     # read one line and parse each word as an integer
     # a,b,c = list(map(int,input().split()))
     # lst = list(map(int,input().split()))
@@ -76,7 +80,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(a,b,c)  # include input here
 
     # print length if applicable
     # print(len(res))
