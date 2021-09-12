585df2c740d6082881c02ad5ac7026613835e9b4
hkmac <tonghuikang@outlook.com>
2021-09-12 22:43:44 +0800

    b wa

--- template/b.py
+++ template/b.py
@@ -47,10 +47,23 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(srr):
     # your solution here
 
-    return ""
+    if "0" not in srr:
+        return 0
+    if "1" not in srr:
+        return 1
+
+    # if 11000 or 00111
+
+    if ("01" in srr) + ("10" in srr) == 1:
+        return 1
+
+    if ("01" in srr) and ("10" in srr) and srr.index("10") < srr.index("01"):
+        return 1
+
+    return 2
 
 
 # for case_num in [0]:  # no loop over test case
@@ -61,7 +74,7 @@ for case_num in range(int(input())):
     # k = int(input())
 
     # read line as a string
-    # srr = input().strip()
+    srr = input().strip()
 
     # read one line and parse each word as a string
     # lst = input().split()
@@ -76,7 +89,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(srr)  # include input here
 
     # print length if applicable
     # print(len(res))
