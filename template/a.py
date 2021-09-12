aa6c9056a61ddaccc37d3252ecc8a15415f3b3ad
hkmac <tonghuikang@outlook.com>
2021-09-12 22:38:02 +0800

    a

--- template/a.py
+++ template/a.py
@@ -47,10 +47,12 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(k,s):
     # your solution here
 
-    return ""
+    left = (k-1)//2
+    log(left)
+    return s // (k - left)
 
 
 # for case_num in [0]:  # no loop over test case
@@ -67,7 +69,7 @@ for case_num in range(int(input())):
     # lst = input().split()
 
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
+    k,s = list(map(int,input().split()))
     # lst = list(map(int,input().split()))
     # lst = minus_one(lst)
 
@@ -76,7 +78,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(k,s)  # include input here
 
     # print length if applicable
     # print(len(res))
