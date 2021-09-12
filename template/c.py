a4fa197356328bd1aa95f08a2fc96346ffdd92e6
hkmac <tonghuikang@outlook.com>
2021-09-12 22:52:27 +0800

    c

--- template/c.py
+++ template/c.py
@@ -47,10 +47,43 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(srr, trr):
     # your solution here
 
-    return ""
+    arr = [0 for _ in srr]
+
+    for i,(a,b) in enumerate(zip(srr, trr)):
+        if a != b:
+            arr[i] = 2
+        if a == b == "0":
+            arr[i] = 1
+        if a == b == "1":
+            arr[i] = 0
+
+    res = sum(arr)
+
+    prev_1 = False
+    prev_0 = False
+    for x in arr:
+        if x == 2:
+            prev_1 = False
+            prev_0 = False
+        if x == 0:
+            if prev_1:
+                res += 1
+                prev_1 = False
+                prev_0 = False
+            else:
+                prev_0 = True
+        if x == 1:
+            if prev_0:
+                res += 1
+                prev_1 = False
+                prev_0 = False
+            else:
+                prev_1 = True
+
+    return res
 
 
 # for case_num in [0]:  # no loop over test case
@@ -58,10 +91,11 @@ def solve_():
 for case_num in range(int(input())):
 
     # read line as an integer
-    # k = int(input())
+    k = int(input())
 
     # read line as a string
-    # srr = input().strip()
+    srr = input().strip()
+    trr = input().strip()
 
     # read one line and parse each word as a string
     # lst = input().split()
@@ -76,7 +110,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(srr, trr)  # include input here
 
     # print length if applicable
     # print(len(res))
