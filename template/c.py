bec29775d37a632918cbd1bb3531e8561cef548c
hkmac <tonghuikang@outlook.com>
2021-09-19 02:02:25 +0800

    c

--- template/c.py
+++ template/c.py
@@ -47,10 +47,136 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
-    # your solution here
+# https://stackoverflow.com/questions/59597399/area-of-triangle-using-3-sets-of-coordinates
+def get_area(x,y):
+    area=0.5*( (x[0]*(y[1]-y[2])) + (x[1]*(y[2]-y[0])) + (x[2]*(y[0]-y[1])) )
+    return area
 
-    return ""
+
+def checkStraightLine(coordinates) -> bool:
+    a,b = coordinates[0]
+    c,d = coordinates[1]
+
+    if a == c:
+        for x,y in coordinates[2:]:
+            if x != a:
+                return False
+        return True
+
+    gradient = (d-b)/(c-a)
+    for x,y in coordinates[2:]:
+        if x == a:
+            if y == b:
+                continue
+            else:
+                return False
+        elif (y-b)/(x-a) != gradient:
+            return False
+    return True
+
+
+def euclidean_dist(x1,y1,x2,y2):
+    return ((x1-x2)**2 + (y1-y2)**2)**0.5
+
+
+def solve_(mrr,x0,y0):
+    # all triangles
+
+    # all quads that are colinear
+
+    minres = 10**18
+
+    for i,(x1,y1) in enumerate(mrr):
+        for j,(x2,y2) in enumerate(mrr[i+1:], start=i+1):
+            for x3,y3 in mrr[j+1:]:
+                log([x1,y1], [x2,y2], [x3,y3])
+                # check if colinear
+
+                if checkStraightLine([[x0,y0], [x1,y1], [x2,y2], [x3,y3]]):
+                    continue
+                if checkStraightLine([[x0,y0],          [x2,y2], [x3,y3]]):
+                    continue
+                if checkStraightLine([[x0,y0], [x1,y1],          [x3,y3]]):
+                    continue
+                if checkStraightLine([[x0,y0], [x1,y1], [x2,y2]         ]):
+                    continue
+
+                default_area = get_area([x1,x2,x3], [y1,y2,y3])
+                a1 = get_area([x1,x2,x0], [y1,y2,y0])
+                a2 = get_area([x1,x0,x3], [y1,y0,y3])
+                a3 = get_area([x0,x2,x3], [y0,y2,y3])
+
+                log(default_area, a1+a2+a3)
+
+                # check if outside
+                if a1 + a2 + a3 - 10**-8 > default_area:
+                    continue
+
+                # parameter = euclidean_dist(x1,y1,x2,y2,x3,y3)
+                p1 = euclidean_dist(x1,y1,x2,y2      )
+                p2 = euclidean_dist(x1,y1,      x3,y3)
+                p3 = euclidean_dist(      x2,y2,x3,y3)
+                p = p1 + p2 + p3
+
+                minres = min(minres, p)
+
+
+    # case that you need quad
+    for i,(x1,y1) in enumerate(mrr):
+        for j,(x2,y2) in enumerate(mrr[i+1:], start=i+1):
+            if checkStraightLine([[x0,y0], [x1,y1], [x2,y2]]):
+
+                minleft = 10**18
+                minright = 10**18
+
+                if x1 == x2:
+                    # no grad
+                    for k,(x3,y3) in enumerate(mrr):
+                        if k == i or k == j:
+                            continue
+                        # if x3 == x2:
+                        #     continue
+                        if checkStraightLine([[x3,y3], [x1,y1], [x2,y2]]):
+                            continue
+
+                        p1 = euclidean_dist(x3,y3,x1,y1)
+                        p2 = euclidean_dist(x3,y3,x2,y2)
+                        p = p1+p2
+
+                        if x3 < x1:  # on left
+                            minleft = min(minleft, p)
+
+                        if x3 > x1:  # on right
+                            minright = min(minright, p)
+
+                else:
+                    # y = mx + c
+                    m = (y2-y1)/(x2-x1)
+                    c = y1 - x1*m
+                    for k,(x3,y3) in enumerate(mrr):
+                        if k == i or k == j:
+                            continue
+                        if checkStraightLine([[x3,y3], [x1,y1], [x2,y2]]):
+                            continue
+
+                        p1 = euclidean_dist(x3,y3,x1,y1)
+                        p2 = euclidean_dist(x3,y3,x2,y2)
+                        p = p1+p2
+
+                        expected_y = m*x3 + c
+                        if expected_y > y:
+                            minleft = min(minleft, p)
+                        else:
+                            minright = min(minright, p)
+
+                total_p = minleft + minright
+                minres = min(minres, total_p)
+
+
+    if minres >= 10**18:
+        return "IMPOSSIBLE"
+
+    return minres
 
 
 # for case_num in [0]:  # no loop over test case
@@ -58,7 +184,7 @@ def solve_():
 for case_num in range(int(input())):
 
     # read line as an integer
-    # k = int(input())
+    n = int(input())
 
     # read line as a string
     # srr = input().strip()
@@ -73,10 +199,11 @@ for case_num in range(int(input())):
 
     # read multiple rows
     # arr = read_strings(k)  # and return as a list of str
-    # mrr = read_matrix(k)  # and return as a list of list of int
+    mrr = read_matrix(n)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
+    x,y = list(map(int,input().split()))
 
-    res = solve()  # include input here
+    res = solve(mrr,x,y)  # include input here
 
     # print length if applicable
     # print(len(res))
@@ -87,6 +214,6 @@ for case_num in range(int(input())):
     # res = "\n".join(" ".join(str(x) for x in row) for row in res)
 
     # print result
-    # print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
+    print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
 
-    print(res)
+    # print(res)
