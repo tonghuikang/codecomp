b11cf6000ae3635fc7d09579614be049381dc507
hkmac <tonghuikang@outlook.com>
2021-09-18 21:12:24 +0800

    filter only alterate

--- template/d.py
+++ template/d.py
@@ -1,25 +1,15 @@
 #!/usr/bin/env python3
-import sys
-import getpass  # not available on codechef
 # import math, random
 # import functools, itertools, collections, heapq, bisect
 from collections import defaultdict
-input = sys.stdin.readline  # to read input quickly
 
 # available on Google, AtCoder Python3, not available on Codeforces
 # import numpy as np
 # import scipy
 
-M9 = 10**9 + 7  # 998244353
-yes, no = "YES", "NO"
-# d4 = [(1,0),(0,1),(-1,0),(0,-1)]
-# d8 = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
-# d6 = [(2,0),(1,1),(-1,1),(-2,0),(-1,-1),(1,-1)]  # hexagonal layout
-MAXINT = sys.maxsize
-
-# if testing locally, print to terminal with a different color
-OFFLINE_TEST = getpass.getuser() == "hkmac"
-# OFFLINE_TEST = False  # codechef does not allow getpass
+# # if testing locally, print to terminal with a different color
+# OFFLINE_TEST = getpass.getuser() == "hkmac"
+OFFLINE_TEST = False  # codechef does not allow getpass
 def log(*args):
     if OFFLINE_TEST:
         print('\033[36m', *args, '\033[0m', file=sys.stderr)
@@ -35,30 +25,82 @@ def solve(*args):
 def read_matrix(rows):
     return [list(map(int,input().split())) for _ in range(rows)]
 
-def read_strings(rows):
-    return [input().strip() for _ in range(rows)]
-
-def minus_one(arr):
-    return [x-1 for x in arr]
-
-def minus_one_matrix(mrr):
-    return [[x-1 for x in row] for row in mrr]
-
 # ---------------------------- template ends here ----------------------------
+
+import os
+import sys
+from io import BytesIO, IOBase
+
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
 
 # import random
 def solve_(mrr,p,q):
     # your solution here
 
-    mrr.sort(key=lambda x: x[0] + x[1])
-    # mrr.sort(key=lambda x: -x[1])
-    mrr.reverse()
+    flag = True
+    # unique = set((x,y) for x,y in mrr)
+    # if len(unique) < 20:
+    #     flag = False
 
-    mincounts = defaultdict(lambda: 1000)
+
+    mrr.sort(key=lambda x: (-x[0]/p -x[1]/q, x))
+    # mrr.sort(key=lambda x: -x[1])
+    # mrr.reverse()
+
+    mincounts = defaultdict(lambda: 301)
     mincounts[0,0] = 0
 
+
     for a,b in mrr:
-        new_count = defaultdict(lambda: 1000)
+        new_count = defaultdict(lambda: 301)
         for (c,d),prev_count in mincounts.items():
             x,y = a+c,b+d
             x = min(p,x)
@@ -73,30 +115,34 @@ def solve_(mrr,p,q):
 
         # log(keys[:10])
 
-        minseen = [301 for _ in range(p+1)]
-        for x,y in keys:
-            count = mincounts[x,y]
-            if minseen[x] <= count:
-                del mincounts[x,y]
-            minseen[x] = count
+        if flag:
+            minseen = [301 for _ in range(p+1)]
+            for x,y in keys:
+                count = mincounts[x,y]
+                if minseen[x] <= count:
+                    del mincounts[x,y]
+                minseen[x] = count
 
-        keys = list(mincounts.keys())
-        keys.sort(key=lambda x:-x[0])
+            keys = list(mincounts.keys())
+            keys.sort(key=lambda x:-x[0])
 
-        minseen = [301 for _ in range(q+1)]
-        for x,y in keys:
-            count = mincounts[x,y]
-            if minseen[y] <= count:
-                del mincounts[x,y]
-            minseen[y] = count
+            minseen = [301 for _ in range(q+1)]
+            for x,y in keys:
+                count = mincounts[x,y]
+                if minseen[y] <= count:
+                    del mincounts[x,y]
+                minseen[y] = count
+
+        flag = not flag
 
 
-        # log(len(mincounts))
+
+        # print(len(mincounts))
 
     # log(len(mincounts))
 
     res = mincounts[p,q]
-    if res == 1000:
+    if res == 301:
         return -1
 
     return res
