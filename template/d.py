cdd91e1551efff91c76f13dd34a75875c388788d
hkmac <tonghuikang@outlook.com>
2021-09-20 23:32:07 +0800

    d

--- template/d.py
+++ template/d.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
 import sys
 import getpass  # not available on codechef
-import math, random
-import functools, itertools, collections, heapq, bisect
-from collections import Counter, defaultdict, deque
+import heapq
 input = sys.stdin.readline  # to read input quickly
 
 # available on Google, AtCoder Python3, not available on Codeforces
 # import numpy as np
 # import scipy
 
-M9 = 10**9 + 7  # 998244353
-yes, no = "YES", "NO"
+# M9 = 10**9 + 7  # 998244353
+# yes, no = "YES", "NO"
 # d4 = [(1,0),(0,1),(-1,0),(0,-1)]
 # d8 = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
 # d6 = [(2,0),(1,1),(-1,1),(-2,0),(-1,-1),(1,-1)]  # hexagonal layout
-MAXINT = sys.maxsize
+# MAXINT = sys.maxsize
 
 # if testing locally, print to terminal with a different color
 OFFLINE_TEST = getpass.getuser() == "hkmac"
@@ -23,6 +21,61 @@ OFFLINE_TEST = getpass.getuser() == "hkmac"
 def log(*args):
     if OFFLINE_TEST:
         print('\033[36m', *args, '\033[0m', file=sys.stderr)
+
+
+import os
+import sys
+from io import BytesIO, IOBase
+
+BUFSIZE = 8192
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
 
 def solve(*args):
     # screen input
@@ -47,18 +100,36 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(arr, brr, banned):
     # your solution here
 
-    return ""
+    n = len(arr)
+    queue = [[sum(row[-1] for row in brr), tuple(x-1 for x in arr)]]
+
+    while queue:
+        score, comb = heapq.heappop(queue)
+        # log(score, comb)
+        if comb not in banned:
+            return comb
+        comb = list(comb)
+        for i in range(n):
+            if comb[i] == 0:
+                continue
+            diff = brr[i][comb[i] - 1] - brr[i][comb[i]]
+            comb[i] -= 1
+            score -= diff
+            heapq.heappush(queue, (score, tuple(comb)))
+            comb[i] += 1
+            score += diff
+
+    return arr
 
 
-# for case_num in [0]:  # no loop over test case
+for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
-for case_num in range(int(input())):
+# for case_num in range(int(input())):
 
     # read line as an integer
-    # k = int(input())
 
     # read line as a string
     # srr = input().strip()
@@ -68,21 +139,35 @@ for case_num in range(int(input())):
 
     # read one line and parse each word as an integer
     # a,b,c = list(map(int,input().split()))
-    # lst = list(map(int,input().split()))
     # lst = minus_one(lst)
+
 
     # read multiple rows
     # arr = read_strings(k)  # and return as a list of str
+    k = int(input())
+    arr = []
+    brr = []
+    for _ in range(k):
+        a, *b = map(int,input().split())
+        arr.append(a)
+        brr.append(b)
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    k = int(input())
+    banned = set()
+    for _ in range(k):
+        to_ban = tuple(map(int,input().split()))
+        to_ban = tuple(x-1 for x in to_ban)
+        banned.add(to_ban)
+
+    res = solve(arr, brr, banned)  # include input here
 
     # print length if applicable
     # print(len(res))
-
+    res = [x+1 for x in res]
     # parse result
-    # res = " ".join(str(x) for x in res)
+    res = " ".join(str(x) for x in res)
     # res = "\n".join(str(x) for x in res)
     # res = "\n".join(" ".join(str(x) for x in row) for row in res)
 
