b620ff2fd5695936439929ed934fd32c0e683719
hkmac <tonghuikang@outlook.com>
2021-09-26 20:58:19 +0800

    from leetcode

--- template/f.py
+++ template/f.py
@@ -1,10 +1,95 @@
 #!/usr/bin/env python3
 import sys
 import getpass  # not available on codechef
-import math, random
-import functools, itertools, collections, heapq, bisect
-from collections import Counter, defaultdict, deque
-input = sys.stdin.readline  # to read input quickly
+import collections
+
+# ------------------------------warmup----------------------------
+import os
+import sys
+from io import BytesIO, IOBase
+sys.setrecursionlimit(300000)
+
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+# https://codeforces.com/blog/entry/80158?locale=en
+from types import GeneratorType
+def bootstrap(f, stack=[]):
+    # usage - please remember to YIELD to call and return
+    '''
+    @bootstrap
+    def recurse(n):
+        if n <= 0:
+            yield 0
+        yield (yield recurse(n-1)) + 2
+
+    res = recurse(10**5)
+    '''
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if type(to) is GeneratorType:
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    if stack:
+                        stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+    return wrappedfunc
+
 
 # available on Google, AtCoder Python3, not available on Codeforces
 # import numpy as np
@@ -47,18 +132,50 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
-    # your solution here
 
-    return ""
+def solve_(N, edges):
+    # https://leetcode.com/problems/sum-of-distances-in-tree/discuss/130583/
+    tree = collections.defaultdict(set)
+    res = [0] * N
+    count = [1] * N
+    for i, j in edges:
+        tree[i].add(j)
+        tree[j].add(i)
 
 
-# for case_num in [0]:  # no loop over test case
+    @bootstrap
+    def dfs(root, pre):
+        for i in tree[root]:
+            if i != pre:
+                yield dfs(i, root)
+                count[root] += count[i]
+                res[root] += res[i] + count[i]
+        yield
+
+    @bootstrap
+    def dfs2(root, pre):
+        for i in tree[root]:
+            if i != pre:
+                res[i] = res[root] - count[i] + N - count[i]
+                yield dfs2(i, root)
+        yield
+
+    dfs(0, -1)
+    dfs2(0, -1)
+    return res
+
+# def solve_():
+#     # your solution here
+
+#     return ""
+
+
+for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
-for case_num in range(int(input())):
+# for case_num in range(int(input())):
 
     # read line as an integer
-    # k = int(input())
+    k = int(input())
 
     # read line as a string
     # srr = input().strip()
@@ -73,17 +190,17 @@ for case_num in range(int(input())):
 
     # read multiple rows
     # arr = read_strings(k)  # and return as a list of str
-    # mrr = read_matrix(k)  # and return as a list of list of int
-    # mrr = minus_one_matrix(mrr)
+    mrr = read_matrix(k-1)  # and return as a list of list of int
+    mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(k, mrr)  # include input here
 
     # print length if applicable
     # print(len(res))
 
     # parse result
     # res = " ".join(str(x) for x in res)
-    # res = "\n".join(str(x) for x in res)
+    res = "\n".join(str(x) for x in res)
     # res = "\n".join(" ".join(str(x) for x in row) for row in res)
 
     # print result
