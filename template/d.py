d99abbd8d9066f40a954b46aacd492841bc9cde8
hkmac <tonghuikang@outlook.com>
2021-10-04 00:03:43 +0800

    attempt d

--- template/d.py
+++ template/d.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys, getpass
+from random import choice
 from collections import Counter, defaultdict, deque
 input = sys.stdin.readline  # to read input quickly
 
@@ -22,141 +23,13 @@ def log(*args):
 
 # ---------------------------- template ends here ----------------------------
 
-def process_tree(mrr):
-
-    g = defaultdict(list)
-
-    for a,b in mrr:
-        g[a].append(b)
-        g[b].append(a)
-
-
-    queue = deque([0])
-    visited = set(queue)
-    while queue:
-        cur = queue.popleft()
-        for nex in g[cur]:
-            if nex in visited:
-                continue
-            visited.add(nex)
-            queue.append(nex)
-
-    queue = deque([cur])
-    visited = set(queue)
-    while queue:
-        cur = queue.popleft()
-        for nex in g[cur]:
-            if nex in visited:
-                continue
-            visited.add(nex)
-            queue.append(nex)
-
-    e1 = cur
-    queue = deque([e1])
-    d1 = {e1:0}
-    while queue:
-        cur = queue.popleft()
-        for nex in g[cur]:
-            if nex in d1:
-                continue
-            d1[nex] = d1[cur] + 1
-            queue.append(nex)
-
-    e2 = cur
-    queue = deque([e2])
-    d2 = {e2:0}
-    while queue:
-        cur = queue.popleft()
-        for nex in g[cur]:
-            if nex in d2:
-                continue
-            d2[nex] = d2[cur] + 1
-            queue.append(nex)
-
-    log(d1)
-    log(d2)
-
-    d1_arr = [0 for _ in range(len(mrr) + 1)]
-    d2_arr = [0 for _ in range(len(mrr) + 1)]
-    for k,v in d1.items():
-        d1_arr[k] = v
-    for k,v in d2.items():
-        d2_arr[k] = v
-    log(d1_arr)
-    log(d2_arr)
-
-    maxd = max(d1_arr)
-
-    endpoints = set()
-    for i,(a,b) in enumerate(zip(d1_arr, d2_arr)):
-        if a == maxd or b == maxd:
-            endpoints.add(i)
-
-    center_point = -1
-    for i,(a,b) in enumerate(zip(d1_arr, d2_arr)):
-        if a == b == maxd/2:
-            # assume only one center point
-            assert center_point == -1
-            # this is the center point
-            center_point = i
-
-
-    log("center_point", center_point)
-
-    if center_point != -1:
-        group_counts = []
-        visited = set([center_point])
-        for start in g[center_point]:
-            group_count = 0
-            visited.add(start)
-            queue = deque([start])
-            while queue:
-                cur = queue.popleft()
-                if cur in endpoints:
-                    group_count += 1
-                for nex in g[cur]:
-                    if nex in visited:
-                        continue
-                    visited.add(nex)
-                    queue.append(nex)
-            group_counts.append(group_count)
-        log(group_counts)
-
-        # select one or zero from each group
-        res = 1
-        for x in group_counts:
-            res = res * (x+1)
-            res = res%998244353
-
-        log(res)
-
-        # select only one group
-        for x in group_counts:
-            res -= x
-
-        # select none of the groups
-        res -= 1
-
-        return res
-
-    # there is a center edge
-    g1_size = 0
-    g2_size = 0
-    for a,b in zip(d1_arr, d2_arr):
-        if a == maxd:
-            g1_size += 1
-        if b == maxd:
-            g2_size += 1
-        assert not a == b == maxd
-
-
-def query(pos):
-    print("{}".format(pos+1), flush=True)
+def query(arr):
+    print("? {}".format(" ".join(str(pos+1) for pos in arr)), flush=True)
     response = int(input())
     return response
 
-def alert(pos):
-    print("! {}".format(pos), flush=True)
+def alert(a,b):
+    print("! {} {}".format(a+1,b+1), flush=True)
     sys.exit()
 
 # -----------------------------------------------------------------------------
@@ -167,19 +40,77 @@ k = int(input())
 def read_matrix(rows):
     return [list(map(int,input().split())) for _ in range(rows)]
 
+def minus_one_matrix(mrr):
+    return [[x-1 for x in row] for row in mrr]
+
 mrr = read_matrix(k-1)  # and return as a list of list of int
+mrr = minus_one_matrix(mrr)
+g = defaultdict(list)
 
+for a,b in mrr:
+    g[a].append(b)
+    g[b].append(a)
 
-# read line as a string
-# srr = input().strip()
+target = query(range(k))
+remainder = set(list(range(k)))
 
-# read one line and parse each word as a string
-# lst = input().split()
+eliminated = set()
 
-# read one line and parse each word as an integer
-# a,b,c = list(map(int,input().split()))
-# lst = list(map(int,input().split()))
+def select_half(remainder, eliminated):
+    start = choice(tuple(remainder))
+    stack = [start]
+    selected = set(stack)
+    while len(selected) < len(remainder) / 2:
+        # print(selected, stack)
+        if stack:
+            cur = stack.pop()
+            for nex in g[cur]:
+                if nex in eliminated:
+                    continue
+                if nex in selected:
+                    continue
+                selected.add(nex)
+                stack.append(nex)
+                if len(selected) >= len(remainder) / 2:
+                    break
+        if not stack:
+            cur = choice(tuple(remainder - selected))
+            stack.append(cur)
 
-# -----------------------------------------------------------------------------
+    return selected
 
-# your code here
+def propagate_selected(selected, eliminated):
+    # include edges between selected and remainder
+    # but do not include edges between eliminated and selected
+    addition = set()
+    for a,b in mrr:
+        if a not in eliminated and (a not in selected) and b in selected:
+            addition.add(b)
+        if b not in eliminated and (b not in selected) and a in selected:
+            addition.add(a)
+    return addition
+
+remainder = set(range(k))
+for _ in range(11):
+    selected = select_half(remainder, eliminated)
+    queried = query(list(selected))
+
+    if queried == target:
+        eliminated.update(remainder - selected)
+        remainder = selected
+
+    else:
+        addition = propagate_selected(selected, eliminated)
+        # print(addition)
+        selected = (remainder - selected) | addition
+        eliminated.update(remainder - selected)
+        remainder = selected
+
+        # print(remainder, addition)
+
+    if len(remainder) == 2:
+        remainder = list(remainder)
+        alert(remainder[0], remainder[1])
+
+remainder = list(remainder)
+alert(remainder[0], remainder[1])
