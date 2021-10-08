80c0aa2dab5da6c483c968714dc016d2b7b854b8
hkmac <tonghuikang@outlook.com>
2021-10-08 23:19:01 +0800

    cases

--- template/a.py
+++ template/a.py
@@ -47,10 +47,10 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(n):
     # your solution here
 
-    return ""
+    return [-n+1, n]
 
 
 # for case_num in [0]:  # no loop over test case
@@ -58,7 +58,7 @@ def solve_():
 for case_num in range(int(input())):
 
     # read line as an integer
-    # k = int(input())
+    n = int(input())
 
     # read line as a string
     # srr = input().strip()
@@ -76,13 +76,13 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(n)  # include input here
 
     # print length if applicable
     # print(len(res))
 
     # parse result
-    # res = " ".join(str(x) for x in res)
+    res = " ".join(str(x) for x in res)
     # res = "\n".join(str(x) for x in res)
     # res = "\n".join(" ".join(str(x) for x in row) for row in res)
 
--- template/b.py
+++ template/b.py
@@ -47,10 +47,24 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(n,k):
     # your solution here
 
-    return ""
+    # k in base n
+    # k -= 1
+    binarr = bin(k)[2:]
+
+    # log(binarr)
+
+    res = 0
+    for c in binarr:
+        res *= n
+        if c == "1":
+            res += 1
+        res = res%M9
+        # log(res)
+
+    return res
 
 
 # for case_num in [0]:  # no loop over test case
@@ -67,7 +81,7 @@ for case_num in range(int(input())):
     # lst = input().split()
 
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
+    n,k = list(map(int,input().split()))
     # lst = list(map(int,input().split()))
     # lst = minus_one(lst)
 
@@ -76,7 +90,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(n,k)  # include input here
 
     # print length if applicable
     # print(len(res))
