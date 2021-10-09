49bb91ac73744fc265981900c8a51ae1aae90ded
hkmac <tonghuikang@outlook.com>
2021-10-09 21:04:44 +0800

    f

--- template/f.py
+++ template/f.py
@@ -15,7 +15,7 @@ yes, no = "YES", "NO"
 # d4 = [(1,0),(0,1),(-1,0),(0,-1)]
 # d8 = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
 # d6 = [(2,0),(1,1),(-1,1),(-2,0),(-1,-1),(1,-1)]  # hexagonal layout
-MAXINT = sys.maxsize
+MAXINT = 10**16
 
 # if testing locally, print to terminal with a different color
 OFFLINE_TEST = getpass.getuser() == "hkmac"
@@ -47,46 +47,95 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
-    # your solution here
+def dijkstra(list_of_indexes_and_costs, start):
+    # short path with nonnegative edge costs
+    # leetcode.com/problems/path-with-maximum-probability/
+    # leetcode.com/problems/network-delay-time/
+    length = len(list_of_indexes_and_costs)
+    visited = [False]*length
+    weights = [MAXINT]*length
+    path = [None]*length
+    queue = []
+    weights[start] = 0
+    heapq.heappush(queue, (0, start))
+    while queue:
+        g, u = heapq.heappop(queue)
+        visited[u] = True
+        for v, w in list_of_indexes_and_costs[u]:
+            if not visited[v]:
+                f = g + w
+                if f < weights[v]:
+                    weights[v] = f
+                    path[v] = u
+                    heapq.heappush(queue, (f, v))
+    return path, weights
 
-    return ""
 
+# def solve_():
+# your solution here
+k = int(input())
 
-# for case_num in [0]:  # no loop over test case
-# for case_num in range(100):  # if the number of test cases is specified
-for case_num in range(int(input())):
+mrr = read_matrix(k-1)  # and return as a list of list of int
+# mrr = minus_one_matrix(mrr)
+lst = list(map(int,input().split()))
 
-    # read line as an integer
-    # k = int(input())
+# log(mrr)
+# log(lst)
 
-    # read line as a string
-    # srr = input().strip()
+g = [[] for _ in range(4*10**5 + 10)]
+for a,b,c in mrr:
+    g[a].append((b,c))
+    g[b].append((a,c))
 
-    # read one line and parse each word as a string
-    # lst = input().split()
+offset = 2*10**5
+for i,x in enumerate(lst, start=1):
+    g[i].append((i+offset,x))
+    g[i+offset].append((i,x))
 
-    # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
-    # lst = list(map(int,input().split()))
-    # lst = minus_one(lst)
+del mrr
 
-    # read multiple rows
-    # arr = read_strings(k)  # and return as a list of str
-    # mrr = read_matrix(k)  # and return as a list of list of int
-    # mrr = minus_one_matrix(mrr)
+res = [0]*(4*10**5 + 10)
 
-    res = solve()  # include input here
+path, weights = dijkstra(g, start=1)
 
-    # print length if applicable
-    # print(len(res))
+e1 = 0
+tmp = 0
+for i,w in enumerate(weights):
+    if w != MAXINT:
+        if w > tmp:
+            tmp = w
+            e1 = i
 
-    # parse result
-    # res = " ".join(str(x) for x in res)
-    # res = "\n".join(str(x) for x in res)
-    # res = "\n".join(" ".join(str(x) for x in row) for row in res)
+# log("end1", e1)
+# log("weights", weights[:10])
+# log("weights", weights[2*10**5:2*10**5 + 10])
 
-    # print result
-    # print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
+path, weights = dijkstra(g, start=e1)
 
-    print(res)
+res = [max(a,b) for a,b in zip(res, weights)]
+
+e2 = 0
+tmp = 0
+for i,w in enumerate(weights):
+    if w != MAXINT:
+        if w > tmp:
+            tmp = w
+            e2 = i
+
+# log("end2", e2)
+# log("weights", weights[:10])
+# log("weights", weights[2*10**5:2*10**5 + 10])
+
+path, weights = dijkstra(g, start=e2)
+
+# log("weights", weights[:10])
+# log("weights", weights[2*10**5:2*10**5 + 10])
+
+res = [max(a,b) for a,b in zip(res, weights)]
+
+res = res[2*10**5+1:]
+
+res = [a-b for a,b in zip(res, lst)][:k]
+res = "\n".join(str(x) for x in res)
+
+print(res)
