c056a326eed74a94c859b972ae510cd0236f04c2
hkmac <tonghuikang@outlook.com>
2021-10-25 16:32:27 +0800

    cases

--- template/e.py
+++ template/e.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import getpass  # not available on codechef
-import math, random
-import functools, itertools, collections, heapq, bisect
-from collections import Counter, defaultdict, deque
+from collections import deque
 input = sys.stdin.readline  # to read input quickly
 
 # available on Google, AtCoder Python3, not available on Codeforces
@@ -47,10 +45,156 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
-    # your solution here
+# Python 3 program to count inversions in an array
 
-    return ""
+# Function to Use Inversion Count
+def mergeSort(arr):
+    n = len(arr)
+    # A temp_arr is created to store
+    # sorted array in merge function
+    temp_arr = [0]*n
+    return _mergeSort(arr, temp_arr, 0, n-1)
+
+# This Function will use MergeSort to count inversions
+
+def _mergeSort(arr, temp_arr, left, right):
+
+    # A variable inv_count is used to store
+    # inversion counts in each recursive call
+
+    inv_count = 0
+
+    # We will make a recursive call if and only if
+    # we have more than one elements
+
+    if left < right:
+
+        # mid is calculated to divide the array into two subarrays
+        # Floor division is must in case of python
+
+        mid = (left + right)//2
+
+        # It will calculate inversion
+        # counts in the left subarray
+
+        inv_count += _mergeSort(arr, temp_arr,
+                                    left, mid)
+
+        # It will calculate inversion
+        # counts in right subarray
+
+        inv_count += _mergeSort(arr, temp_arr,
+                                  mid + 1, right)
+
+        # It will merge two subarrays in
+        # a sorted subarray
+
+        inv_count += merge(arr, temp_arr, left, mid, right)
+    return inv_count
+
+
+# This function will merge two subarrays
+# in a single sorted subarray
+def merge(arr, temp_arr, left, mid, right):
+    i = left     # Starting index of left subarray
+    j = mid + 1 # Starting index of right subarray
+    k = left     # Starting index of to be sorted subarray
+    inv_count = 0
+
+    # Conditions are checked to make sure that
+    # i and j don't exceed their
+    # subarray limits.
+
+    while i <= mid and j <= right:
+
+        # There will be no inversion if arr[i] <= arr[j]
+
+        if arr[i] <= arr[j]:
+            temp_arr[k] = arr[i]
+            k += 1
+            i += 1
+        else:
+            # Inversion will occur.
+            temp_arr[k] = arr[j]
+            inv_count += (mid-i + 1)
+            k += 1
+            j += 1
+
+    # Copy the remaining elements of left
+    # subarray into temporary array
+    while i <= mid:
+        temp_arr[k] = arr[i]
+        k += 1
+        i += 1
+
+    # Copy the remaining elements of right
+    # subarray into temporary array
+    while j <= right:
+        temp_arr[k] = arr[j]
+        k += 1
+        j += 1
+
+    # Copy the sorted subarray into Original array
+    for loop_var in range(left, right + 1):
+        arr[loop_var] = temp_arr[loop_var]
+
+    # https://www.geeksforgeeks.org/counting-inversions/
+    return inv_count
+
+
+
+def solve_(arr, brr):
+    # your solution here
+    # https://cs.stackexchange.com/questions/140295/
+
+    irr = [0 for _ in arr]
+
+    def binary_insert(sa,ea,sb,eb):
+        if sa >= ea:
+            return
+        ma = (sa + ea)//2
+        # log(arr[ma],sa,ea,sb,eb)
+
+        cur = 0
+        for mb in range(sb,eb):
+            if brr[mb] < arr[ma]:
+                cur += 1
+
+        best = cur
+        best_pos = sb
+        for mb in range(sb,eb):
+            if brr[mb] > arr[ma]:
+                cur += 1
+            if brr[mb] < arr[ma]:
+                cur -= 1
+            if cur < best:
+                best = cur
+                best_pos = mb+1
+
+        irr[ma] = best_pos
+        binary_insert(sa,ma,sb,mb)
+        binary_insert(ma+1,ea,mb,eb)
+
+    binary_insert(0,len(arr),0,len(brr))
+
+    irr = deque([(pb,i) for i,pb in enumerate(irr)])
+
+    # log(irr)
+
+    res = []
+    for i,x in enumerate(brr):
+        while irr and irr[0][0] <= i:
+            pb,i = irr.popleft()
+            res.append(arr[i])
+        res.append(x)
+
+    while irr:
+        pb,i = irr.popleft()
+        res.append(arr[i])
+
+    # log(res)
+
+    return mergeSort(res)
 
 
 # for case_num in [0]:  # no loop over test case
@@ -67,16 +211,21 @@ for case_num in range(int(input())):
     # lst = input().split()
 
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
-    # lst = list(map(int,input().split()))
+    a,b = list(map(int,input().split()))
+    brr = list(map(int,input().split()))
+    arr = list(map(int,input().split()))
     # lst = minus_one(lst)
+
+    arr.sort()
+
+
 
     # read multiple rows
     # arr = read_strings(k)  # and return as a list of str
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(arr, brr)  # include input here
 
     # print length if applicable
     # print(len(res))
