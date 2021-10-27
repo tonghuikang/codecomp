77b670b193af3ad5b7ce20dcf38a0170efed9177
hkmac <tonghuikang@outlook.com>
2021-10-27 22:43:15 +0800

    cases

--- template/c.py
+++ template/c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import sys
-import getpass  # not available on codechef
+# import getpass  # not available on codechef
 import math, random
 import functools, itertools, collections, heapq, bisect
 from collections import Counter, defaultdict, deque
@@ -18,8 +18,8 @@ yes, no = "YES", "NO"
 MAXINT = sys.maxsize
 
 # if testing locally, print to terminal with a different color
-OFFLINE_TEST = getpass.getuser() == "hkmac"
-# OFFLINE_TEST = False  # codechef does not allow getpass
+# OFFLINE_TEST = getpass.getuser() == "hkmac"
+OFFLINE_TEST = False  # codechef does not allow getpass
 def log(*args):
     if OFFLINE_TEST:
         print('\033[36m', *args, '\033[0m', file=sys.stderr)
@@ -47,10 +47,42 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(arr, limit, k):
     # your solution here
 
-    return ""
+    discard_rows = (k-1)//2
+    arr = arr[:-discard_rows*k]
+    colsize = k-discard_rows
+
+    heap = []
+    pool = []
+
+    for i,x in enumerate(arr, start=1):
+        if i%colsize == 0:
+            heap.append(x)
+        else:
+            pool.append(x)
+
+    log(heap)
+    log(pool)
+
+    sumheap = sum(heap)
+    if sumheap > limit:
+        return -1
+
+    maxres = heap[0]
+
+    for p in pool:
+        outgoing = heapq.heappop(heap)
+        incoming = p
+        heapq.heappush(heap, p)
+        sumheap += incoming - outgoing
+        if sumheap > limit:
+            return maxres
+        # log(p, heap, sumheap)
+        maxres = max(maxres, heap[0])
+
+    return maxres
 
 
 # for case_num in [0]:  # no loop over test case
@@ -67,16 +99,20 @@ for case_num in range(int(input())):
     # lst = input().split()
 
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
+    k,limit = list(map(int,input().split()))
     # lst = list(map(int,input().split()))
     # lst = minus_one(lst)
 
     # read multiple rows
     # arr = read_strings(k)  # and return as a list of str
+    arr = []
+    for _ in range(k):
+        arr.extend(list(map(int,input().split())))
+    arr.sort()
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(arr, limit, k)  # include input here
 
     # print length if applicable
     # print(len(res))
