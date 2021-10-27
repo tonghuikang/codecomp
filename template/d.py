0f32b0ddc8d6877ac59212708809171f1b8164dc
hkmac <tonghuikang@outlook.com>
2021-10-28 00:17:07 +0800

    dp for partition problem

--- template/d.py
+++ template/d.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 import sys
-# import getpass  # not available on codechef
-import math, random
-import functools, itertools, collections, heapq, bisect
 from collections import Counter, defaultdict, deque
 input = sys.stdin.readline  # to read input quickly
 
@@ -18,6 +15,7 @@ yes, no = "YES", "NO"
 MAXINT = sys.maxsize
 
 # if testing locally, print to terminal with a different color
+# import getpass  # not available on codechef
 # OFFLINE_TEST = getpass.getuser() == "hkmac"
 OFFLINE_TEST = False  # codechef does not allow getpass
 def log(*args):
@@ -45,6 +43,63 @@ def minus_one_matrix(mrr):
     return [[x-1 for x in row] for row in mrr]
 
 # ---------------------------- template ends here ----------------------------
+# A Recursive Python3 program to solve
+# minimum sum partition problem.
+import sys
+
+# Returns the minimum value of the
+# difference of the two sets.
+
+
+def findMin(a):
+    n = len(a)
+    # https://www.geeksforgeeks.org/partition-a-set-into-two-subsets-such-that-the-difference-of-subset-sums-is-minimum/
+    su = 0
+
+    # Calculate sum of all elements
+    su = sum(a)
+
+    # Create an 2d list to store
+    # results of subproblems
+    dp = [[0 for i in range(su + 1)]
+          for j in range(n + 1)]
+
+    # Initialize first column as true.
+    # 0 sum is possible
+    # with all elements.
+    for i in range(n + 1):
+        dp[i][0] = True
+
+    # Initialize top row, except dp[0][0],
+    # as false. With 0 elements, no other
+    # sum except 0 is possible
+    for j in range(1, su + 1):
+        dp[0][j] = False
+
+    # Fill the partition table in
+    # bottom up manner
+    for i in range(1, n + 1):
+        for j in range(1, su + 1):
+
+            # If i'th element is excluded
+            dp[i][j] = dp[i - 1][j]
+
+            # If i'th element is included
+            if a[i - 1] <= j:
+                dp[i][j] |= dp[i - 1][j - a[i - 1]]
+
+    # Initialize difference
+    # of two sums.
+    diff = sys.maxsize
+
+    # Find the largest j such that dp[n][j]
+    # is true where j loops from sum/2 t0 0
+    for j in range(su // 2, -1, -1):
+        if dp[n][j] == True:
+            diff = su - (2 * j)
+            break
+
+    return diff
 
 
 def solve_(mrr,q,k):
@@ -61,27 +116,34 @@ def solve_(mrr,q,k):
     depth[0] = 0
 
     while queue:
-        cur = queue.popleft()
+        cur = queue.pop()
         for nex in g[cur]:
             if nex in depth:
                 continue
             depth[nex] = depth[cur] + 1
             queue.append(nex)
 
-
     log(depth)
 
-    c = Counter(depth.values())
     diffs = []
-
-    for i in range(0,k+2,2):
-        diff = abs(c[i+1] - c[i])
-        if diff:
+    for cur in range(k):
+        if depth[cur]%2 == 0:
+            diff = -1
+            for nex in g[cur]:
+                if depth[nex] == depth[cur] + 1:
+                    diff += 1
+            log(cur, diff)
             diffs.append(diff)
+
+    diffs = [abs(x) for x in diffs]
+
 
     log(diffs)
 
-    return sum(diffs)
+    if q == 1:
+        return sum(diffs)
+
+    return findMin(diffs)
 
 
 # for case_num in [0]:  # no loop over test case
