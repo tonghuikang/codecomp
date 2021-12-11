d39f9e49d46089897508c2396e75b0769bfb24d9
hkmac <tonghuikang@outlook.com>
2021-12-11 03:44:26 -0800

    cases

--- template/e.py
+++ template/e.py
@@ -70,8 +70,8 @@ yes, no = "YES", "NO"
 MAXINT = sys.maxsize
 
 # if testing locally, print to terminal with a different color
-# OFFLINE_TEST = getpass.getuser() == "hkmac"
-OFFLINE_TEST = False  # codechef does not allow getpass
+OFFLINE_TEST = getpass.getuser() == "hkmac"
+# OFFLINE_TEST = False  # codechef does not allow getpass
 def log(*args):
     if OFFLINE_TEST:
         print('\033[36m', *args, '\033[0m', file=sys.stderr)
