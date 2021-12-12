242a7eb019b7eb19c004a4382303f67a4197beb2
hkmac <tonghuikang@outlook.com>
2021-12-12 07:58:22 -0800

    d

--- template/d.py
+++ template/d.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import sys
 import getpass  # not available on codechef
-import math, random
-import functools, itertools, collections, heapq, bisect
-from collections import Counter, defaultdict, deque
+# import math, random
+# import functools, itertools, collections, heapq, bisect
+# from collections import Counter, defaultdict, deque
 input = sys.stdin.readline  # to read input quickly
 
 # available on Google, AtCoder Python3, not available on Codeforces
@@ -47,10 +47,131 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+# Python 3 program to count inversions in an array
+
+# Function to Use Inversion Count
+
+
+def mergeSort(arr):
+	n = len(arr)
+	# A temp_arr is created to store
+	# sorted array in merge function
+	temp_arr = [0]*n
+	return _mergeSort(arr, temp_arr, 0, n-1)
+
+# This Function will use MergeSort to count inversions
+
+
+def _mergeSort(arr, temp_arr, left, right):
+
+	# A variable inv_count is used to store
+	# inversion counts in each recursive call
+
+	inv_count = 0
+
+	# We will make a recursive call if and only if
+	# we have more than one elements
+
+	if left < right:
+
+		# mid is calculated to divide the array into two subarrays
+		# Floor division is must in case of python
+
+		mid = (left + right)//2
+
+		# It will calculate inversion
+		# counts in the left subarray
+
+		inv_count += _mergeSort(arr, temp_arr,
+								left, mid)
+
+		# It will calculate inversion
+		# counts in right subarray
+
+		inv_count += _mergeSort(arr, temp_arr,
+								mid + 1, right)
+
+		# It will merge two subarrays in
+		# a sorted subarray
+
+		inv_count += merge(arr, temp_arr, left, mid, right)
+	return inv_count
+
+# This function will merge two subarrays
+# in a single sorted subarray
+
+
+def merge(arr, temp_arr, left, mid, right):
+	i = left	 # Starting index of left subarray
+	j = mid + 1 # Starting index of right subarray
+	k = left	 # Starting index of to be sorted subarray
+	inv_count = 0
+
+	# Conditions are checked to make sure that
+	# i and j don't exceed their
+	# subarray limits.
+
+	while i <= mid and j <= right:
+
+		# There will be no inversion if arr[i] <= arr[j]
+
+		if arr[i] <= arr[j]:
+			temp_arr[k] = arr[i]
+			k += 1
+			i += 1
+		else:
+			# Inversion will occur.
+			temp_arr[k] = arr[j]
+			inv_count += (mid-i + 1)
+			k += 1
+			j += 1
+
+	# Copy the remaining elements of left
+	# subarray into temporary array
+	while i <= mid:
+		temp_arr[k] = arr[i]
+		k += 1
+		i += 1
+
+	# Copy the remaining elements of right
+	# subarray into temporary array
+	while j <= right:
+		temp_arr[k] = arr[j]
+		k += 1
+		j += 1
+
+	# Copy the sorted subarray into Original array
+	for loop_var in range(left, right + 1):
+		arr[loop_var] = temp_arr[loop_var]
+
+	return inv_count
+
+
+# # Driver Code
+# # Given array is
+# arr = [1, 20, 6, 4, 5]
+# n = len(arr)
+# result = mergeSort(arr, n)
+# print("Number of inversions are", result)
+
+# # This code is contributed by ankush_953
+
+
+
+def solve_(arr):
     # your solution here
 
-    return ""
+    seen = set()
+    for x in arr:
+        if x in seen:
+            return "YES"
+        seen.add(x)
+
+    inv = mergeSort(arr)
+    log(inv)
+    if inv%2 == 1:
+        return "NO"
+    return "YES"
 
 
 # for case_num in [0]:  # no loop over test case
@@ -58,7 +179,7 @@ def solve_():
 for case_num in range(int(input())):
 
     # read line as an integer
-    # k = int(input())
+    k = int(input())
 
     # read line as a string
     # srr = input().strip()
@@ -68,7 +189,7 @@ for case_num in range(int(input())):
 
     # read one line and parse each word as an integer
     # a,b,c = list(map(int,input().split()))
-    # lst = list(map(int,input().split()))
+    arr = list(map(int,input().split()))
     # lst = minus_one(lst)
 
     # read multiple rows
@@ -76,7 +197,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(arr)  # include input here
 
     # print length if applicable
     # print(len(res))
