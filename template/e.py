7f422282f7971a508f93b0263840e73ff8deda48
hkmac <tonghuikang@outlook.com>
2021-12-18 08:53:56 -0800

    cases

--- template/e.py
+++ template/e.py
@@ -47,18 +47,85 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(mrr):
     # your solution here
 
-    return ""
+    curptr = -2
+    ptr_to_val = {}
+    val_to_ptr = {}
+    arr = []
+
+    for i,*q in mrr:
+        curptr -= 1
+
+        # log(arr)
+        # log(ptr_to_val)
+        # log(val_to_ptr)
+        # log([ptr_to_val[ptr] for ptr in arr])
+        # log()
+        # assert len(ptr_to_val) == len(val_to_ptr)
+
+        if i == 1:
+            x = q[0]
+
+            if x in val_to_ptr:
+                ptr = val_to_ptr[x]
+                arr.append(ptr)
+            else:
+                ptr_to_val[curptr] = x
+                val_to_ptr[x] = curptr
+                arr.append(curptr)
+
+        elif i == 2:
+            x,y = q[0],q[1]
+
+            if x == y:
+                continue
+
+            if x not in val_to_ptr:
+                # nothing to replace
+                continue
+
+            if y not in val_to_ptr:
+                # relink
+                ptr = val_to_ptr[x]
+                ptr_to_val[ptr] = y
+                val_to_ptr[y] = ptr
+                del val_to_ptr[x]
+
+            elif y in val_to_ptr:
+                # point to another pointer
+                xptr = val_to_ptr[x]
+                yptr = val_to_ptr[y]
+                ptr_to_val[xptr] = yptr
+                del val_to_ptr[x]
+
+        else:
+            assert False
+
+    # log(arr)
+    # log(ptr_to_val)
+    # log(val_to_ptr)
+    # log([ptr_to_val[ptr] for ptr in arr])
+    # log()
+
+    res = []
+    for ptr in arr:
+        val = ptr_to_val[ptr]
+        while val < 0:
+            val = ptr_to_val[val]
+        ptr_to_val[ptr] = val
+        res.append(val)
+
+    return res
 
 
-# for case_num in [0]:  # no loop over test case
+for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
-for case_num in range(int(input())):
+# for case_num in range(int(input())):
 
     # read line as an integer
-    # k = int(input())
+    k = int(input())
 
     # read line as a string
     # srr = input().strip()
@@ -73,16 +140,16 @@ for case_num in range(int(input())):
 
     # read multiple rows
     # arr = read_strings(k)  # and return as a list of str
-    # mrr = read_matrix(k)  # and return as a list of list of int
+    mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(mrr)  # include input here
 
     # print length if applicable
     # print(len(res))
 
     # parse result
-    # res = " ".join(str(x) for x in res)
+    res = " ".join(str(x) for x in res)
     # res = "\n".join(str(x) for x in res)
     # res = "\n".join(" ".join(str(x) for x in row) for row in res)
 
