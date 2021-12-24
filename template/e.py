4ec1830c31513b35e63bf9f85f5d30d1a8288a75
hkmac <tonghuikang@outlook.com>
2021-12-24 08:50:57 -0800

    cases

--- template/e.py
+++ template/e.py
@@ -238,25 +238,78 @@ class SortedList:
         return 'SortedList({0})'.format(list(self))
 
 
-import sys
-import getpass  # not available on codechef
+# import getpass  # not available on codechef
 from collections import defaultdict
-input = sys.stdin.readline  # to read input quickly
+
+import os
+import sys
+from io import BytesIO, IOBase
+
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
 
 # available on Google, AtCoder Python3, not available on Codeforces
 # import numpy as np
 # import scipy
 
-M9 = 10**9 + 7  # 998244353
-yes, no = "YES", "NO"
+# M9 = 10**9 + 7  # 998244353
+# yes, no = "YES", "NO"
 # d4 = [(1,0),(0,1),(-1,0),(0,-1)]
 # d8 = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
 # d6 = [(2,0),(1,1),(-1,1),(-2,0),(-1,-1),(1,-1)]  # hexagonal layout
-MAXINT = sys.maxsize
+# MAXINT = sys.maxsize
 
 # if testing locally, print to terminal with a different color
-OFFLINE_TEST = getpass.getuser() == "hkmac"
-# OFFLINE_TEST = False  # codechef does not allow getpass
+# OFFLINE_TEST = getpass.getuser() == "hkmac"
+OFFLINE_TEST = False  # codechef does not allow getpass
 def log(*args):
     if OFFLINE_TEST:
         print('\033[36m', *args, '\033[0m', file=sys.stderr)
@@ -311,6 +364,8 @@ def solve_(mrr, n, k):
             stack.append(nex)
             children[cur].append(nex)
             parents[nex] = cur
+
+    del edges
 
     # log("children", children)
     # log("parent", parents)
@@ -388,7 +443,7 @@ def solve_(mrr, n, k):
     return minres
 
 
-for case_num in [0]:  # no loop over test case
+# for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
 # for case_num in range(int(input())):
 
@@ -402,16 +457,16 @@ for case_num in [0]:  # no loop over test case
     # lst = input().split()
 
     # read one line and parse each word as an integer
-    n,k = list(map(int,input().split()))
+n,k = list(map(int,input().split()))
     # lst = list(map(int,input().split()))
     # lst = minus_one(lst)
 
     # read multiple rows
     # arr = read_strings(k)  # and return as a list of str
-    mrr = read_matrix(n-1)  # and return as a list of list of int
-    mrr = minus_one_matrix(mrr)
+mrr = read_matrix(n-1)  # and return as a list of list of int
+mrr = minus_one_matrix(mrr)
 
-    res = solve(mrr, n, k)  # include input here
+res = solve(mrr, n, k)  # include input here
 
     # print length if applicable
     # print(len(res))
@@ -424,4 +479,4 @@ for case_num in [0]:  # no loop over test case
     # print result
     # print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
 
-    print(res)
+print(res)
