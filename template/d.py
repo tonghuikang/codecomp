ee89f8072847a5d65fd75a9d300f219acc1c42dc
hkmac <tonghuikang@outlook.com>
2021-12-27 08:16:38 -0800

    cases

--- template/d.py
+++ template/d.py
@@ -10,7 +10,7 @@ input = sys.stdin.readline  # to read input quickly
 # import numpy as np
 # import scipy
 
-M9 = 10**9 + 7  # 998244353
+M9 = 998244353
 yes, no = "YES", "NO"
 # d4 = [(1,0),(0,1),(-1,0),(0,-1)]
 # d8 = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
@@ -46,28 +46,103 @@ def minus_one_matrix(mrr):
 
 # ---------------------------- template ends here ----------------------------
 
+LARGE = 10**4
+p = 998244353
+factorial_mod_p = [1 for _ in range(LARGE)]
+for i in range(1,LARGE):
+    factorial_mod_p[i] = (factorial_mod_p[i-1]*i)%p
 
-def solve_():
+
+def ncr_mod_p(n, r, p=p):
+    num = factorial_mod_p[n]
+    dem = factorial_mod_p[r]*factorial_mod_p[n-r]
+    return (num * pow(dem, p-2, p))%p
+
+
+
+def solve_(srr, n, k):
     # your solution here
 
-    return ""
+    if k == 0:
+        return 1
+
+    if srr.count("1") < k:
+        return 1
+
+    res = 0
+
+    start = [0]
+    ends = []
+
+    for i,x in enumerate(srr):
+        if x == "1":
+            start.append(i+1)
+            ends.append(i)
+
+    ends.append(n)
+
+    res = 0
+
+    intervals = []
+    for a,b in zip(start, ends[k:]):
+        val = ncr_mod_p(b-a, k)
+        log(a,b,val)
+        res += val
+        intervals.append([a,b])
 
 
-# for case_num in [0]:  # no loop over test case
+    psum = [0]
+    for x in srr:
+        if x == "1":
+            psum.append(psum[-1] + 1)
+        else:
+            psum.append(psum[-1])
+
+    log(psum)
+    log(intervals)
+
+    # second_intervals = []
+
+    for i,(a,b) in enumerate(intervals):
+        for c,d in intervals[i+1:]:
+            # if c >= b:
+            #     break
+            overlap_length = b - c
+            overlap_count = psum[b] - psum[c]
+            val = ncr_mod_p(overlap_length, overlap_count)
+            # second_intervals.append([c,b])
+            log(a,b,c,d, "|", overlap_length, overlap_count, val)
+            res -= val
+
+    # for i,(a,b) in enumerate(second_intervals):
+    #     for c,d in second_intervals[i+1:]:
+    #         # if c >= b:
+    #         #     break
+    #         overlap_length = b - c
+    #         overlap_count = psum[b] - psum[c]
+    #         val = ncr_mod_p(overlap_length, overlap_count)
+    #         # second_intervals.append([c,b])
+    #         log(a,b,c,d, "|", overlap_length, overlap_count, val)
+    #         res += val
+
+    return res%M9
+
+
+for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
-for case_num in range(int(input())):
+# for case_num in range(int(input())):
 
     # read line as an integer
     # k = int(input())
 
     # read line as a string
-    # srr = input().strip()
+    n, k = list(map(int,input().split()))
+    srr = input().strip()
 
     # read one line and parse each word as a string
     # lst = input().split()
 
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
     # lst = list(map(int,input().split()))
     # lst = minus_one(lst)
 
@@ -76,7 +151,10 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(srr, n, k)  # include input here
+
+    # if OFFLINE_TEST:
+    #     assert solve_(srr, n, k) == solve_(srr[::-1], n, k)
 
     # print length if applicable
     # print(len(res))
