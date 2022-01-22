c9ac3aad033de46efb75259bb6e03eaccf5f1cd5
hkmac <tonghuikang@outlook.com>
2022-01-22 07:07:19 -0800

    cases

--- template/a.py
+++ template/a.py
@@ -47,18 +47,43 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(arr):
     # your solution here
 
-    return ""
+    c = Counter(arr)
+    for i in range(len(arr)+1):
+        if i in c:
+            continue
+        else:
+            break
+    mex = i
+    searching = set(range(mex))
 
+    res = []
+
+    for x in arr:
+        c[x] -= 1
+        searching.discard(x)
+        if not searching:
+            res.append(mex)
+            for i in range(mex+1):
+                if c[i] > 0:
+                    continue
+                else:
+                    mex = i
+                    searching = set(range(mex))
+                    break
+        # log(x, mex)
+
+
+    return res
 
 # for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
 for case_num in range(int(input())):
 
     # read line as an integer
-    # k = int(input())
+    k = int(input())
 
     # read line as a string
     # srr = input().strip()
@@ -68,7 +93,7 @@ for case_num in range(int(input())):
 
     # read one line and parse each word as an integer
     # a,b,c = list(map(int,input().split()))
-    # arr = list(map(int,input().split()))
+    arr = list(map(int,input().split()))
     # arr = minus_one(arr)
 
     # read multiple rows
@@ -76,13 +101,13 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(arr)  # include input here
 
     # print length if applicable
-    # print(len(res))
+    print(len(res))
 
     # parse result
-    # res = " ".join(str(x) for x in res)
+    res = " ".join(str(x) for x in res)
     # res = "\n".join(str(x) for x in res)
     # res = "\n".join(" ".join(str(x) for x in row) for row in res)
 
