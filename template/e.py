a880d381b6abd583a065835149f71610c5a4d293
Hui Kang Tong <you@example.com>
2022-02-20 08:20:55 -0800

    runtime error

--- template/e.py
+++ template/e.py
@@ -47,15 +47,102 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_(arr):
+
+def topological_sort(map_from_node_to_nodes, all_nodes=set()):
+    # leetcode.com/problems/course-schedule-ii/
+    indegree_counter = defaultdict(int)
+    for lst in map_from_node_to_nodes.values():
+        for v in lst:
+            indegree_counter[v] += 1
+
+    if not all_nodes:  # assume all nodes are found in the map
+        all_nodes = set(indegree_counter.keys()) | set(map_from_node_to_nodes)
+
+    dq = deque([node for node in all_nodes if node not in indegree_counter])
+
+    res = []
+    while dq:
+        cur = dq.popleft()
+        res.append(cur)
+        for nex in map_from_node_to_nodes[cur]:
+            indegree_counter[nex] -= 1
+            if indegree_counter[nex] == 0:
+                dq.append(nex)
+    return res if len(res) == len(all_nodes) else []
+
+
+def solve_(arr, n):
     # your solution here
 
-    return ""
+    # is_destined = {}
+    g = defaultdict(list)
+
+    for a,b,c in arr:
+    #     is_destined[b,c] = a == 1  # deducted one previously
+    #     is_destined[c,b] = a == 1
+        g[b].append(c)
+        g[c].append(b)
+
+    color = {}
+    for i in range(n):
+        if i in color:
+            continue
+        color[i] = 1
+        stack = [i]
+        while stack:
+            cur = stack.pop()
+            for nex in g[cur]:
+                if nex in color:
+                    if color[nex] == color[cur]:
+                        return False, []
+                    continue
+                color[nex] = 1 - color[cur]
+                stack.append(nex)
+
+    # 0 = faceleft
+    # 1 = faceright
+
+    dirs = defaultdict(list)
+
+    for a,b,c in arr:
+        # deducted one previously
+        is_destined = a == 1
+
+        if is_destined:
+            if color[b] == 1:
+                # b faceright posleft, c faceleft posright
+                dirs[b].append(c)
+            else:
+                dirs[c].append(b)
+
+        else:
+            if color[b] == 1:
+                # b faceright posright, c faceleft posleft
+                dirs[c].append(b)
+            else:
+                dirs[b].append(c)
+
+    arr = topological_sort(dirs, set(range(n)))
+    posting = {x:i for i,x in enumerate(arr)}
+
+    # log(arr)
+    # log(color)
+
+    res = []
+    for i in range(n):
+        if color[i] == 0:
+            dirr = "L"
+        else:
+            dirr = "R"
+        pos = posting[i]
+        res.append("{} {}".format(dirr, pos))
+
+    return True, res
 
 
-# for case_num in [0]:  # no loop over test case
+for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
-for case_num in range(int(input())):
+# for case_num in range(int(input())):
 
     # read line as an integer
     # k = int(input())
@@ -67,23 +154,29 @@ for case_num in range(int(input())):
     # arr = input().split()
 
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
+    n,k = list(map(int,input().split()))
     # arr = list(map(int,input().split()))
     # arr = minus_one(arr)
 
     # read multiple rows
     # arr = read_strings(k)  # and return as a list of str
-    # mrr = read_matrix(k)  # and return as a list of list of int
-    # mrr = minus_one_matrix(mrr)
+    mrr = read_matrix(k)  # and return as a list of list of int
+    mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    boo, res = solve(mrr, n)  # include input here
 
+    if not boo:
+        print(no)
+        continue
+
+    print(yes)
+    
     # print length if applicable
     # print(len(res))
 
     # parse result
     # res = " ".join(str(x) for x in res)
-    # res = "\n".join(str(x) for x in res)
+    res = "\n".join(str(x) for x in res)
     # res = "\n".join(" ".join(str(x) for x in row) for row in res)
 
     # print result
