9f0a2aefbfaa864f5cb2e5f27bbd3ee0c8f4fecb
Hui Kang Tong <you@example.com>
2022-03-11 07:57:44 -0800

    wa d

--- template/d.py
+++ template/d.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import getpass  # not available on codechef
-import math, random
-import functools, itertools, collections, heapq, bisect
-from collections import Counter, defaultdict, deque
+from collections import Counter
 input = sys.stdin.readline  # to read input quickly
 
 # available on Google, AtCoder Python3, not available on Codeforces
@@ -47,10 +45,93 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
+def get_largest_prime_factors(num):
+    # get largest prime factor for each number
+    # you can use this to obtain primes
+    largest_prime_factors = [1] * num
+    for i in range(2, num):
+        if largest_prime_factors[i] > 1:  # not prime
+            continue
+        for j in range(i, num, i):
+            largest_prime_factors[j] = i
+    return largest_prime_factors
+
+
+largest_prime_factors = get_largest_prime_factors(int(10**5))   # take care that it begins with [1,1,2,...]
+primes = [x for i,x in enumerate(largest_prime_factors[2:], start=2) if x == i][::-1]
+
+log(primes[-10:])
+
+def get_prime_factors_with_precomp_largest_factors(num, largest_prime_factors=largest_prime_factors):
+    onum = num
+
+    if num == 1:
+        return [1]
+
+    factors = []
+
+    for p in primes:
+        while num%p == 0:
+            factors.append(p)
+            num = num // p
+    if num > 1:
+        factors.append(num)
+
+    log(onum, factors)
+    return factors
+
+    #     log(onum, factors)
+    #     return factors
+            
+    # # factorise into prime factors given precomputed largest_prime_factors
+    # lf = largest_prime_factors[num]
+    # while lf != num:
+    #     factors.append(lf)
+    #     num //= lf
+    #     lf = largest_prime_factors[num]
+    # if num > 1:
+    #     factors.append(num)
+    # return sorted(factors)
+
+
 def solve_(x,d):
     # your solution here
 
-    return ""
+    factors_x = get_prime_factors_with_precomp_largest_factors(x)
+    counter_x = Counter(factors_x)
+
+    factors_d = get_prime_factors_with_precomp_largest_factors(d)
+    d_is_prime = len(factors_d) == 1
+    # c = Counter(factors)
+
+    count_d = 0
+    remainder = x
+    while remainder%d == 0:
+        remainder = remainder//d
+        count_d += 1
+
+    factors_r = get_prime_factors_with_precomp_largest_factors(remainder)
+    r_is_prime = len(factors_r) == 1
+
+    log(factors_x)
+    log(factors_d)
+    log(factors_r)
+
+    if count_d == 1:
+        # only one representation
+        return no
+
+    if r_is_prime and count_d == 2:
+        # only one representation
+        return no
+
+    if r_is_prime and d_is_prime:
+        return no
+
+    if r_is_prime and remainder * remainder == d:  # [2,1] = [1,2]
+        return no
+    
+    return yes
 
 
 # for case_num in [0]:  # no loop over test case
