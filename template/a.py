ce89517fe2850e28c3b6820ad2cb0507d7b30d0d
Hui Kang Tong <you@example.com>
2022-04-09 07:51:02 -0700

    ab

--- template/a.py
+++ template/a.py
@@ -48,10 +48,25 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(arr, brr):
     # your solution here
 
-    return ""
+    xrr = []
+    yrr = []
+
+    for a,b in zip(arr, brr):
+        if a > b:
+            a,b = b,a
+        xrr.append(a)
+        yrr.append(b)
+
+    res = 0
+    for a,b in zip(xrr, xrr[1:]):
+        res += abs(b-a)
+    for a,b in zip(yrr, yrr[1:]):
+        res += abs(b-a)
+
+    return res
 
 
 # for case_num in [0]:  # no loop over test case
@@ -59,7 +74,7 @@ def solve_():
 for case_num in range(int(input())):
 
     # read line as an integer
-    # k = int(input())
+    k = int(input())
 
     # read line as a string
     # srr = input().strip()
@@ -69,7 +84,8 @@ for case_num in range(int(input())):
 
     # read one line and parse each word as an integer
     # a,b,c = list(map(int,input().split()))
-    # arr = list(map(int,input().split()))
+    arr = list(map(int,input().split()))
+    brr = list(map(int,input().split()))
     # arr = minus_one(arr)
 
     # read multiple rows
@@ -77,7 +93,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(arr, brr)  # include input here
 
     # print length if applicable
     # print(len(res))
--- template/b.py
+++ template/b.py
@@ -48,18 +48,37 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(lst):
     # your solution here
 
-    return ""
+    reslst = []
+    for x in lst:
+        minres = 15
+
+        for addn in range(16):
+            binarr = bin(x)[2:].zfill(15)
+            trailing_zeroes = 15 - len(binarr.rstrip("0"))
+            if x == 32768:
+                res = 0
+            else:
+                res = 15 - trailing_zeroes
+            res += addn
+        
+            minres = min(res, minres)
+            x += 1
+
+        reslst.append(minres)
 
 
-# for case_num in [0]:  # no loop over test case
+    return reslst
+
+
+for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
-for case_num in range(int(input())):
+# for case_num in range(int(input())):
 
     # read line as an integer
-    # k = int(input())
+    k = int(input())
 
     # read line as a string
     # srr = input().strip()
@@ -69,7 +88,7 @@ for case_num in range(int(input())):
 
     # read one line and parse each word as an integer
     # a,b,c = list(map(int,input().split()))
-    # arr = list(map(int,input().split()))
+    arr = list(map(int,input().split()))
     # arr = minus_one(arr)
 
     # read multiple rows
@@ -77,13 +96,13 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(arr)  # include input here
 
     # print length if applicable
     # print(len(res))
 
     # parse result
-    # res = " ".join(str(x) for x in res)
+    res = " ".join(str(x) for x in res)
     # res = "\n".join(str(x) for x in res)
     # res = "\n".join(" ".join(str(x) for x in row) for row in res)
 
