f2e405fa48f2ff1b691fb35377c6a6a0f03bc56c
Hui Kang Tong <you@example.com>
2022-05-22 12:23:52 -0700

    dump

--- template/e.py
+++ template/e.py
@@ -48,18 +48,142 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+# A recursive function used by longestPath. See below
+# link for details
+# https:#www.geeksforgeeks.org/topological-sorting/
+def topologicalSortUtil(v):
+    global Stack, visited, adj
+    visited[v] = True
+ 
+    # Recur for all the vertices adjacent to this vertex
+    # list<AdjListNode>::iterator i
+    for i in adj[v]:
+        if (not visited[i[0]]):
+            topologicalSortUtil(i[0])
+ 
+    # Push current vertex to stack which stores topological
+    # sort
+    Stack.append(v)
+ 
+# The function to find longest distances from a given vertex.
+# It uses recursive topologicalSortUtil() to get topological
+# sorting.
+def longestPath(s):
+    global Stack, visited, adj, V
+    dist = [-10**9 for i in range(V)]
+ 
+    # Call the recursive helper function to store Topological
+    # Sort starting from all vertices one by one
+    for i in range(V):
+        if (visited[i] == False):
+            topologicalSortUtil(i)
+    # print(Stack)
+ 
+    # Initialize distances to all vertices as infinite and
+    # distance to source as 0
+    dist[s] = 0
+    # Stack.append(1)
+ 
+    # Process vertices in topological order
+    while (len(Stack) > 0):
+       
+        # Get the next vertex from topological order
+        u = Stack[-1]
+        del Stack[-1]
+        #print(u)
+ 
+        # Update distances of all adjacent vertices
+        # list<AdjListNode>::iterator i
+        if (dist[u] != 10**9):
+            for i in adj[u]:
+                # print(u, i)
+                if (dist[i[0]] < dist[u] + i[1]):
+                    dist[i[0]] = dist[u] + i[1]
+ 
+    # Print calculated longest distances
+    # print(dist)
+    for i in range(V):
+        print("INF ",end="") if (dist[i] == -10**9) else print(dist[i],end=" ")
+ 
+# Driver code
+if __name__ == '__main__':
+    V, Stack, visited = 6, [], [False for i in range(7)]
+    adj = [[] for i in range(7)]
+     
+    # Create a graph given in the above diagram.
+    # Here vertex numbers are 0, 1, 2, 3, 4, 5 with
+    # following mappings:
+    # 0=r, 1=s, 2=t, 3=x, 4=y, 5=z
+    adj[0].append([1, 5])
+    adj[0].append([2, 3])
+    adj[1].append([3, 6])
+    adj[1].append([2, 2])
+    adj[2].append([4, 4])
+    adj[2].append([5, 2])
+    adj[2].append([3, 7])
+    adj[3].append([5, 1])
+    adj[3].append([4, -1])
+    adj[4].append([5, -2])
+ 
+    s = 1
+    print("Following are longest distances from source vertex ",s)
+    longestPath(s)
+ 
+    # This code is contributed by mohit kumar 29.
+
+
+def z_function(S):
+    # https://github.com/cheran-senthil/PyRival/blob/master/pyrival/strings/z_algorithm.py
+    # https://cp-algorithms.com/string/z-function.html
+    n = len(S)
+    Z = [0] * n
+    l = r = 0
+
+    for i in range(1, n):
+        z = Z[i - l]
+        if i + z >= r:
+            z = max(r - i, 0)
+            while i + z < n and S[z] == S[i + z]:
+                z += 1
+
+            l, r = i, i + z
+
+        Z[i] = z
+
+    Z[0] = n
+    return Z
+
+
+def solve_(arr):
     # your solution here
+
+    g = defaultdict(list)
+
+    for i,x in enumerate(arr):
+        for j,y in enumerate(arr):
+            if i == j:
+                continue
+            combined = x + "$#" + y
+            frr = z_function(combined)
+            log(x,y,frr)
+            maxval = 0
+            for a,b in enumerate(frr[-len(y):][::-1], start=1):
+                if a == b:
+                    maxval = a
+            if maxval > 0:
+                g[j].append((i, maxval))
+
+    log(g)
 
     return ""
 
 
-# for case_num in [0]:  # no loop over test case
+for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
-for case_num in range(int(input())):
+# for case_num in range(int(input())):
 
     # read line as an integer
-    # k = int(input())
+    k = int(input())
 
     # read line as a string
     # srr = input().strip()
@@ -73,11 +197,11 @@ for case_num in range(int(input())):
     # arr = minus_one(arr)
 
     # read multiple rows
-    # arr = read_strings(k)  # and return as a list of str
+    arr = read_strings(k)  # and return as a list of str
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(arr)  # include input here
 
     # print length if applicable
     # print(len(res))
