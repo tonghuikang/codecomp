3cb1cdcd0f0884770f205fa8af169a253169558d
Hui Kang Tong <you@example.com>
2022-05-22 12:59:51 -0700

    dump

--- template/e.py
+++ template/e.py
@@ -51,27 +51,27 @@ def minus_one_matrix(mrr):
 # A recursive function used by longestPath. See below
 # link for details
 # https:#www.geeksforgeeks.org/topological-sorting/
-def topologicalSortUtil(v):
-    global Stack, visited, adj
-    visited[v] = True
- 
-    # Recur for all the vertices adjacent to this vertex
-    # list<AdjListNode>::iterator i
-    for i in adj[v]:
-        if (not visited[i[0]]):
-            topologicalSortUtil(i[0])
- 
-    # Push current vertex to stack which stores topological
-    # sort
-    Stack.append(v)
+
  
 # The function to find longest distances from a given vertex.
 # It uses recursive topologicalSortUtil() to get topological
 # sorting.
-def longestPath(s):
-    global Stack, visited, adj, V
+def longestPath(s, Stack, visited, adj, V):
     dist = [-10**9 for i in range(V)]
  
+    def topologicalSortUtil(v):
+        visited[v] = True
+    
+        # Recur for all the vertices adjacent to this vertex
+        # list<AdjListNode>::iterator i
+        for i in adj[v]:
+            if (not visited[i[0]]):
+                topologicalSortUtil(i[0])
+    
+        # Push current vertex to stack which stores topological
+        # sort
+        Stack.append(v)
+
     # Call the recursive helper function to store Topological
     # Sort starting from all vertices one by one
     for i in range(V):
@@ -102,35 +102,8 @@ def longestPath(s):
  
     # Print calculated longest distances
     # print(dist)
-    for i in range(V):
-        print("INF ",end="") if (dist[i] == -10**9) else print(dist[i],end=" ")
+    return max(dist)
  
-# Driver code
-if __name__ == '__main__':
-    V, Stack, visited = 6, [], [False for i in range(7)]
-    adj = [[] for i in range(7)]
-     
-    # Create a graph given in the above diagram.
-    # Here vertex numbers are 0, 1, 2, 3, 4, 5 with
-    # following mappings:
-    # 0=r, 1=s, 2=t, 3=x, 4=y, 5=z
-    adj[0].append([1, 5])
-    adj[0].append([2, 3])
-    adj[1].append([3, 6])
-    adj[1].append([2, 2])
-    adj[2].append([4, 4])
-    adj[2].append([5, 2])
-    adj[2].append([3, 7])
-    adj[3].append([5, 1])
-    adj[3].append([4, -1])
-    adj[4].append([5, -2])
- 
-    s = 1
-    print("Following are longest distances from source vertex ",s)
-    longestPath(s)
- 
-    # This code is contributed by mohit kumar 29.
-
 
 def z_function(S):
     # https://github.com/cheran-senthil/PyRival/blob/master/pyrival/strings/z_algorithm.py
@@ -157,7 +130,10 @@ def z_function(S):
 def solve_(arr):
     # your solution here
 
+    maxres = 0
+    n = len(arr)
     g = defaultdict(list)
+    adj = [[] for i in range(len(arr))]
 
     for i,x in enumerate(arr):
         for j,y in enumerate(arr):
@@ -172,10 +148,18 @@ def solve_(arr):
                     maxval = a
             if maxval > 0:
                 g[j].append((i, maxval))
+                adj[j].append((i, maxval))
 
-    log(g)
+    for s in range(n):
+        log(g)
 
-    return ""
+        V, Stack, visited = n, [], [False for i in range(n)] 
+
+        res = longestPath(s, Stack, visited, adj, V)
+        maxres = max(maxres, res)
+    # This code is contributed by mohit kumar 29.
+
+    return maxres
 
 
 for case_num in [0]:  # no loop over test case
