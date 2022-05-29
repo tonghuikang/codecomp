02ec90df43bd7d45b0c4f13c0f269f0267355dc3
Hui Kang Tong <you@example.com>
2022-05-28 19:26:01 -0700

    init segtree

---
+++ template/template_segment_trees.py
@@ -0,0 +1,369 @@
+
+# -------------------------- Segment Tree --------------------------
+# https://atcoder.github.io/ac-library/master/document_en/segtree.html
+
+
+import typing
+
+def _ceil_pow2(n: int) -> int:
+    x = 0
+    while (1 << x) < n:
+        x += 1
+
+    return x
+
+
+def _bsf(n: int) -> int:
+    x = 0
+    while n % 2 == 0:
+        x += 1
+        n //= 2
+
+    return x
+
+
+class SegTree:
+    # https://github.com/not522/ac-library-python/blob/master/atcoder/segtree.py
+    def __init__(self,
+                 op: typing.Callable[[typing.Any, typing.Any], typing.Any],
+                 e: typing.Any,
+                 v: typing.Union[int, typing.List[typing.Any]]) -> None:
+        self._op = op
+        self._e = e
+
+        if isinstance(v, int):
+            v = [e] * v
+
+        self._n = len(v)
+        self._log = _ceil_pow2(self._n)
+        self._size = 1 << self._log
+        self._d = [e] * (2 * self._size)
+
+        for i in range(self._n):
+            self._d[self._size + i] = v[i]
+        for i in range(self._size - 1, 0, -1):
+            self._update(i)
+
+    def set(self, p: int, x: typing.Any) -> None:
+        assert 0 <= p < self._n
+
+        p += self._size
+        self._d[p] = x
+        for i in range(1, self._log + 1):
+            self._update(p >> i)
+
+    def get(self, p: int) -> typing.Any:
+        assert 0 <= p < self._n
+
+        return self._d[p + self._size]
+
+    def prod(self, left: int, right: int) -> typing.Any:
+        assert 0 <= left <= right <= self._n
+        sml = self._e
+        smr = self._e
+        left += self._size
+        right += self._size
+
+        while left < right:
+            if left & 1:
+                sml = self._op(sml, self._d[left])
+                left += 1
+            if right & 1:
+                right -= 1
+                smr = self._op(self._d[right], smr)
+            left >>= 1
+            right >>= 1
+
+        return self._op(sml, smr)
+
+    def all_prod(self) -> typing.Any:
+        return self._d[1]
+
+    def max_right(self, left: int,
+                  f: typing.Callable[[typing.Any], bool]) -> int:
+        assert 0 <= left <= self._n
+        assert f(self._e)
+
+        if left == self._n:
+            return self._n
+
+        left += self._size
+        sm = self._e
+
+        first = True
+        while first or (left & -left) != left:
+            first = False
+            while left % 2 == 0:
+                left >>= 1
+            if not f(self._op(sm, self._d[left])):
+                while left < self._size:
+                    left *= 2
+                    if f(self._op(sm, self._d[left])):
+                        sm = self._op(sm, self._d[left])
+                        left += 1
+                return left - self._size
+            sm = self._op(sm, self._d[left])
+            left += 1
+
+        return self._n
+
+    def min_left(self, right: int,
+                 f: typing.Callable[[typing.Any], bool]) -> int:
+        assert 0 <= right <= self._n
+        assert f(self._e)
+
+        if right == 0:
+            return 0
+
+        right += self._size
+        sm = self._e
+
+        first = True
+        while first or (right & -right) != right:
+            first = False
+            right -= 1
+            while right > 1 and right % 2:
+                right >>= 1
+            if not f(self._op(self._d[right], sm)):
+                while right < self._size:
+                    right = 2 * right + 1
+                    if f(self._op(self._d[right], sm)):
+                        sm = self._op(self._d[right], sm)
+                        right -= 1
+                return right + 1 - self._size
+            sm = self._op(self._d[right], sm)
+
+        return 0
+
+    def _update(self, k: int) -> None:
+        self._d[k] = self._op(self._d[2 * k], self._d[2 * k + 1])
+
+
+class LazySegTree:
+    # https://github.com/not522/ac-library-python/blob/master/atcoder/lazysegtree.py
+    def __init__(
+            self,
+            op: typing.Callable[[typing.Any, typing.Any], typing.Any],
+            e: typing.Any,
+            mapping: typing.Callable[[typing.Any, typing.Any], typing.Any],
+            composition: typing.Callable[[typing.Any, typing.Any], typing.Any],
+            id_: typing.Any,
+            v: typing.Union[int, typing.List[typing.Any]]) -> None:
+        self._op = op
+        self._e = e
+        self._mapping = mapping
+        self._composition = composition
+        self._id = id_
+
+        if isinstance(v, int):
+            v = [e] * v
+
+        self._n = len(v)
+        self._log = _ceil_pow2(self._n)
+        self._size = 1 << self._log
+        self._d = [e] * (2 * self._size)
+        self._lz = [self._id] * self._size
+        for i in range(self._n):
+            self._d[self._size + i] = v[i]
+        for i in range(self._size - 1, 0, -1):
+            self._update(i)
+
+    def set(self, p: int, x: typing.Any) -> None:
+        assert 0 <= p < self._n
+
+        p += self._size
+        for i in range(self._log, 0, -1):
+            self._push(p >> i)
+        self._d[p] = x
+        for i in range(1, self._log + 1):
+            self._update(p >> i)
+
+    def get(self, p: int) -> typing.Any:
+        assert 0 <= p < self._n
+
+        p += self._size
+        for i in range(self._log, 0, -1):
+            self._push(p >> i)
+        return self._d[p]
+
+    def prod(self, left: int, right: int) -> typing.Any:
+        assert 0 <= left <= right <= self._n
+
+        if left == right:
+            return self._e
+
+        left += self._size
+        right += self._size
+
+        for i in range(self._log, 0, -1):
+            if ((left >> i) << i) != left:
+                self._push(left >> i)
+            if ((right >> i) << i) != right:
+                self._push(right >> i)
+
+        sml = self._e
+        smr = self._e
+        while left < right:
+            if left & 1:
+                sml = self._op(sml, self._d[left])
+                left += 1
+            if right & 1:
+                right -= 1
+                smr = self._op(self._d[right], smr)
+            left >>= 1
+            right >>= 1
+
+        return self._op(sml, smr)
+
+    def all_prod(self) -> typing.Any:
+        return self._d[1]
+
+    def apply(self, left: int, right: typing.Optional[int] = None,
+              f: typing.Optional[typing.Any] = None) -> None:
+        assert f is not None
+
+        if right is None:
+            p = left
+            assert 0 <= left < self._n
+
+            p += self._size
+            for i in range(self._log, 0, -1):
+                self._push(p >> i)
+            self._d[p] = self._mapping(f, self._d[p])
+            for i in range(1, self._log + 1):
+                self._update(p >> i)
+        else:
+            assert 0 <= left <= right <= self._n
+            if left == right:
+                return
+
+            left += self._size
+            right += self._size
+
+            for i in range(self._log, 0, -1):
+                if ((left >> i) << i) != left:
+                    self._push(left >> i)
+                if ((right >> i) << i) != right:
+                    self._push((right - 1) >> i)
+
+            l2 = left
+            r2 = right
+            while left < right:
+                if left & 1:
+                    self._all_apply(left, f)
+                    left += 1
+                if right & 1:
+                    right -= 1
+                    self._all_apply(right, f)
+                left >>= 1
+                right >>= 1
+            left = l2
+            right = r2
+
+            for i in range(1, self._log + 1):
+                if ((left >> i) << i) != left:
+                    self._update(left >> i)
+                if ((right >> i) << i) != right:
+                    self._update((right - 1) >> i)
+
+    def max_right(
+            self, left: int, g: typing.Callable[[typing.Any], bool]) -> int:
+        assert 0 <= left <= self._n
+        assert g(self._e)
+
+        if left == self._n:
+            return self._n
+
+        left += self._size
+        for i in range(self._log, 0, -1):
+            self._push(left >> i)
+
+        sm = self._e
+        first = True
+        while first or (left & -left) != left:
+            first = False
+            while left % 2 == 0:
+                left >>= 1
+            if not g(self._op(sm, self._d[left])):
+                while left < self._size:
+                    self._push(left)
+                    left *= 2
+                    if g(self._op(sm, self._d[left])):
+                        sm = self._op(sm, self._d[left])
+                        left += 1
+                return left - self._size
+            sm = self._op(sm, self._d[left])
+            left += 1
+
+        return self._n
+
+    def min_left(self, right: int, g: typing.Any) -> int:
+        assert 0 <= right <= self._n
+        assert g(self._e)
+
+        if right == 0:
+            return 0
+
+        right += self._size
+        for i in range(self._log, 0, -1):
+            self._push((right - 1) >> i)
+
+        sm = self._e
+        first = True
+        while first or (right & -right) != right:
+            first = False
+            right -= 1
+            while right > 1 and right % 2:
+                right >>= 1
+            if not g(self._op(self._d[right], sm)):
+                while right < self._size:
+                    self._push(right)
+                    right = 2 * right + 1
+                    if g(self._op(self._d[right], sm)):
+                        sm = self._op(self._d[right], sm)
+                        right -= 1
+                return right + 1 - self._size
+            sm = self._op(self._d[right], sm)
+
+        return 0
+
+    def _update(self, k: int) -> None:
+        self._d[k] = self._op(self._d[2 * k], self._d[2 * k + 1])
+
+    def _all_apply(self, k: int, f: typing.Any) -> None:
+        self._d[k] = self._mapping(f, self._d[k])
+        if k < self._size:
+            self._lz[k] = self._composition(f, self._lz[k])
+
+    def _push(self, k: int) -> None:
+        self._all_apply(2 * k, self._lz[k])
+        self._all_apply(2 * k + 1, self._lz[k])
+        self._lz[k] = self._id
+
+
+
+# Todo
+
+# https://cp-algorithms.com/data_structures/segment_tree.html#simplest-form-of-a-segment-tree
+
+
+# Possible range queries
+# - sum
+# - maximum, minimum
+# - greatest common divisor, least common multiple
+# - number of zeros, k-th zeros (or any other number?)
+# - first element larger than q
+
+# Possible updates
+# - pointwise update/sum
+# - rangewise sum, multiplication, ax+c
+# - rangewise assign
+# - (lazy propagation)
+
+# Other types
+# 2D segment tree
+# Implict segment tree
+
+# Give some examples
+# - 2D Segment tree for https://codeforces.com/contest/1658/problem/E
+#   - probably https://codeforces.com/contest/1658/submission/151178090
