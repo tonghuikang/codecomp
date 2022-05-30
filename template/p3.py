1012538a4e6fc997fc11dc02b4b21b0ca87aeed8
Hui Kang Tong <you@example.com>
2022-05-30 15:15:23 -0700

    dump

--- template/p3.py
+++ template/p3.py
@@ -1,93 +1,74 @@
 #!/usr/bin/env python3
 import sys
-import getpass  # not available on codechef
-import math, random
-import functools, itertools, collections, heapq, bisect
-from collections import Counter, defaultdict, deque
+# import getpass  # not available on codechef
 input = sys.stdin.readline  # to read input quickly
 
 # available on Google, AtCoder Python3, not available on Codeforces
 # import numpy as np
 # import scipy
 
-m9 = 10**9 + 7  # 998244353
-yes, no = "YES", "NO"
-# d4 = [(1,0),(0,1),(-1,0),(0,-1)]
-# d8 = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
-# d6 = [(2,0),(1,1),(-1,1),(-2,0),(-1,-1),(1,-1)]  # hexagonal layout
-MAXINT = sys.maxsize
-e18 = 10**18 + 10
 
 # if testing locally, print to terminal with a different color
-OFFLINE_TEST = getpass.getuser() == "htong"
-# OFFLINE_TEST = False  # codechef does not allow getpass
+# OFFLINE_TEST = getpass.getuser() == "htong"
+OFFLINE_TEST = False  # codechef does not allow getpass
 def log(*args):
     if OFFLINE_TEST:
         print('\033[36m', *args, '\033[0m', file=sys.stderr)
 
-def solve(*args):
-    # screen input
-    if OFFLINE_TEST:
-        log("----- solving ------")
-        log(*args)
-        log("----- ------- ------")
-    return solve_(*args)
-
-def read_matrix(rows):
-    return [list(map(int,input().split())) for _ in range(rows)]
-
-def read_strings(rows):
-    return [input().strip() for _ in range(rows)]
-
-def minus_one(arr):
-    return [x-1 for x in arr]
-
-def minus_one_matrix(mrr):
-    return [[x-1 for x in row] for row in mrr]
-
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_(n,m,mrr):
-    # your solution here
+n,m = list(map(int,input().split()))
+mrr = [list(map(int,input().split())) for _ in range(m)]  # and return as a list of list of int
+# mrr = minus_one_matrix(mrr)
 
-    return ""
+# your solution here
+# just get 50% of the points here
 
+mrr.sort(key=lambda x:(x[1], -x[2]), reverse=True)
 
-for case_num in [0]:  # no loop over test case
-# for case_num in range(100):  # if the number of test cases is specified
-# for case_num in range(int(input())):
+cur = 10**6 + 100
+res = [0]*(n+1)
+right = 0
+constrained = False
+increasing = True
 
-    # read line as an integer
-    # k = int(input())
+exiting = False
 
-    # read line as a string
-    # srr = input().strip()
+for i in range(1,n+1):
+    # log(right, constrained, increasing)
 
-    # read one line and parse each word as a string
-    # arr = input().split()
+    if constrained:
+        # check feasibility, extend right
+        while mrr and mrr[-1][1] < i:
+            t,a,b = mrr.pop()
+            if t == 1 and not increasing:
+                print(-1)
+                exiting = True
+                break
+            if t == 2 and increasing:
+                print(-1)
+                exiting = True
+                break
+            right = max(right, b)
 
-    # read one line and parse each word as an integer
-    n,m = list(map(int,input().split()))
-    # arr = list(map(int,input().split()))
-    # arr = minus_one(arr)
+    if right <= i:
+        constrained = False
 
-    # read multiple rows
-    # arr = read_strings(k)  # and return as a list of str
-    mrr = read_matrix(m)  # and return as a list of list of int
-    # mrr = minus_one_matrix(mrr)
+    if increasing:
+        res[i] = (res[i-1] + 1)
+    else:
+        res[i] = (res[i-1] - 1)
 
-    res = solve(n,m,mrr)  # include input here
+    if mrr and mrr[-1][1] == i:
+        t,a,b = mrr.pop()
+        constrained = True
+        increasing = t == 1
+        right = b
 
-    # print length if applicable
-    # print(len(res))
+if not exiting:
 
-    # parse result
-    # res = " ".join(str(x) for x in res)
-    # res = "\n".join(str(x) for x in res)
-    # res = "\n".join(" ".join(str(x) for x in row) for row in res)
-
-    # print result
-    # print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
-
+    minres = min(res[1:]) - 1
+    res = " ".join(str(x - minres) for x in res[1:])
     print(res)
+
