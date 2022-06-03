7ffbb5dff79e4b18f7589b602628f350e78041ba
Hui Kang Tong <you@example.com>
2022-06-03 09:09:21 -0700

    dump

--- template/c.py
+++ template/c.py
@@ -51,8 +51,67 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+class FenwickTree:
+    # also known as Binary Indexed Tree
+    # binarysearch.com/problems/Virtual-Array
+    # https://leetcode.com/problems/create-sorted-array-through-instructions
+    # may need to be implemented again to reduce constant factor
+
+    # ALL ELEMENTS ARE TO BE POSITIVE
+    def __init__(self, bits=18):
+        self.LARGE = 2**bits
+        self.c = [0]*(self.LARGE + 10)
+
+    def update(self, x, increment):
+        # future query(y) to increase for all y >= x
+        x += 1  # to avoid infinite loop at x > 0
+        while x <= self.LARGE:
+            # increase by the greatest power of two that divides x
+            self.c[x] += increment
+            x += x & -x
+
+    def query(self, x):
+        x += 1  # to avoid infinite loop at x > 0
+        res = 0
+        while x > 0:
+            # decrease by the greatest power of two that divides x
+            res += self.c[x]
+            x -= x & -x
+        return res
+
+
+
+def solve_(arr, brr, mrr, n, m):
     # your solution here
+
+    # any order is fine
+
+    f = FenwickTree(len(bin(n)) - 1)
+    for i,a in enumerate(arr):
+        f.update(i, a)
+
+    rsum = [0]
+    for a in arr:
+        psum.append(rsum[-1] + a)
+
+    psum = [0]
+    for b in brr:
+        psum.append(psum[-1] + b)
+
+    if arr[-1] != brr[-1]:
+        return no
+
+    asum = [rsum[b] - rsum[a-1] for a,b in mrr]
+    bsum = [psum[b] - psum[a-1] for a,b in mrr]
+
+
+    for i,(a,b) in enumerate(zip(asum, bsum)):
+        if a == b:
+            break
+
+        
+
+
 
     return ""
 
@@ -71,16 +130,17 @@ for case_num in range(int(input())):
     # arr = input().split()
 
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
-    # arr = list(map(int,input().split()))
+    n,m = list(map(int,input().split()))
+    arr = list(map(int,input().split()))
+    brr = list(map(int,input().split()))
     # arr = minus_one(arr)
 
     # read multiple rows
     # arr = read_strings(k)  # and return as a list of str
-    # mrr = read_matrix(k)  # and return as a list of list of int
+    mrr = read_matrix(m)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(arr, brr, mrr, n, m)  # include input here
 
     # print length if applicable
     # print(len(res))
