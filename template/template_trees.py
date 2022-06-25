de4bcbf0b4b441905388a52a542d753ab568ee3e
Hui Kang Tong <you@example.com>
2022-06-25 12:07:10 -0700

    range query and lca

--- template/template_trees.py
+++ template/template_trees.py
@@ -10,6 +10,55 @@ MAXINT = sys.maxsize
 
 # ------------------------ standard imports ends here ------------------------
 
+
+class RangeQuery:
+    # range minimum query, range maximum query, rmq
+    # https://codeforces.com/contest/1696/submission/161778620
+    def __init__(self, data, func=min):
+        # https://github.com/cheran-senthil/PyRival/blob/master/pyrival/graphs/lca.py
+        self.func = func
+        self._data = _data = [list(data)]
+        i, n = 1, len(_data[0])
+        while 2 * i <= n:
+            prev = _data[-1]
+            _data.append([func(prev[j], prev[j + i]) for j in range(n - 2 * i + 1)])
+            i <<= 1
+
+    def query(self, begin, end):
+        # queries data[begin:end]
+        depth = (end - begin).bit_length() - 1
+        return self.func(self._data[depth][begin], self._data[depth][end - (1 << depth)])
+
+
+class LowestCommonAncestor:
+    # https://github.com/cheran-senthil/PyRival/blob/master/pyrival/graphs/lca.py
+    # LCA
+    def __init__(self, root, graph):
+        self.time = [-1] * len(graph)
+        self.path = [-1] * len(graph)
+        P = [-1] * len(graph)
+        t = -1
+        dfs = [root]
+        while dfs:
+            node = dfs.pop()
+            self.path[t] = P[node]
+            self.time[node] = t = t + 1
+            for nei in graph[node]:
+                if self.time[nei] == -1:
+                    P[nei] = node
+                    dfs.append(nei)
+        self.rmq = RangeQuery(self.time[node] for node in self.path)
+
+    def __call__(self, a, b):
+        if a == b:
+            return a
+        a = self.time[a]
+        b = self.time[b]
+        if a > b:
+            a, b = b, a
+        return self.path[self.rmq.query(a, b)]
+
+
 # ------------------------------ Binary Lifting ------------------------------
 
 
