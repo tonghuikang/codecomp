9d059da4497b61a76ef81043351b97bfdbcbf093
Hui Kang Tong <you@example.com>
2022-06-25 09:06:54 -0700

    dump

--- template/e.py
+++ template/e.py
@@ -51,18 +51,42 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+LARGE = 10**5 + 10
+p = 10**9 + 7
+factorial_mod_p = [1 for _ in range(LARGE)]
+for i in range(1,LARGE):
+    factorial_mod_p[i] = (factorial_mod_p[i-1]*i)%p
+
+def ncr_mod_p(n, r, p=p):
+    num = factorial_mod_p[n]
+    dem = factorial_mod_p[r]*factorial_mod_p[n-r]
+    return (num * pow(dem, p-2, p))%p
+
+
+def solve_(arr):
     # your solution here
 
-    return ""
+    res = 0
+
+    for r,n in enumerate(arr, start=1):
+        if n == 0:
+            continue
+        # n += 1
+        top = n + r-1
+        bottom = r
+        val = ncr_mod_p(top, bottom)
+        # log(val)
+        res += val
+
+    return res
 
 
-# for case_num in [0]:  # no loop over test case
+for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
-for case_num in range(int(input())):
+# for case_num in range(int(input())):
 
     # read line as an integer
-    # k = int(input())
+    k = int(input())
 
     # read line as a string
     # srr = input().strip()
@@ -72,7 +96,7 @@ for case_num in range(int(input())):
 
     # read one line and parse each word as an integer
     # a,b,c = list(map(int,input().split()))
-    # arr = list(map(int,input().split()))
+    arr = list(map(int,input().split()))
     # arr = minus_one(arr)
 
     # read multiple rows
@@ -80,7 +104,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(arr)  # include input here
 
     # print length if applicable
     # print(len(res))
