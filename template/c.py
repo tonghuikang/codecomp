c780e70fa3182ce573c9ea7a4efd890c5ff8652e
Hui Kang Tong <you@example.com>
2022-07-09 22:56:50 -0700

    dump

--- template/c.py
+++ template/c.py
@@ -51,10 +51,79 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
-    # your solution here
+def remove_consecutive_duplicates(lst):
+    res = []
+    for x in lst:
+        if res and x == res[-1]:
+            continue
+        res.append(x)
+    return res
 
-    return ""
+
+LARGE = 10**18
+LARGE2 = 10**16
+
+def solve_(arr, brr, n, m):
+    # your solution here
+    arr = remove_consecutive_duplicates(arr)
+
+    setbrr = set(brr)
+
+    key_to_loc = {x:[-LARGE2] for x in setbrr}
+
+    for i,x in enumerate(brr):
+        key_to_loc[x].append(i)
+
+    for k in key_to_loc:
+        key_to_loc[k].append(LARGE2)
+
+    log(key_to_loc)
+
+    dp = {}
+    for idx in key_to_loc[arr[0]]:
+        dp[idx] = 0
+    dp[-LARGE2] = LARGE
+    dp[LARGE2] = LARGE
+
+    def transition(dp, old_idxs, new_idxs):
+        assert len(old_idxs) == len(dp)
+        assert set(dp.keys()) == set(old_idxs)
+
+        new_dp = {idx:LARGE for idx in new_idxs}
+        aptr = 0
+        bptr = 0
+        mincost = LARGE
+        while aptr < len(old_idxs) and bptr < len(new_idxs):
+            if old_idxs[aptr] < new_idxs[bptr]:
+                cost = dp[old_idxs[aptr]] - old_idxs[aptr]
+                mincost = min(mincost, cost)
+                aptr += 1
+            else:
+                new_dp[new_idxs[bptr]] = min(new_dp[new_idxs[bptr]], new_idxs[bptr] + mincost) 
+                bptr += 1
+            # log(aptr, bptr, mincost)
+        return new_dp
+
+    for a,b in zip(arr, arr[1:]):
+        old_idxs = key_to_loc[a]
+        new_idxs = key_to_loc[b]
+
+        new_dp1 = transition(dp, old_idxs, new_idxs)
+        new_dp2 = transition({-k:v for k,v in dp.items()}, [-x for x in old_idxs], [-x for x in new_idxs])
+
+        # log()
+        # log(dp)
+        # log({-k:v for k,v in dp.items()})
+        # log(new_dp1)
+        # log(new_dp2)
+
+        dp = {}
+        for k,v in new_dp1.items():
+            dp[k] = min(v, new_dp2[-k])
+
+        log(dp)
+
+    return min(dp.values())
 
 
 # for case_num in [0]:  # no loop over test case
@@ -72,7 +141,10 @@ for case_num in range(int(input())):
 
     # read one line and parse each word as an integer
     # a,b,c = list(map(int,input().split()))
-    # arr = list(map(int,input().split()))
+    n = int(input())
+    arr = list(map(int,input().split()))
+    m = int(input())
+    brr = list(map(int,input().split()))
     # arr = minus_one(arr)
 
     # read multiple rows
@@ -80,7 +152,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(arr, brr, n, m)  # include input here
 
     # print length if applicable
     # print(len(res))
@@ -91,6 +163,6 @@ for case_num in range(int(input())):
     # res = "\n".join(" ".join(str(x) for x in row) for row in res)
 
     # print result
-    # print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
+    print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
 
-    print(res)
+    # print(res)
