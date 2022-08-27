c70b132bdad3f6c45eedd6bcf306df1879063934
Hui Kang Tong <you@example.com>
2022-08-27 07:39:41 -0700

    dump

--- template/a.py
+++ template/a.py
@@ -52,10 +52,26 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(srr):
     # your solution here
 
-    return ""
+    c = Counter(list(srr))
+
+    arr = sorted(c.values())[::-1]
+    log(arr)
+
+    if arr == [4]:
+        return 0
+    if arr == [3,1]:
+        return 1
+    if arr == [2,2]:
+        return 1
+    if arr == [2,1,1]:
+        return 2
+    if arr == [1,1,1,1]:
+        return 3
+
+    return 3
 
 
 # for case_num in [0]:  # no loop over test case
@@ -66,7 +82,8 @@ for case_num in range(int(input())):
     # k = int(input())
 
     # read line as a string
-    # srr = input().strip()
+    srr1 = input().strip()
+    srr2 = input().strip()
 
     # read one line and parse each word as a string
     # arr = input().split()
@@ -81,7 +98,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(srr1 + srr2)  # include input here
 
     # print length if applicable
     # print(len(res))
--- template/b.py
+++ template/b.py
@@ -52,7 +52,7 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(n,m,sx,sy,d):
     # your solution here
 
     return ""
@@ -72,7 +72,7 @@ for case_num in range(int(input())):
     # arr = input().split()
 
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
+    n,m,sx,sy,d = list(map(int,input().split()))
     # arr = list(map(int,input().split()))
     # arr = minus_one(arr)
 
@@ -81,7 +81,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(n,m,sx,sy,d)  # include input here
 
     # print length if applicable
     # print(len(res))
