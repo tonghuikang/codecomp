1b688171a86d6bfe8061d567b5c1e14173a044ad
Hui Kang Tong <you@example.com>
2022-08-27 23:17:04 -0700

    dump

--- template/a.py
+++ template/a.py
@@ -52,10 +52,18 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(n,k,srr):
     # your solution here
 
-    return ""
+    arr = Counter(srr).values()
+    for v in arr:
+        if v > 2:
+            return no
+    
+    if n > k*2:
+        return no
+
+    return yes
 
 
 # for case_num in [0]:  # no loop over test case
@@ -72,8 +80,8 @@ for case_num in range(int(input())):
     # arr = input().split()
 
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
-    # arr = list(map(int,input().split()))
+    n,k = list(map(int,input().split()))
+    srr = list(map(int,input().split()))
     # arr = minus_one(arr)
 
     # read multiple rows
@@ -81,7 +89,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(n,k,srr)  # include input here
 
     # print length if applicable
     # print(len(res))
@@ -92,6 +100,6 @@ for case_num in range(int(input())):
     # res = "\n".join(" ".join(str(x) for x in row) for row in res)
 
     # print result
-    # print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
+    print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
 
-    print(res)
+    # print(res)
