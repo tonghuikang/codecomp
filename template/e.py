010d6c4623890068417fe12aba117326492ca092
Hui Kang Tong <you@example.com>
2022-09-30 10:32:26 -0700

    dump

--- template/e.py
+++ template/e.py
@@ -9,7 +9,7 @@ input = sys.stdin.readline  # to read input quickly
 # import numpy as np
 # import scipy
 
-m9 = 10**9 + 7  # 998244353
+m9 = 998244353 
 yes, no = "YES", "NO"
 # d4 = [(1,0),(0,1),(-1,0),(0,-1)]
 # d8 = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
@@ -52,10 +52,56 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(arr):
     # your solution here
 
-    return ""
+
+    psum = defaultdict(int)
+    psum_idx = defaultdict(list)
+    psum_idx[0].append(0)
+
+    total = sum(arr)
+    rightsum = total
+    leftsum = 0
+
+    prevsum = -1
+
+    qrr = []
+
+    for i,x in enumerate(arr, start=1):
+        rightsum -= x
+        leftsum += x
+        psum_idx[leftsum].append(i)
+        log(i)
+        if rightsum in psum_idx:
+            log(psum_idx[rightsum])
+            if rightsum == prevsum:
+                qrr[-1][-1] += 1
+            elif rightsum == leftsum:
+                qrr.append([0,len(psum_idx[rightsum]),1])
+                log("x")
+            else:
+                qrr.append([1,len(psum_idx[rightsum]),1])
+        prevsum = rightsum
+
+    # qrr = qrr[2:]
+    # log(qrr)
+
+    res = 1
+    if qrr[0][0] == 0:
+        res = pow(2, qrr[0][-1] - 1, m9)
+        qrr = qrr[1:]
+
+    if not qrr:
+        return res
+
+    log(qrr, res)
+    
+    # for x in qrr:
+    #     res *= 2**x
+    # res = res // 2
+
+    return res
 
 
 # for case_num in [0]:  # no loop over test case
@@ -63,7 +109,7 @@ def solve_():
 for case_num in range(int(input())):
 
     # read line as an integer
-    # k = int(input())
+    k = int(input())
 
     # read line as a string
     # srr = input().strip()
@@ -73,7 +119,7 @@ for case_num in range(int(input())):
 
     # read one line and parse each word as an integer
     # a,b,c = list(map(int,input().split()))
-    # arr = list(map(int,input().split()))
+    arr = list(map(int,input().split()))
     # arr = minus_one(arr)
 
     # read multiple rows
@@ -81,7 +127,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(arr)  # include input here
 
     # print length if applicable
     # print(len(res))
