3ed154d6a4ca676967ec2b7d6bca92fe196c60d3
Hui Kang Tong <you@example.com>
2022-10-29 03:34:05 -0700

    dump

--- template/e.py
+++ template/e.py
@@ -52,18 +52,27 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+
+def solve_(arr, n):
     # your solution here
 
-    return ""
+    seen = set()
+
+    res = n
+    for x in arr:
+        if x in seen:
+            res -= 1
+        seen.add(x)
+
+    return res
 
 
-# for case_num in [0]:  # no loop over test case
+for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
-for case_num in range(int(input())):
+# for case_num in range(int(input())):
 
     # read line as an integer
-    # k = int(input())
+    n = int(input())
 
     # read line as a string
     # srr = input().strip()
@@ -73,7 +82,7 @@ for case_num in range(int(input())):
 
     # read one line and parse each word as an integer
     # a,b,c = list(map(int,input().split()))
-    # arr = list(map(int,input().split()))
+    arr = list(map(int,input().split()))
     # arr = minus_one(arr)
 
     # read multiple rows
@@ -81,7 +90,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(arr, n)  # include input here
 
     # print length if applicable
     # print(len(res))
