6646ac24a73d3c9c0ce94086a4050c21387dcf6c
Hui Kang Tong <you@example.com>
2022-11-25 07:37:40 -0800

    dump

--- template/a.py
+++ template/a.py
@@ -52,10 +52,10 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(srr):
     # your solution here
 
-    return ""
+    return srr + srr[::-1]
 
 
 # for case_num in [0]:  # no loop over test case
@@ -67,7 +67,7 @@ for case_num in range(int(input())):
     # k = int(input())
 
     # read line as a string
-    # srr = input().strip()
+    srr = input().strip()
 
     # read one line and parse each word as a string
     # arr = input().split()
@@ -82,7 +82,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(srr)  # include input here
 
     # print length if applicable
     # print(len(res))
