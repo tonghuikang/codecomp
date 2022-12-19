52108726e51501e384905d880cd5eaac121397b7
Hui Kang Tong <you@example.com>
2022-12-19 06:52:51 -0800

    dump

--- template/a.py
+++ template/a.py
@@ -52,10 +52,18 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(arr):
     # your solution here
 
-    return ""
+    minval = arr[0]
+    maxval = arr[0]
+
+    for x in arr:
+        minval = minval & x
+        maxval = maxval | x
+
+
+    return maxval - minval
 
 
 # for case_num in [0]:  # no loop over test case
@@ -63,7 +71,7 @@ def solve_():
 for case_num in range(int(input())):
 
     # read line as an integer
-    # n = int(input())
+    n = int(input())
     # k = int(input())
 
     # read line as a string
@@ -74,7 +82,7 @@ for case_num in range(int(input())):
 
     # read one line and parse each word as an integer
     # a,b,c = list(map(int,input().split()))
-    # arr = list(map(int,input().split()))
+    arr = list(map(int,input().split()))
     # arr = minus_one(arr)
 
     # read multiple rows
@@ -82,7 +90,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(arr)  # include input here
 
     # print length if applicable
     # print(len(res))
