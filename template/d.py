2ce6c4e9bf5dd4a80493f6b41cc7a4ff4d87b39c
Hui Kang Tong <you@example.com>
2022-12-30 13:39:39 -0800

    dump

--- template/d.py
+++ template/d.py
@@ -51,168 +51,77 @@ def minus_one_matrix(mrr):
 
 # ---------------------------- template ends here ----------------------------
 
+class DisjointSet:
+    # github.com/not522/ac-library-python/blob/master/atcoder/dsu.py
+
+    def __init__(self, n: int = 0) -> None:
+        if n > 0:  # constant size DSU
+            self.parent_or_size = [-1]*n
+        else:
+            self.parent_or_size = defaultdict(lambda: -1)
+
+    def union(self, a: int, b: int) -> int:
+        x = self.find(a)
+        y = self.find(b)
+
+        if x == y:
+            return x
+
+        if -self.parent_or_size[x] < -self.parent_or_size[y]:
+            x, y = y, x
+
+        self.parent_or_size[x] += self.parent_or_size[y]
+        self.parent_or_size[y] = x
+
+        return x
+
+    def find(self, a: int) -> int:
+        parent = self.parent_or_size[a]
+        while parent >= 0:
+            if self.parent_or_size[parent] < 0:
+                return parent
+            self.parent_or_size[a], a, parent = (
+                self.parent_or_size[parent],
+                self.parent_or_size[parent],
+                self.parent_or_size[self.parent_or_size[parent]]
+            )
+        return a
+
+    def size(self, a: int) -> int:
+        return -self.parent_or_size[self.find(a)]
+
 
 def solve_(n,arr,brr):
     # your solution here
 
-    cntr = Counter(arr + brr)
-    cntr3 = Counter(arr + brr)
-    if len(cntr) < n:
-        return 0
-
-    wildcards = set()
-    idx1 = defaultdict(set)
-    idx2 = defaultdict(set)
-
-    xrr = []
-    yrr = []
-
-    for i,(a,b) in enumerate(zip(arr,brr)):
-        if a == b and a in wildcards:
-            return 0
-        if a == b:
-            wildcards.add(a)
-            continue
-        if cntr[a] == 1 and cntr[b] == 1:
-            return 0
-        idx1[a].add(i)
-        idx2[b].add(i)
-
-    prune = set()   # force all alternate appearances
-
-    for i,(a,b) in enumerate(zip(arr,brr)):
-        if a == b and a in wildcards:
-            continue
-        if a in wildcards or cntr[b] <= 1:
-            if b in prune:
-                return 0
-            cntr3[b] -= 1
-            if cntr3[b] <= 1:
-                prune.add(b)
-            continue
-        if b in wildcards or cntr[a] <= 1:
-            if a in prune:
-                return 0
-            cntr3[a] -= 1
-            if cntr3[a] <= 1:
-                prune.add(a)
-            continue
-    # log(arr)
-    # log(brr)
-    # log(prune)
-    stack = list(prune)
-
-    while stack:
-        cur = stack.pop()
-
-        for i in idx1[cur]:
-            b = brr[i]
-            if b in prune:
-                return 0
-            if b in wildcards:
-                continue
-            cntr3[b] -= 1
-            if cntr3[b] <= 1:
-                prune.add(b)
-            stack.append(b)
-
-        for i in idx2[cur]:
-            a = arr[i]
-            if a in prune:
-                return 0
-            if a in wildcards:
-                continue
-            cntr3[a] -= 1
-            if cntr3[a] <= 1:
-                prune.add(a)
-            stack.append(a)
-
-    assert not(prune & wildcards)
-
-    # log(wildcards, prune)
-
-    for a,b in zip(arr,brr):
-        if a in wildcards or a in prune:
-            assert b in wildcards or b in prune
-        if a in wildcards or b in wildcards or a in prune or b in prune:
-            continue
-        if a > b:
-            a,b = b,a
-        xrr.append(a)
-        yrr.append(b)
-
-    pairs = Counter()
-    num_cycles = 0
-
-    for a,b in zip(xrr, yrr):
-        if a in wildcards and b in wildcards:
-            return 0
-        if a in wildcards:
-            continue
-        if b in wildcards:
-            continue
-        pairs[a,b] += 1
-        if pairs[a,b] == 2:
-            num_cycles += 1
-        if pairs[a,b] > 2:
-            return 0
-
+    ds = DisjointSet(n)
     g = defaultdict(list)
 
-    cntr2 = Counter()
+    for a,b in zip(arr, brr):
+        ds.union(a,b)
 
-    for a,b in zip(xrr, yrr):
-        if a in wildcards:
-            continue
-        if b in wildcards:
-            continue
-        if pairs[a,b] == 2:
-            continue
-        g[a].append(b)
-        g[b].append(a)
-        cntr2[a] += 1
-        cntr2[b] += 1
+    vertices = defaultdict(set)
+    edges = defaultdict(int)
 
-    paired = set()
-    for (a,b),c in pairs.items():
-        if c == 2:
-            paired.add(a)
-            paired.add(b)
+    num_self = 0
 
-    assert len(prune | wildcards | set(cntr2.keys()) | paired) == n
-    assert len(prune) + len(wildcards) + len(cntr2.keys()) + len(paired) == n
+    for a,b in zip(arr, brr):
+        vertices[ds.find(a)].add(a)
+        vertices[ds.find(a)].add(b)
+        edges[ds.find(a)] += 1
+        if a == b:
+            num_self += 1
+    
+    for k in vertices:
+        if len(vertices[k]) != edges[k]:
+            return 0
 
-    # log(g)
-    assert all(v == 2 for v in cntr2.values())
-    for k,v in g.items():
-        assert len(v) == 2
-        assert v[0] != v[1]
-        assert k != v[0]
-        assert k != v[1]
-        # assert len(v) == len(set(v))
+    num_cycles = len(edges) - num_self
 
-    visited = set()
+    log(num_self)
+    log(num_cycles)
 
-    for x in list(g.keys()):
-        if x in visited:
-            continue
-        visited.add(x)
-        stack = [x]
-        num_cycles += 1
-        while stack:
-            cur = stack.pop()
-            for nex in g[cur]:
-                if nex in visited:
-                    continue
-                visited.add(nex)
-                stack.append(nex)
-
-    # (n ** something) * (2 ** something)
-
-    log(len(wildcards), num_cycles, g)
-    # log(g)
-  
-    return (pow(n, len(wildcards), m9) * pow(2, num_cycles, m9)) % m9
+    return (pow(n, num_self, m9) * pow(2, num_cycles, m9)) % m9
 
 
 # while OFFLINE_TEST:
@@ -240,7 +149,8 @@ for case_num in range(int(input())):
     # a,b,c = list(map(int,input().split()))
     arr = list(map(int,input().split()))
     brr = list(map(int,input().split()))
-    # arr = minus_one(arr)
+    arr = minus_one(arr)
+    brr = minus_one(brr)
 
     # read multiple rows
     # arr = read_strings(k)  # and return as a list of str
