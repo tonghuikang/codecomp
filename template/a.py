67f40c4b8de772f55d28c32c610b5887f4be0412
Hui Kang Tong <you@example.com>
2022-12-30 06:48:52 -0800

    dump

--- template/a.py
+++ template/a.py
@@ -52,10 +52,16 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(arr, brr):
     # your solution here
 
-    return ""
+    heapq.heapify(arr)
+
+    for x in brr:
+        _ = heapq.heappop(arr)
+        heapq.heappush(arr, x)
+
+    return sum(arr)
 
 
 # for case_num in [0]:  # no loop over test case
@@ -73,8 +79,9 @@ for case_num in range(int(input())):
     # arr = input().split()
 
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
-    # arr = list(map(int,input().split()))
+    a,b = list(map(int,input().split()))
+    arr = list(map(int,input().split()))
+    brr = list(map(int,input().split()))
     # arr = minus_one(arr)
 
     # read multiple rows
@@ -82,7 +89,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(arr, brr)  # include input here
 
     # print length if applicable
     # print(len(res))
--- template/b.py
+++ template/b.py
@@ -52,10 +52,35 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(n,k):
     # your solution here
+    # left = []
+    # right = []
+    # boo = True
+    # for x in range(n,0,-1):
+    #     if boo:
+    #         left.append(x)
+    #     else:
+    #         right.append(x)
+    #     boo = not boo
 
-    return ""
+    arr = [-1 for _ in range(n)]
+    ptr = k-1
+    val = 1
+    while ptr < n:
+        arr[ptr] = val
+        ptr += k
+        val += 1
+    
+    log(arr)
+
+    val = n
+    for i in range(n):
+        if arr[i] == -1:
+            arr[i] = val
+            val -= 1
+    
+    return arr
 
 
 # for case_num in [0]:  # no loop over test case
@@ -73,7 +98,7 @@ for case_num in range(int(input())):
     # arr = input().split()
 
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
+    n,k = list(map(int,input().split()))
     # arr = list(map(int,input().split()))
     # arr = minus_one(arr)
 
@@ -82,13 +107,13 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(n,k)  # include input here
 
     # print length if applicable
     # print(len(res))
 
     # parse result
-    # res = " ".join(str(x) for x in res)
+    res = " ".join(str(x) for x in res)
     # res = "\n".join(str(x) for x in res)
     # res = "\n".join(" ".join(str(x) for x in row) for row in res)
 
