7cee26d0fadf151096e90ba90e970356dbd7ed9e
Hui Kang Tong <you@example.com>
2023-01-08 06:46:42 -0800

    dump

--- template/b.py
+++ template/b.py
@@ -52,10 +52,31 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(n):
     # your solution here
 
-    return ""
+    arr = list(range(1, n**2 + 1))
+    arr = deque(arr)
+    brr = []
+
+    while arr:
+        if arr:
+            brr.append(arr.popleft())
+        if arr:
+            brr.append(arr.pop())
+
+    res = [[0 for _ in range(n)] for _ in range(n)]
+
+    boo = False
+    for i in range(n):
+        for j in range(n):
+            if boo:
+                res[i][j] = brr.pop()
+            else:
+                res[i][~j] = brr.pop()
+        boo = not boo
+
+    return res
 
 
 # for case_num in [0]:  # no loop over test case
@@ -63,7 +84,7 @@ def solve_():
 for case_num in range(int(input())):
 
     # read line as an integer
-    # n = int(input())
+    n = int(input())
     # k = int(input())
 
     # read line as a string
@@ -82,7 +103,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(n)  # include input here
 
     # print length if applicable
     # print(len(res))
@@ -90,7 +111,7 @@ for case_num in range(int(input())):
     # parse result
     # res = " ".join(str(x) for x in res)
     # res = "\n".join(str(x) for x in res)
-    # res = "\n".join(" ".join(str(x) for x in row) for row in res)
+    res = "\n".join(" ".join(str(x) for x in row) for row in res)
 
     # print result
     # print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
