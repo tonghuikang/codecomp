09c0abc57bf048d94d803908decb8b913e21a728
Hui Kang Tong <you@example.com>
2023-02-05 04:35:10 -0800

    dump

--- template/a.py
+++ template/a.py
@@ -52,10 +52,20 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(arr):
     # your solution here
 
-    return ""
+    arr.sort()
+
+    res = 0
+    prev = 0
+    for i,x in enumerate(arr):
+        if x == prev:
+            continue
+        prev += 1
+        res += x - prev
+
+    return res
 
 
 # for case_num in [0]:  # no loop over test case
@@ -63,7 +73,7 @@ def solve_():
 for case_num in range(int(input())):
 
     # read line as an integer
-    # n = int(input())
+    n = int(input())
     # k = int(input())
 
     # read line as a string
@@ -74,7 +84,7 @@ for case_num in range(int(input())):
 
     # read one line and parse each word as an integer
     # a,b,c = list(map(int,input().split()))
-    # arr = list(map(int,input().split()))
+    arr = list(map(int,input().split()))
     # arr = minus_one(arr)
 
     # read multiple rows
@@ -82,7 +92,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(arr)  # include input here
 
     # print length if applicable
     # print(len(res))
--- template/b.py
+++ template/b.py
@@ -51,11 +51,93 @@ def minus_one_matrix(mrr):
 
 # ---------------------------- template ends here ----------------------------
 
+mapp = {"w": 0, "i": 1, "n": 2}
+wrr = "win"
 
-def solve_():
-    # your solution here
+def solve_(mrr, k):
+    # direct exchange always possible?
+    mrr = [sorted(mapp[x] for x in xx) for xx in mrr]
 
-    return ""
+    arr = []
+
+    for i,x in enumerate(mrr):
+        if x == [0,1,2]:
+            continue
+        if x == [0,0,0]:
+            arr.append((i,0,1))
+            arr.append((i,0,2))
+            continue
+        if x == [1,1,1]:
+            arr.append((i,1,0))
+            arr.append((i,1,2))
+            continue
+        if x == [2,2,2]:
+            arr.append((i,2,0))
+            arr.append((i,2,1))
+            continue
+        extra = -1
+        need = -1
+        for q in range(3):
+            if x.count(q) == 2:
+                extra = q
+            if x.count(q) == 0:
+                need = q
+        arr.append((i,extra,need))
+
+    # log(arr)
+
+    d = defaultdict(list)
+
+    for i,a,b in arr:
+        d[a,b].append(i)
+
+    res = []
+
+    for a in range(3):
+        for b in range(a+1,3):
+            while d[a,b] and d[b,a]:
+                x = d[a,b].pop()
+                y = d[b,a].pop()
+                res.append((x+1,wrr[a],y+1,wrr[b]))
+
+    while d[2,0] and d[0,1]:
+        x = d[2,0].pop()
+        y = d[0,1].pop()
+        res.append((x+1,wrr[2],y+1,wrr[0]))
+        d[2,1].append(y)
+
+    while d[0,2] and d[1,0]:
+        x = d[0,2].pop()
+        y = d[1,0].pop()
+        res.append((x+1,wrr[0],y+1,wrr[2]))
+        d[1,2].append(y)
+
+    for a in range(3):
+        for b in range(a+1,3):
+            while d[a,b] and d[b,a]:
+                x = d[a,b].pop()
+                y = d[b,a].pop()
+                res.append((x+1,wrr[a],y+1,wrr[b]))
+
+    # for x in list(d.keys()):
+    #     if d[x] == []:
+    #         del d[x]
+
+    # log(d)
+    
+    return res
+
+
+import random
+for _ in range(10):
+    m = random.randint(1,100)
+    srr = ["w"]*m + ["i"]*m + ["n"]*m
+    random.shuffle(srr)
+    inp = []
+    for _ in range(m):
+        inp.append([srr.pop(), srr.pop(), srr.pop()])
+    solve(inp, m)
+    
 
 
 # for case_num in [0]:  # no loop over test case
@@ -64,7 +146,7 @@ for case_num in range(int(input())):
 
     # read line as an integer
     # n = int(input())
-    # k = int(input())
+    k = int(input())
 
     # read line as a string
     # srr = input().strip()
@@ -78,11 +160,15 @@ for case_num in range(int(input())):
     # arr = minus_one(arr)
 
     # read multiple rows
-    # arr = read_strings(k)  # and return as a list of str
+    mrr = read_strings(k)  # and return as a list of str
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(mrr, k)  # include input here
+    print(len(res))
+
+    if len(res) == 0:
+        continue
 
     # print length if applicable
     # print(len(res))
@@ -90,7 +176,7 @@ for case_num in range(int(input())):
     # parse result
     # res = " ".join(str(x) for x in res)
     # res = "\n".join(str(x) for x in res)
-    # res = "\n".join(" ".join(str(x) for x in row) for row in res)
+    res = "\n".join(" ".join(str(x) for x in row) for row in res)
 
     # print result
     # print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
