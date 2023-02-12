3d1906ee5a67cf8d68df8075023b8e9cbb136ac5
Hui Kang Tong <you@example.com>
2023-02-12 00:37:05 -0800

    dump

--- template/a.py
+++ template/a.py
@@ -73,7 +73,8 @@ for case_num in range(int(input())):
     # arr = input().split()
 
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
+    a,b = list(map(int,input().split()))
+    n,m = list(map(int,input().split()))
     # arr = list(map(int,input().split()))
     # arr = minus_one(arr)
 
@@ -82,7 +83,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(a,b,n,m)  # include input here
 
     # print length if applicable
     # print(len(res))
