ada92cacdbe710001fcfca909f6e2d2c8230ada0
Hui Kang Tong <you@example.com>
2023-02-25 06:24:14 -0800

    dump

--- template/a.py
+++ template/a.py
@@ -52,10 +52,19 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(arr):
     # your solution here
 
-    return ""
+    arr.sort()
+
+    gcd = x
+    for x in arr:
+        gcd = math.gcd(x, gcd)
+
+    if gcd > 2:
+        return no
+
+    return yes
 
 
 # for case_num in [0]:  # no loop over test case
@@ -63,7 +72,7 @@ def solve_():
 for case_num in range(int(input())):
 
     # read line as an integer
-    # n = int(input())
+    n = int(input())
     # k = int(input())
 
     # read line as a string
@@ -74,7 +83,7 @@ for case_num in range(int(input())):
 
     # read one line and parse each word as an integer
     # a,b,c = list(map(int,input().split()))
-    # arr = list(map(int,input().split()))
+    arr = list(map(int,input().split()))
     # arr = minus_one(arr)
 
     # read multiple rows
@@ -82,7 +91,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(arr)  # include input here
 
     # print length if applicable
     # print(len(res))
