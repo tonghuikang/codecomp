06a1ac47422b25f2a7546d0a17aad16c51f55e7d
Hui Kang Tong <you@example.com>
2023-02-27 09:09:02 -0800

    dump

--- template/d1.py
+++ template/d1.py
@@ -370,6 +370,92 @@ def solve_ref2(n, k, arr, crr, hrr):
     return min(dp)
 
 
+class LazySegmentTree:
+    def __init__(self, data, default=LARGE, func=min):
+        """initialize the lazy segment tree with data"""
+        self._default = default
+        self._func = func
+
+        self._len = len(data)
+        self._size = _size = 1 << (self._len - 1).bit_length()
+        self._lazy = [0] * (2 * _size)
+
+        self.data = [default] * (2 * _size)
+        self.data[_size:_size + self._len] = data
+        for i in reversed(range(_size)):
+            self.data[i] = func(self.data[i + i], self.data[i + i + 1])
+
+    def __len__(self):
+        return self._len
+
+    def _push(self, idx):
+        """push query on idx to its children"""
+        # Let the children know of the queries
+        q, self._lazy[idx] = self._lazy[idx], 0
+
+        self._lazy[2 * idx] += q
+        self._lazy[2 * idx + 1] += q
+        self.data[2 * idx] += q
+        self.data[2 * idx + 1] += q
+
+    def _update(self, idx):
+        """updates the node idx to know of all queries applied to it via its ancestors"""
+        for i in reversed(range(1, idx.bit_length())):
+            self._push(idx >> i)
+
+    def _build(self, idx):
+        """make the changes to idx be known to its ancestors"""
+        idx >>= 1
+        while idx:
+            self.data[idx] = self._func(self.data[2 * idx], self.data[2 * idx + 1]) + self._lazy[idx]
+            idx >>= 1
+
+    def add(self, start, stop, value):
+        """lazily add value to [start, stop)"""
+        start = start_copy = start + self._size
+        stop = stop_copy = stop + self._size
+        while start < stop:
+            if start & 1:
+                self._lazy[start] += value
+                self.data[start] += value
+                start += 1
+            if stop & 1:
+                stop -= 1
+                self._lazy[stop] += value
+                self.data[stop] += value
+            start >>= 1
+            stop >>= 1
+
+        # Tell all nodes above of the updated area of the updates
+        self._build(start_copy)
+        self._build(stop_copy - 1)
+
+    def query(self, start, stop, default=0):
+        """func of data[start, stop)"""
+        start += self._size
+        stop += self._size
+
+        # Apply all the lazily stored queries
+        self._update(start)
+        self._update(stop - 1)
+
+        res = default
+        while start < stop:
+            if start & 1:
+                res = self._func(res, self.data[start])
+                start += 1
+            if stop & 1:
+                stop -= 1
+                res = self._func(res, self.data[stop])
+            start >>= 1
+            stop >>= 1
+        return res
+
+    def __repr__(self):
+        return "LazySegmentTree({0})".format(self.data)
+
+
+
 def solve_(n, k, arr, crr, hrr):
     # your solution here
 
@@ -391,7 +477,7 @@ def solve_(n, k, arr, crr, hrr):
 
     dp = [LARGE for _ in range(k)]
     dp.append(0)
-    st = LazySegTree(op, e, mapping, composition, id_, dp)
+    st = LazySegmentTree(dp)
     # st.apply(l, r, f)   # redefine f before calling this
     # st.prod(l, r)
 
@@ -417,22 +503,22 @@ def solve_(n, k, arr, crr, hrr):
         f = crr[idx]
 
         # new_dp[idx] = min(min(dp) + crr[idx], dp[idx] + hrr[idx])
-        min_dp = st.all_prod()
-        dp_idx = st.get(idx)
+        min_dp = st.query(0, k+1)
+        dp_idx = st.query(idx, idx+1)
         new_dp_idx = min(min_dp + crr[idx], dp_idx + hrr[idx])
 
         st.apply(0, k+1, f)
-        st.set(idx, new_dp_idx)
+        st.add(idx, idx+1, new_dp_idx - st.query(idx, idx+1))
 
         # new_dp[prev] = min(new_dp[prev], dp[idx] + hrr[idx])
         dp_prev = st.get(prev)
         new_dp_prev = min(dp_prev, dp_idx + hrr[idx])
-        st.set(prev, new_dp_prev)
+        st.add(prev, prev+1, new_dp_prev - st.query(prev, prev+1))
 
         # dp = new_dp
         prev = idx
 
-    return st.all_prod()
+    return st.query(0, k+1)
 
 
 
