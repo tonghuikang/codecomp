eaf1a4ca10b0e6b4bdf836ba0df7b2106152ba6c
Hui Kang Tong <you@example.com>
2023-04-02 11:50:13 -0700

    todo: generate random trees

---
+++ template/template_generate.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys, os, getpass
+import math, random
+import functools, itertools, collections, heapq, bisect
+from collections import Counter, defaultdict, deque
+
+MAXINT = sys.maxsize
+
+
+# generate random tree
+
+# generate the standard trees
+# line
+# hub and spoke
+
+# generate random array
