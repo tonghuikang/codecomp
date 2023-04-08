a7bd30840538de1e1014919ec80febb2b958e40e
Hui Kang Tong <you@example.com>
2023-04-08 08:17:26 -0700

    dump

--- template/c.py
+++ template/c.py
@@ -47,31 +47,36 @@ def check(i,j,top_left,top_right,bottom_left):
                 return True
     return False
 
-# read line as an integer
-for case_num in range(int(input())):
-
-    # read line as a string
-    # srr = input().strip()
-
-    # read one line and parse each word as a string
-    # lst = input().split()
+def solve(n,m,query=query,alert=alert):
 
     # read one line and parse each word as an integer
-    n,m = list(map(int,input().split()))
     # lst = list(map(int,input().split()))
 
     top_left = query(0,0)
-    top_right = query(0,m-1)
 
-    if top_left + top_right == m-1:
-        x = query(0,top_left)
-        alert(x,top_left)
-        continue
+    if m >= n:
+        top_right = query(0,m-1)
+        if top_left + top_right == m-1:
+            # log("y")
+            x = query(0,top_left)
+            alert(x,top_left)
+            return
+        bottom_left = query(n-1,0)
 
-    bottom_left = query(n-1,0)
+    else:
+        bottom_left = query(n-1,0)
+        if top_left + bottom_left == n-1:
+            # log("x")
+            x = query(top_left,0)
+            alert(top_left,x)
+            return
+        top_right = query(0,m-1)
+
+    assert top_left + top_right >= m-1
+    assert top_left + bottom_left >= n-1
 
     qrr = []
-    for q in [top_left, top_right, bottom_left]:
+    for q in [top_left, top_right, bottom_left, m-1-top_right, n-1-bottom_left]:
         qrr.append(q)
 
     # xrr = []
@@ -87,15 +92,58 @@ for case_num in range(int(input())):
     # yrr.append(bottom_left)
 
     qrr = sorted(set(qrr))
+    # log(qrr)
 
     allcnt = 0
     for x in qrr:
+        if not (0 <= x <= n-1):
+            continue
         for y in qrr:
+            if not (0 <= y <= m-1):
+                continue
             if check(x,y,top_left,top_right,bottom_left):
                 allcnt += 1
                 alert(x,y)
+                # return
 
+    # log(allcnt)
     assert allcnt == 1
+
+
+while OFFLINE_TEST:
+    n = random.randint(1,5)
+    m = random.randint(1,5)
+    a = random.randint(1,n)
+    b = random.randint(1,m)
+
+    def query(x,y):
+        return dist(a,b,x+1,y+1)
+
+    def alert(x,y):
+        log(x,y)
+        assert (x+1,y+1) == (a,b)
+    
+    log()
+    log(n,m,a,b)
+    solve(n,m,query,alert)
+
+
+
+# read line as an integer
+for case_num in range(int(input())):
+
+    # read line as a string
+    # srr = input().strip()
+
+    # read one line and parse each word as a string
+    # lst = input().split()
+    n,m = list(map(int,input().split()))
+
+    solve(n,m)
+
+
+
+
 
 # -----------------------------------------------------------------------------
 sys.exit()
