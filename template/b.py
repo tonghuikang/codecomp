f209b17a48991ac521da70e8286bf91d0075ec3a
Hui Kang Tong <you@example.com>
2023-04-09 08:42:28 -0700

    dump

--- template/b.py
+++ template/b.py
@@ -25,31 +25,96 @@ def log(*args):
 
 # ---------------------------- template ends here ----------------------------
 
-def query(pos):
-    print("? {}".format(pos+1), flush=True)
+def add(pos):
+    print("+ {}".format(pos+1), flush=True)
     response = int(input())
     return response
 
-def alert(pos):
-    print("! {}".format(pos+1), flush=True)
-    sys.exit()
+def query(a,b):
+    print("? {} {}".format(a+1, b+1), flush=True)
+    response = int(input())
+    return response
+
+def alert(perm1, perm2):
+    print("! {} {}".format(
+        " ".join(str(x) for x in perm1),
+        " ".join(str(x) for x in perm2),        
+    ), flush=True)
 
 # -----------------------------------------------------------------------------
 
 for case_num in range(int(input())):
     # read line as an integer
-    # k = int(input())
+    n = int(input())
 
-    # read line as a string
-    # srr = input().strip()
+    add(n)
+    add(n+1)
 
-    # read one line and parse each word as a string
-    # lst = input().split()
+    dist = query(0,1)
+    dist_from_zero = [-1,-1]
+    dist_from_one = [-1,-1]
 
-    # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
-    # lst = list(map(int,input().split()))
+    for i in range(2,n):   # 2n-4
+        d0 = query(0,i)
+        d1 = query(1,i)
+    
+    p0 = max(dist_from_zero)
+    p1 = max(dist_from_one)
+    p1 = n-1-p1
 
-    # -----------------------------------------------------------------------------
+    arr = list(range(n // 2))[::-1]
+    brr = list(range(n // 2, n))
 
-    # your code here
+    perm_to_order = [-1 for _ in range(n)]
+    perm_to_order[0] = p0
+    perm_to_order[1] = p1
+
+    for i in range(2, n):
+        # find where the item on position i of perm is on the order
+        d0 = dist_from_zero[i]
+        d1 = dist_from_one[i]
+
+        if d0 + d1 == dist:
+            # in the middle
+            perm_to_order[i] = p1 + d1  # p0 - d0
+            assert p0 - d0 == p1 + d1
+            continue
+        
+        if d0 > d1:
+            # on the p1 side
+            perm_to_order[i] = p0 - d0  # or p1 - d1
+            assert p0 - d0 == or p1 - d1
+            continue
+
+        if d0 < d1:
+            # on the p0 side
+            perm_to_order[i] = p0 + d0  # or p1 + d1
+            assert p0 + d0 == or p1 + d1
+            continue
+
+    assert -1 not in perm_to_order
+    assert sorted(perm_to_order) == list(range(n))
+
+    order = []
+    while arr or brr:
+        if arr:
+            order.append(arr.pop())
+        if brr:
+            order.append(brr.pop())
+    # print(order)
+
+    perm1 = [-1 for _ in range(n)]
+    for i,x in enumerate(perm_to_order):
+        perm1[x] = order[i]
+
+    order.reverse()
+
+    perm2 = [-1 for _ in range(n)]
+    for i,x in enumerate(perm_to_order):
+        perm1[x] = order[i]
+
+    alert(perm1, perm2)
+
+
+
+sys.exit()
