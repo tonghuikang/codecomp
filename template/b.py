ec532632695f0c536e6c99261ca41ff284c9bafa
Hui Kang Tong <you@example.com>
2023-04-09 09:14:24 -0700

    dump

--- template/b.py
+++ template/b.py
@@ -45,12 +45,7 @@ def alert(perm1, perm2):
     assert response == 1
 
 
-# -----------------------------------------------------------------------------
-
-def solve():
-    # read line as an integer
-    n = int(input())
-
+def get_order(n):
     arr = list(range(n // 2))[::-1]
     brr = list(range(n // 2, n))
 
@@ -60,14 +55,29 @@ def solve():
             order.append(arr.pop())
         if brr:
             order.append(brr.pop())
-    log(order)
+    return order
+
+
+# -----------------------------------------------------------------------------
+
+def solve(n, add=add, query=query, alert=alert):
+    if n == 2:
+        alert([0,1], [1,0])
+        return
+    # read line as an integer
+
+    order = get_order(n)
 
     add(n)
     add(n+1)
 
     dist = query(0,1)
+    log("dist", dist)
     dist_from_zero = [-1,-1]
     dist_from_one = [-1,-1]
+
+    dist_from_zero[1] = dist
+    dist_from_one[0] = dist
 
     for i in range(2,n):   # 2n-4
         d0 = query(0,i)
@@ -78,18 +88,25 @@ def solve():
     del d0
     del d1
     
+    perm_to_order = [-1 for _ in range(n)]
+
     p0 = max(dist_from_zero)
     p1 = max(dist_from_one)
-    p1 = n-1-p1
 
-    perm_to_order = [-1 for _ in range(n)]
+    log("p0p1", p0, p1)
+
+    p1 = p0-dist
+
+    log("p0p1", p0, p1, "d")
+
     perm_to_order[0] = p0
     perm_to_order[1] = p1
-
+    
     for i in range(2, n):
         # find where the item on position i of perm is on the order
         d0 = dist_from_zero[i]
         d1 = dist_from_one[i]
+        log("d0d1", d0, d1)
 
         if d0 + d1 == dist:
             # in the middle
@@ -109,6 +126,8 @@ def solve():
             assert p0 + d0 == p1 + d1
             continue
 
+    log("perm_to_order", perm_to_order)
+
     assert -1 not in perm_to_order
     assert sorted(perm_to_order) == list(range(n))
 
@@ -121,7 +140,10 @@ def solve():
 
     perm2 = [-1 for _ in range(n)]
     for i,x in enumerate(perm_to_order):
-        perm1[x] = order[i]
+        perm2[x] = order[i]
+
+    log(perm1)
+    log(perm2)
 
     assert sorted(perm1) == list(range(n))
     assert sorted(perm2) == list(range(n))
@@ -129,9 +151,33 @@ def solve():
     alert(perm1, perm2)
 
 
+import random
+while True:
+
+    n = random.randint(2,10)
+
+    perm = list(range(n))
+    random.shuffle(perm)
+    log(perm)
+
+    order = get_order(n)
+
+    def add2(pos):
+        pass
+
+    def query2(a,b):
+        aval = perm[a]
+        bval = perm[b]
+        return abs(order.index(aval) - order.index(bval))
+
+    def alert2(perm1, perm2):
+        assert perm1 == perm or perm2 == perm
+
+    solve(n, add=add2, query=query2, alert=alert2)
+
 
 for case_num in range(int(input())):
-    solve()
+    n = int(input())
+    solve(n)
 
-
-sys.exit()
+    sys.exit()
