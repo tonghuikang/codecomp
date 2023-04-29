0b0c8219137b99f9395ea3ca31d3e0970a64e21e
Hui Kang Tong <you@example.com>
2023-04-29 08:18:30 -0700

    dump

--- template/a.py
+++ template/a.py
@@ -52,15 +52,223 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+# -------------------------- Segment Tree --------------------------
+# https://atcoder.github.io/ac-library/master/document_en/segtree.html
+
+
+import typing
+
+def _ceil_pow2(n: int) -> int:
+    x = 0
+    while (1 << x) < n:
+        x += 1
+
+    return x
+
+
+def _bsf(n: int) -> int:
+    x = 0
+    while n % 2 == 0:
+        x += 1
+        n //= 2
+
+    return x
+
+
+class SegTree:
+    # https://github.com/not522/ac-library-python/blob/master/atcoder/segtree.py
+    def __init__(self,
+                 op: typing.Callable[[typing.Any, typing.Any], typing.Any],
+                 e: typing.Any,
+                 v: typing.Union[int, typing.List[typing.Any]]) -> None:
+        self._op = op
+        self._e = e
+
+        if isinstance(v, int):
+            v = [e] * v
+
+        self._n = len(v)
+        self._log = _ceil_pow2(self._n)
+        self._size = 1 << self._log
+        self._d = [e] * (2 * self._size)
+
+        for i in range(self._n):
+            self._d[self._size + i] = v[i]
+        for i in range(self._size - 1, 0, -1):
+            self._update(i)
+
+    def set(self, p: int, x: typing.Any) -> None:
+        assert 0 <= p < self._n
+
+        p += self._size
+        self._d[p] = x
+        for i in range(1, self._log + 1):
+            self._update(p >> i)
+
+    def get(self, p: int) -> typing.Any:
+        assert 0 <= p < self._n
+
+        return self._d[p + self._size]
+
+    def prod(self, left: int, right: int) -> typing.Any:
+        assert 0 <= left <= right <= self._n
+        sml = self._e
+        smr = self._e
+        left += self._size
+        right += self._size
+
+        while left < right:
+            if left & 1:
+                sml = self._op(sml, self._d[left])
+                left += 1
+            if right & 1:
+                right -= 1
+                smr = self._op(self._d[right], smr)
+            left >>= 1
+            right >>= 1
+
+        return self._op(sml, smr)
+
+    def all_prod(self) -> typing.Any:
+        return self._d[1]
+
+    def max_right(self, left: int,
+                  f: typing.Callable[[typing.Any], bool]) -> int:
+        assert 0 <= left <= self._n
+        assert f(self._e)
+
+        if left == self._n:
+            return self._n
+
+        left += self._size
+        sm = self._e
+
+        first = True
+        while first or (left & -left) != left:
+            first = False
+            while left % 2 == 0:
+                left >>= 1
+            if not f(self._op(sm, self._d[left])):
+                while left < self._size:
+                    left *= 2
+                    if f(self._op(sm, self._d[left])):
+                        sm = self._op(sm, self._d[left])
+                        left += 1
+                return left - self._size
+            sm = self._op(sm, self._d[left])
+            left += 1
+
+        return self._n
+
+    def min_left(self, right: int,
+                 f: typing.Callable[[typing.Any], bool]) -> int:
+        assert 0 <= right <= self._n
+        assert f(self._e)
+
+        if right == 0:
+            return 0
+
+        right += self._size
+        sm = self._e
+
+        first = True
+        while first or (right & -right) != right:
+            first = False
+            right -= 1
+            while right > 1 and right % 2:
+                right >>= 1
+            if not f(self._op(self._d[right], sm)):
+                while right < self._size:
+                    right = 2 * right + 1
+                    if f(self._op(self._d[right], sm)):
+                        sm = self._op(self._d[right], sm)
+                        right -= 1
+                return right + 1 - self._size
+            sm = self._op(self._d[right], sm)
+
+        return 0
+
+    def _update(self, k: int) -> None:
+        self._d[k] = self._op(self._d[2 * k], self._d[2 * k + 1])
+
+
+
+
+def solve_(arr, mrr, n, k):
     # your solution here
 
-    return ""
+    if n < 3:
+        return [n]*k
+
+    brr = []
+    idx = 3
+    for x,y,z in zip(arr,arr[1:],arr[2:]):
+        if x >= y >= z:
+            brr.append(idx)
+            idx += 1
+        else:
+            brr.append(0)
+
+    brr = [-1] + brr + [-2]
+
+    log(brr)
+
+    # LONGEST SUBSTRING OF ONE REPEATING CHARACTER
+    # https://leetcode.com/problems/longest-substring-of-one-repeating-character/
+
+    e = -1, -1, -1, -1, True, True, 0
+    # left count, left value, right count, right value, is contiguous, is empty, max result
+
+    def op(left, right):
+        lc1, lv1, rc1, rv1, contig1, empty1, mr1 = left
+        lc2, lv2, rc2, rv2, contig2, empty2, mr2 = right
+
+        if empty1 and empty2:
+            return e
+        if empty1:
+            return right
+        if empty2:
+            return left
+
+        if contig1 and contig2 and rv1 == lv2:
+            return mr1 + mr2, lv1, mr1 + mr2, rv2, True, False, mr1 + mr2
+
+        mr = max(mr1, mr2)
+        if rv1 == lv2:
+            mr = max(mr, rc1 + lc2)
+
+            if contig1:
+                return mr1 + lc2, lv1, rc2, rv2, False, False, mr
+
+            if contig2:
+                return lc1, lv1, rc1 + mr2, rv2, False, False, mr
+
+        return lc1, lv1, rc2, rv2, False, False, mr
+
+    def init(val):
+        return 1, val, 1, val, True, False, 1
+
+    st = SegTree(op, e, [init(x) for x in brr])
+    res = []
+
+    for a,b in mrr:
+        if a == b:
+            res.append(1)
+            continue
+        if a+1 == b:
+            res.append(2)
+            continue
+        a += 1
+        b -= 1
+        val = st.prod(a, b+1)[-1]
+        res.append(val + 2)
+
+    return res
 
 
-# for case_num in [0]:  # no loop over test case
+for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
-for case_num in range(int(input())):
+# for case_num in range(int(input())):
 
     # read line as an integer
     # n = int(input())
@@ -73,23 +281,23 @@ for case_num in range(int(input())):
     # arr = input().split()
 
     # read one line and parse each word as an integer
-    # a,b,c = list(map(int,input().split()))
-    # arr = list(map(int,input().split()))
+    n,k = list(map(int,input().split()))
+    arr = list(map(int,input().split()))
     # arr = minus_one(arr)
 
     # read multiple rows
     # arr = read_strings(k)  # and return as a list of str
-    # mrr = read_matrix(k)  # and return as a list of list of int
-    # mrr = minus_one_matrix(mrr)
+    mrr = read_matrix(k)  # and return as a list of list of int
+    mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(arr, mrr, n, k)  # include input here
 
     # print length if applicable
     # print(len(res))
 
     # parse result
     # res = " ".join(str(x) for x in res)
-    # res = "\n".join(str(x) for x in res)
+    res = "\n".join(str(x) for x in res)
     # res = "\n".join(" ".join(str(x) for x in row) for row in res)
 
     # print result
