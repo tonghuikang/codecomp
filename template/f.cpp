eb9f464f9e0983632825fa74c0f66d192a98348b
Hui Kang Tong <you@example.com>
2023-05-25 08:58:11 -0700

    dump

--- template/f.cpp
+++ template/f.cpp
@@ -1,65 +1,59 @@
-#include <algorithm>
-#include <functional>
+// GPT-4 rewrite
+
 #include <iostream>
-#include <set>
 #include <vector>
+#include <algorithm>
+#include <set>
+#include <iterator>
+#include <limits>
 
-int binary_search(std::function<bool(int)> func, bool first = true, bool target = true,
-                  int left = 0, int right = (1 << 31) - 1) {
-    auto func_wrapper = [&](int val) {
-        if (first ^ target) {
-            return !func(val);
-        }
-        return func(val);
-    };
-
+long long binary_search(std::function<bool(long long)> func,
+                        long long left = 0,
+                        long long right = (1LL << 31) - 1) {
     while (left < right) {
-        int mid = (left + right) / 2;
-        if (func_wrapper(mid)) {
+        long long mid = (left + right) / 2;
+        if (func(mid)) {
             right = mid;
         } else {
             left = mid + 1;
         }
     }
-    if (first) {
-        return left;
-    } else {
-        return left - 1;
-    }
+    return left;
 }
 
-int solve_(const std::vector<int>& arr, int n, int k) {
+long long solve_(const std::vector<int>& arr, int n, int k) {
     std::vector<int> sorted_arr(arr);
     std::sort(sorted_arr.begin(), sorted_arr.end());
 
-    auto func = [&](int64_t target) {
+    auto func = [&](long long target) {
         std::multiset<int> right_pool(sorted_arr.begin(), sorted_arr.begin() + k);
         std::multiset<int> left_pool;
 
         std::multiset<int> right_excess(sorted_arr.begin() + k, sorted_arr.end());
         std::multiset<int> left_excess;
 
-        int64_t right_pool_sum = 0;
+        long long right_pool_sum = 0;
         for (int val : right_pool) {
             right_pool_sum += val;
         }
-        int64_t left_pool_sum = 0;
+
+        long long left_pool_sum = 0;
 
         for (int x : arr) {
             left_pool.insert(x);
             left_pool_sum += x;
 
-            if (right_pool.count(x) > 0) {
-                right_pool.erase(right_pool.find(x));
+            auto it = right_pool.find(x);
+            if (it != right_pool.end()) {
+                right_pool.erase(it);
                 right_pool_sum -= x;
             } else {
                 right_excess.erase(right_excess.find(x));
-
                 if (!right_pool.empty()) {
                     int val = *right_pool.rbegin();
+                    right_pool.erase(right_pool.find(val));
                     right_pool_sum -= val;
                     right_excess.insert(val);
-                    right_pool.erase(right_pool.find(val));
                 }
             }
 
@@ -69,14 +63,14 @@ int solve_(const std::vector<int>& arr, int n, int k) {
                 }
 
                 int val = *left_pool.rbegin();
+                left_pool.erase(left_pool.find(val));
                 left_pool_sum -= val;
                 left_excess.insert(val);
-                left_pool.erase(left_pool.find(val));
 
                 val = *right_excess.begin();
+                right_excess.erase(right_excess.begin());
                 right_pool_sum += val;
                 right_pool.insert(val);
-                right_excess.erase(right_excess.begin());
             }
 
             if (std::max(left_pool_sum, right_pool_sum) <= target) {
@@ -87,7 +81,7 @@ int solve_(const std::vector<int>& arr, int n, int k) {
         return false;
     };
 
-    return binary_search(func, true, true, 0, 1e16);
+    return binary_search(func, 0, 1e16);
 }
 
 int main() {
@@ -100,7 +94,9 @@ int main() {
         for (int i = 0; i < n; ++i) {
             std::cin >> arr[i];
         }
-        int res = solve_(arr, n, k);
+        long long res = solve_(arr, n, k);
         std::cout << res << std::endl;
     }
+
+    return 0;
 }
