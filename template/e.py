172f6feccdb11d6e4ec79b62ac3a060a9ac2add1
Hui Kang Tong <you@example.com>
2023-06-03 09:32:22 -0700

    dump

--- template/e.py
+++ template/e.py
@@ -2,34 +2,21 @@
 import sys, getpass
 input = sys.stdin.readline  # to read input quickly
 
-# available on Google, AtCoder Python3, not available on Codeforces
-# import numpy as np
-# import scipy
-
-m9 = 10**9 + 7  # 998244353
-# d4 = [(1,0),(0,1),(-1,0),(0,-1)]
-# d8 = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
-# d6 = [(2,0),(1,1),(-1,1),(-2,0),(-1,-1),(1,-1)]  # hexagonal layout
-MAXINT = sys.maxsize
-e18 = 10**18 + 10
-
-# if testing locally, print to terminal with a different color
-OFFLINE_TEST = getpass.getuser() == "htong"
-# OFFLINE_TEST = False  # codechef does not allow getpass
-def log(*args):
-    if OFFLINE_TEST:
-        print('\033[36m', *args, '\033[0m', file=sys.stderr)
 
 # ---------------------------- template ends here ----------------------------
 
 def compute(arr):
     arr = sorted(arr)
     vals = set([0])
+    suffix_size = sum(arr)
     for x in arr:
+        suffix_size -= x
         new_vals = set()
         for y in vals:
-            new_vals.add(x+y)
-            new_vals.add(x-y)
+            if x+y <= suffix_size:
+                new_vals.add(x+y)
+            if x+y >= -suffix_size:
+                new_vals.add(x-y)
         vals = new_vals
     # log(arr, 0 in vals)
     return 0 in vals
@@ -68,16 +55,18 @@ if compute(arr):
                 continue
             if arr[pos] == 0:
                 continue
-            brr = [x for x in arr]
 
             val = min(arr[pos], arr[idx])
-            brr[pos] -= val
-            brr[idx] -= val
+            arr[pos] -= val
+            arr[idx] -= val
 
-            if compute(brr):
-                arr = brr
+            if compute(arr):
                 print("{}".format(pos+1), flush=True)
                 break
+
+            arr[pos] += val
+            arr[idx] += val
+
         else:
             break
 
