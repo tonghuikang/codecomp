c3917eb496a8e75e2e9fcfb175e2838557cbd948
Hui Kang Tong <you@example.com>
2023-07-29 06:45:17 -0700

    dump

--- template/z.py
+++ template/z.py
@@ -53,19 +53,72 @@ def minus_one_matrix(mrr):
 
 # ---------------------------- template ends here ----------------------------
 
+def get_largest_prime_factors(num):
+    # get largest prime factor for each number
+    # you can use this to obtain primes
+    largest_prime_factors = [1] * num
+    for i in range(2, num):
+        if largest_prime_factors[i] > 1:  # not prime
+            continue
+        for j in range(i, num, i):
+            largest_prime_factors[j] = i
+    return largest_prime_factors
+
+
+SIZE_OF_PRIME_ARRAY = 10**6 + 10
+largest_prime_factors = get_largest_prime_factors(SIZE_OF_PRIME_ARRAY)   # take care that it begins with [1,1,2,...]
+primes = [x for i,x in enumerate(largest_prime_factors[2:], start=2) if x == i]
+
+
+def get_prime_factors_with_precomp(num):
+    # requires precomputed `largest_prime_factors``
+    # for numbers below SIZE_OF_PRIME_ARRAY
+    # O(log n)
+    factors = []
+    lf = largest_prime_factors[num]
+    while lf != num:
+        factors.append(lf)
+        num //= lf
+        lf = largest_prime_factors[num]
+    if num > 1:
+        factors.append(num)
+    return factors
+
+def get_all_divisors_given_prime_factorization(factors):
+    c = Counter(factors)
+
+    divs = [1]
+    for prime, count in c.most_common()[::-1]:
+        l = len(divs)
+        prime_pow = 1
+
+        for _ in range(count):
+            prime_pow *= prime
+            for j in range(l):
+                divs.append(divs[j]*prime_pow)
+
+    # NOT IN SORTED ORDER
+    return divs
+
+
+def factorize_name(name):
+    factors = sorted(get_all_divisors_given_prime_factorization(len(name)))
+    factors.sort()
+
+    for factor in factors:
+        if name == name[:factor] * (len(name) // factor):
+            return name[:factor]
+
 
 def solve_(n,arr):
     # your solution here
 
-    names = set()
+    factor_to_count = defaultdict(int)
     res = []
 
-    for x in arr:
-        k = 1
-        while x*k in names:
-            k += 1
-        names.add(x*k)
-        res.append(k)
+    for name in names:
+        factor = factorize_name(name)
+        log(name, factor)
 
     return res
 
