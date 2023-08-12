3c22b93b4ec094cff078cbd49e9783a4607a7db2
Hui Kang Tong <you@example.com>
2023-08-12 06:12:34 -0700

    dump

--- template/f.py
+++ template/f.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 import sys
-import math, random
-import functools, itertools, collections, heapq, bisect
-from collections import Counter, defaultdict, deque
+from collections import defaultdict
 input = sys.stdin.readline  # to read input quickly
 
 # available on Google, AtCoder Python3, not available on Codeforces
 # import numpy as np
 # import scipy
 
-m9 = 10**9 + 7  # 998244353
+m9 = 998244353
 yes, no = "YES", "NO"
 # d4 = [(1,0),(0,1),(-1,0),(0,-1)]
 # d8 = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
@@ -54,10 +52,95 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
+class DisjointSet:
+    # github.com/not522/ac-library-python/blob/master/atcoder/dsu.py
+    # faster implementation of DSU
+    def __init__(self, n: int = 0) -> None:
+        if n > 0:  # constant size DSU
+            self.parent_or_size = [-1]*n
+        else:
+            # WARNING: non-negative numeric elements only
+            self.parent_or_size = defaultdict(lambda: -1)
+
+    def union(self, a: int, b: int) -> int:
+        x = self.find(a)
+        y = self.find(b)
+
+        if x == y:
+            return x
+
+        if -self.parent_or_size[x] < -self.parent_or_size[y]:
+            x, y = y, x
+
+        self.parent_or_size[x] += self.parent_or_size[y]
+        self.parent_or_size[y] = x
+
+        return x
+
+    def find(self, a: int) -> int:
+        parent = self.parent_or_size[a]
+        while parent >= 0:
+            if self.parent_or_size[parent] < 0:
+                return parent
+            self.parent_or_size[a], a, parent = (
+                self.parent_or_size[parent],
+                self.parent_or_size[parent],
+                self.parent_or_size[self.parent_or_size[parent]]
+            )
+        return a
+
+    def size(self, a: int) -> int:
+        return -self.parent_or_size[self.find(a)]
+
+
+def modinv_p(base, p=m9):
+    
+    # modular inverse if the modulo is a prime
+    return pow(base, -1, p)  # for Python 3.8+
+    # return pow(base, p-2, p)  # if Python version is below 3.8
+
+
+
 def solve_(n, mrr):
     # your solution here
 
-    return ""
+    # traverse down the binary tree and give value
+    ds = DisjointSet(n)
+    val = [1 for _ in range(n+n-1)]
+    pairs = [(-1,-1) for _ in range(n+n-1)]
+    leads = [x for x in range(n+n-1)]
+
+    for i,(a,b) in enumerate(mrr, start=n):
+        aa = leads[ds.find(a)]
+        bb = leads[ds.find(b)]
+
+        ds.union(a,b)
+        ds.find(a)
+
+        x = ds.size(a)
+        val[i] = x
+        
+        pairs[i] = (aa,bb)
+        leads[ds.find(a)] = i
+
+    res = [0 for _ in range(n+n-1)]
+
+    for i,(a,b) in list(enumerate(mrr, start=n))[::-1]:
+        left, right = pairs[i]
+        left_size = val[left]
+        right_size = val[right]
+        
+        res[left] += ((left_size * modinv_p(left_size + right_size)) + res[i])%m9
+        res[right] += ((right_size * modinv_p(left_size + right_size)) + res[i])%m9
+
+    #     res[left] += (left_size / (left_size + right_size)) + res[i] 
+    #     res[right] += (right_size / (left_size + right_size)) + res[i]
+
+    # log(res)
+
+    return [x%m9 for x in res[:n]]
+
+    # return res[:n]
 
 
 for case_num in [0]:  # no loop over test case
@@ -82,7 +165,7 @@ for case_num in [0]:  # no loop over test case
     # read multiple rows
     # arr = read_strings(k)  # and return as a list of str
     mrr = read_matrix(n-1)  # and return as a list of list of int
-    # mrr = minus_one_matrix(mrr)
+    mrr = minus_one_matrix(mrr)
 
     res = solve(n, mrr)  # include input here
 
@@ -90,7 +173,7 @@ for case_num in [0]:  # no loop over test case
     # print(len(res))
 
     # parse result
-    # res = " ".join(str(x) for x in res)
+    res = " ".join(str(x) for x in res)
     # res = "\n".join(str(x) for x in res)
     # res = "\n".join(" ".join(str(x) for x in row) for row in res)
 
