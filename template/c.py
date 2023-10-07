06a3a00a54ae88b44a0f7fa5b7831d66b3f96471
Hui Kang Tong <you@example.com>
2023-10-07 09:12:41 -0700

    dump

--- template/c.py
+++ template/c.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 import sys
-import math, random
-import functools, itertools, collections, heapq, bisect
-from collections import Counter, defaultdict, deque
 
 input = sys.stdin.readline  # to read input quickly
 
@@ -63,17 +60,93 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_():
+def solve_(n, arr):
     # your solution here
 
-    return ""
+    if n == 1:
+        return 1
+
+    arr.sort()
+    minres = 10**18
+
+    target_sum = arr[0] + arr[-2]
+    addn = target_sum - arr[-1]
+    brr = [addn] + arr
+
+
+    for a,b in zip(brr, brr[::-1]):
+        if a + b != target_sum:
+            break
+    else:
+        if addn > 0:
+            minres = min(minres, addn)
+
+    # log(addn)
+    del addn
+
+    target_sum = arr[1] + arr[-1]
+    addn = target_sum - arr[0]
+    brr = arr + [addn]
+
+    for a,b in zip(brr, brr[::-1]):
+        if a + b != target_sum:
+            break
+    else:
+        if addn > 0:
+            minres = min(minres, addn)
+
+    # log(addn)
+    del addn
+
+    # from collections import Counter
+    # log(Counter(arr).items())
+
+    target_sum = arr[0] + arr[-1]
+    for a,b in zip(arr[:n], arr[::-1]):
+        log("-")
+        log(a,b)
+        if a + b != target_sum:
+            break
+
+    addn1 = target_sum - a
+    addn2 = target_sum - b
+
+    log("x")
+    log(target_sum)
+    log(addn1)
+    log(addn2)
+
+    brr = arr + [addn1]
+    brr.sort()
+
+    for a,b in zip(brr, brr[::-1]):
+        if a + b != target_sum:
+            break
+    else:
+        if addn1 > 0:
+            minres = min(minres, addn1)
+
+    brr = arr + [addn2]
+    brr.sort()
+
+    for a,b in zip(brr, brr[::-1]):
+        if a + b != target_sum:
+            break
+    else:
+        if addn2 > 0:
+            minres = min(minres, addn2)
+
+    if minres == 10**18:
+        return -1
+
+    return minres
 
 
 # for case_num in [0]:  # no loop over test case
 # for case_num in range(100):  # if the number of test cases is specified
 for case_num in range(int(input())):
     # read line as an integer
-    # n = int(input())
+    n = int(input())
     # k = int(input())
 
     # read line as a string
@@ -84,7 +157,7 @@ for case_num in range(int(input())):
 
     # read one line and parse each word as an integer
     # a,b,c = list(map(int,input().split()))
-    # arr = list(map(int,input().split()))
+    arr = list(map(int,input().split()))
     # arr = minus_one(arr)
 
     # read multiple rows
@@ -92,7 +165,7 @@ for case_num in range(int(input())):
     # mrr = read_matrix(k)  # and return as a list of list of int
     # mrr = minus_one_matrix(mrr)
 
-    res = solve()  # include input here
+    res = solve(n, arr)  # include input here
 
     # print length if applicable
     # print(len(res))
@@ -103,6 +176,6 @@ for case_num in range(int(input())):
     # res = "\n".join(" ".join(str(x) for x in row) for row in res)
 
     # print result
-    # print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
+    print("Case #{}: {}".format(case_num+1, res))   # Google and Facebook - case number required
 
-    print(res)
+    # print(res)
