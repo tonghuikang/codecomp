5673c8a71fd0e7fcdbdb79ae47cb69a282c654c9
Hui Kang Tong <you@example.com>
2023-10-22 01:57:50 -0700

    dump

--- template/d.py
+++ template/d.py
@@ -1,116 +1,133 @@
-#!/usr/bin/env python3
-import sys
-from collections import Counter, defaultdict
-input = sys.stdin.readline  # to read input quickly
+#include <bits/stdc++.h>
+using namespace std;
 
+const int SIZE_OF_PRIME_ARRAY = 1e6 + 10;
+vector<int> largest_prime_factors(SIZE_OF_PRIME_ARRAY);
+vector<int> primes;
 
-# ---------------------------- template ends here ----------------------------
+vector<int> get_largest_prime_factors(int num) {
+    vector<int> largest_prime_factors(num, 1);
+    for(int i = 2; i < num; ++i) {
+        if(largest_prime_factors[i] > 1) continue;
+        for(int j = i; j < num; j += i) {
+            largest_prime_factors[j] = i;
+        }
+    }
+    return largest_prime_factors;
+}
 
+vector<int> get_prime_factors_with_precomp(int num) {
+    if(num == 0) return {};
+    vector<int> factors;
+    int lf = largest_prime_factors[num];
+    while(lf != num) {
+        factors.push_back(lf);
+        num /= lf;
+        lf = largest_prime_factors[num];
+    }
+    if(num > 1) factors.push_back(num);
+    return factors;
+}
 
-def get_largest_prime_factors(num):
-    # get largest prime factor for each number
-    # you can use this to obtain primes
-    largest_prime_factors = [1] * num
-    for i in range(2, num):
-        if largest_prime_factors[i] > 1:  # not prime
-            continue
-        for j in range(i, num, i):
-            largest_prime_factors[j] = i
-    return largest_prime_factors
+vector<int> get_all_divisors_given_prime_factorization(vector<int> factors) {
+    map<int, int> c;
+    for(int factor : factors) {
+        c[factor]++;
+    }
 
+    vector<int> divs = {1};
+    for(auto [prime, count] : c) {
+        int prime_pow = 1;
+        for(int i = 0; i < count; ++i) {
+            prime_pow *= prime;
+            int l = divs.size();
+            for(int j = 0; j < l; ++j) {
+                divs.push_back(divs[j] * prime_pow);
+            }
+        }
+    }
+    sort(divs.begin(), divs.end(), greater<int>());
+    return divs;
+}
 
-SIZE_OF_PRIME_ARRAY = 10**6 + 10
-largest_prime_factors = get_largest_prime_factors(SIZE_OF_PRIME_ARRAY)  # take care that it begins with [1,1,2,...]
-primes = [x for i, x in enumerate(largest_prime_factors[2:], start=2) if x == i]
+vector<int> divisors(int x) {
+    return get_all_divisors_given_prime_factorization(get_prime_factors_with_precomp(x));
+}
 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-def get_prime_factors_with_precomp(num):
-    if num == 0:
-        return []
-    # requires precomputed `largest_prime_factors``
-    # for numbers below SIZE_OF_PRIME_ARRAY
-    # O(log n)
-    factors = []
-    lf = largest_prime_factors[num]
-    while lf != num:
-        factors.append(lf)
-        num //= lf
-        lf = largest_prime_factors[num]
-    if num > 1:
-        factors.append(num)
-    return factors
+    largest_prime_factors = get_largest_prime_factors(SIZE_OF_PRIME_ARRAY);
+    for(int i = 2; i < SIZE_OF_PRIME_ARRAY; ++i) {
+        if(largest_prime_factors[i] == i) {
+            primes.push_back(i);
+        }
+    }
 
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int i = 0; i < n; ++i) {
+            cin >> arr[i];
+        }
 
-def get_all_divisors_given_prime_factorization(factors):
-    c = Counter(factors)
+        map<int, vector<int>> small_to_large;
+        map<int, int> divisor_count, cntr;
+        set<int> allset;
 
-    divs = [1]
-    for prime, count in c.most_common()[::-1]:
-        l = len(divs)
-        prime_pow = 1
+        for(int num : arr) {
+            cntr[num]++;
+            allset.insert(num);
+        }
 
-        for _ in range(count):
-            prime_pow *= prime
-            for j in range(l):
-                divs.append(divs[j] * prime_pow)
+        for(auto [k, v] : cntr) {
+            for(int divisor : divisors(k)) {
+                divisor_count[divisor] += v;
+                if(divisor != k) {
+                    small_to_large[divisor].push_back(k);
+                }
+            }
+        }
 
-    # NOT IN SORTED ORDER
-    return sorted(divs, reverse=True)
+        long long allres = 0;
 
+        for(auto [k, v0] : cntr) {
+            int v = v0;
+            map<int, int> cur_counts;
 
-def divisors(x):
-    return get_all_divisors_given_prime_factorization(get_prime_factors_with_precomp(x))
-    
+            for(int large : small_to_large[k]) {
+                v += cntr[large];
+            }
 
-for case_num in range(int(input())):
-    n = int(input())
-    arr = list(map(int,input().split()))
+            for(int divisor : divisors(k)) {
+                cur_counts[divisor] += divisor_count[divisor];
+                cur_counts[divisor] -= v;
+            }
 
-    small_to_large = defaultdict(list)
+            for(int divisor : divisors(k)) {
+                int deductible = cur_counts[divisor];
+                bool flag = false;
+                for(int d : divisors(divisor)) {
+                    if(allset.find(d) != allset.end()) {
+                        flag = true;
+                    }
+                    cur_counts[d] -= deductible;
+                }
+                if(flag) {
+                    v += deductible;
+                }
+            }
 
-    divisor_count = defaultdict(int)
-    cntr = Counter(arr)
-    allset = set(arr)
+            int res = n - v;
+            allres += 1LL * res * v0;
+        }
 
-    # log(cntr)
+        cout << (allres / 2) << "\n";
+    }
 
-    for k,v in cntr.items():
-        for divisor in divisors(k):
-            divisor_count[divisor] += v
-            if divisor != k:
-                small_to_large[divisor].append(k)
-    
-    # log(divisor_count)
-
-    allres = 0
-
-    for k,v in cntr.items():
-        v0 = v
-        cur_counts = defaultdict(int)
-
-        for large in small_to_large[k]:
-            v += cntr[large]
-
-        for divisor in divisors(k):
-            cur_counts[divisor] += divisor_count[divisor]
-            cur_counts[divisor] -= v
-
-        # log("cur_counts", {k:v for k,v in cur_counts.items() if v != 0})
-
-        for divisor in divisors(k):
-            deductible = cur_counts[divisor]
-            flag = False
-            for d in divisors(divisor):
-                if d in allset:
-                    flag = True
-                cur_counts[d] -= deductible
-            if flag:
-                v += deductible
-
-        res = n - v
-        # log(k, v0, v, res)
-        # log("cur_counts", {k:v for k,v in cur_counts.items() if v != 0})
-        allres += res * v0
-
-    # assert allres%2 == 0
-    print(allres // 2)
+    return 0;
+}
