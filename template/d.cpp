fa73d8b93a81ca65f5f231d95c87247b8b0ce561
Hui Kang Tong <you@example.com>
2023-10-22 02:08:26 -0700

    dump

--- template/d.cpp
+++ template/d.cpp
@@ -1,17 +1,12 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <algorithm>
-#include <set>
+#include <bits/stdc++.h>
+using namespace std;
 
-const int SIZE_OF_PRIME_ARRAY = 1000010;
-std::vector<int> largest_prime_factors(SIZE_OF_PRIME_ARRAY);
-std::vector<int> primes;
+const int SIZE_OF_PRIME_ARRAY = 1e6 + 10;
 
-std::vector<int> get_largest_prime_factors(int num) {
+vector<int> get_largest_prime_factors(int num) {
+    vector<int> largest_prime_factors(num, 1);
     for (int i = 2; i < num; ++i) {
-        if (largest_prime_factors[i] > 1)
-            continue;
+        if (largest_prime_factors[i] > 1) continue;
         for (int j = i; j < num; j += i) {
             largest_prime_factors[j] = i;
         }
@@ -19,27 +14,27 @@ std::vector<int> get_largest_prime_factors(int num) {
     return largest_prime_factors;
 }
 
-std::vector<int> get_prime_factors_with_precomp(int num) {
-    std::vector<int> factors;
-    int lf = largest_prime_factors[num];
-    while (lf != num) {
-        factors.push_back(lf);
-        num /= lf;
-        lf = largest_prime_factors[num];
+vector<int> largest_prime_factors = get_largest_prime_factors(SIZE_OF_PRIME_ARRAY);
+vector<int> primes;
+
+vector<int> get_prime_factors_with_precomp(int num) {
+    if (num == 0) return {};
+    vector<int> factors;
+    while (largest_prime_factors[num] != num) {
+        factors.push_back(largest_prime_factors[num]);
+        num /= largest_prime_factors[num];
     }
-    if (num > 1) {
-        factors.push_back(num);
-    }
+    if (num > 1) factors.push_back(num);
     return factors;
 }
 
-std::vector<int> get_all_divisors_given_prime_factorization(const std::vector<int>& factors) {
-    std::map<int, int> c;
-    for (int factor : factors) {
-        c[factor]++;
+vector<int> get_all_divisors_given_prime_factorization(const vector<int>& factors) {
+    unordered_map<int, int> c;
+    for (int f : factors) {
+        c[f]++;
     }
-    std::vector<int> divs = {1};
-    for (const auto& [prime, count] : c) {
+    vector<int> divs = {1};
+    for (auto [prime, count] : c) {
         int l = divs.size();
         int prime_pow = 1;
         for (int i = 0; i < count; ++i) {
@@ -49,40 +44,37 @@ std::vector<int> get_all_divisors_given_prime_factorization(const std::vector<in
             }
         }
     }
-    std::sort(divs.begin(), divs.end(), std::greater<int>());
+    sort(divs.begin(), divs.end(), greater<int>());
     return divs;
 }
 
-std::vector<int> divisors(int x) {
+vector<int> divisors(int x) {
     return get_all_divisors_given_prime_factorization(get_prime_factors_with_precomp(x));
 }
 
 int main() {
-    largest_prime_factors = get_largest_prime_factors(SIZE_OF_PRIME_ARRAY);
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    
     for (int i = 2; i < SIZE_OF_PRIME_ARRAY; ++i) {
-        if (largest_prime_factors[i] == i) {
-            primes.push_back(i);
-        }
+        if (largest_prime_factors[i] == i) primes.push_back(i);
     }
 
     int t;
-    std::cin >> t;
+    cin >> t;
     while (t--) {
         int n;
-        std::cin >> n;
-        std::vector<int> arr(n);
-        for (int i = 0; i < n; ++i) {
-            std::cin >> arr[i];
-        }
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; ++i) cin >> arr[i];
 
-        std::map<int, std::vector<int>> small_to_large;
-        std::map<int, int> divisor_count, cntr;
+        unordered_map<int, vector<int>> small_to_large;
+        unordered_map<int, int> divisor_count, cntr;
 
         for (int num : arr) {
             cntr[num]++;
         }
 
-        for (const auto& [k, v] : cntr) {
+        for (auto [k, v] : cntr) {
             for (int divisor : divisors(k)) {
                 divisor_count[divisor] += v;
                 if (divisor != k) {
@@ -91,13 +83,14 @@ int main() {
             }
         }
 
-        int allres = 0;
-        for (const auto& [k, v] : cntr) {
+        long long allres = 0;
+        for (auto [k, v] : cntr) {
             int v0 = v;
-            std::map<int, int> cur_counts;
+            unordered_map<int, int> cur_counts;
             for (int large : small_to_large[k]) {
                 v += cntr[large];
             }
+
             for (int divisor : divisors(k)) {
                 cur_counts[divisor] += divisor_count[divisor];
                 cur_counts[divisor] -= v;
@@ -118,10 +111,10 @@ int main() {
             }
 
             int res = n - v;
-            allres += res * v0;
+            allres += (long long)res * v0;
         }
-
-        std::cout << (allres / 2) << std::endl;
+        cout << allres / 2 << "\n";
     }
+
     return 0;
 }
