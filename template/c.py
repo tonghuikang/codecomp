8cb0170b005827658080a08c4cb18ed47bf844cc
Hui Kang Tong <you@example.com>
2023-12-24 07:03:21 -0800

    dump

--- template/c.py
+++ template/c.py
@@ -1,69 +1,13 @@
 #!/usr/bin/env python3
 import sys
-import math, random
-import functools, itertools, collections, heapq, bisect
-from collections import Counter, defaultdict, deque
 
 input = sys.stdin.readline  # to read input quickly
-
-# available on Google, AtCoder Python3, not available on Codeforces
-# import numpy as np
-# import scipy
-
-m9 = 10**9 + 7  # 998244353
-yes, no = "YES", "NO"
-# d4 = [(1,0),(0,1),(-1,0),(0,-1)]
-# d8 = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
-# d6 = [(2,0),(1,1),(-1,1),(-2,0),(-1,-1),(1,-1)]  # hexagonal layout
-# abc = "abcdefghijklmnopqrstuvwxyz"
-# abc_map = {c:i for i,c in enumerate(abc)}
-MAXINT = sys.maxsize
-e18 = 10**18 + 10
-
-# if testing locally, print to terminal with a different color
-OFFLINE_TEST = False
-CHECK_OFFLINE_TEST = True
-# CHECK_OFFLINE_TEST = False  # uncomment this on Codechef
-if CHECK_OFFLINE_TEST:
-    import getpass
-
-    OFFLINE_TEST = getpass.getuser() == "htong"
-
-
-def log(*args):
-    if CHECK_OFFLINE_TEST and OFFLINE_TEST:
-        print("\033[36m", *args, "\033[0m", file=sys.stderr)
-
-
-def solve(*args):
-    # screen input
-    if OFFLINE_TEST:
-        log("----- solving ------")
-        log(*args)
-        log("----- ------- ------")
-    return solve_(*args)
-
-
-def read_matrix(rows):
-    return [list(map(int, input().split())) for _ in range(rows)]
-
-
-def read_strings(rows):
-    return [input().strip() for _ in range(rows)]
-
-
-def minus_one(arr):
-    return [x - 1 for x in arr]
-
-
-def minus_one_matrix(mrr):
-    return [[x - 1 for x in row] for row in mrr]
 
 
 # ---------------------------- template ends here ----------------------------
 
 
-def solve_(n,k,d,arr,vrr):
+def solve(n,k,d,arr,vrr):
     # your solution here
 
     # grow once and reset, repeat
