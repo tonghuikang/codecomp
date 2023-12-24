ccd280c3d1c8183d8ddfe63a2cca8e33f677eea2
Hui Kang Tong <you@example.com>
2023-12-23 20:52:42 -0800

    rabin_karp

--- template/template_arrays.py
+++ template/template_arrays.py
@@ -240,22 +240,36 @@ def all_nearest_smaller_values(arr):
 
 
 def rabin_karp(arr, window_size, modulus):
-    # return all hashes of each substring of a certain window_size
-    # run this multiple times to avoid hash collisions
+    # returns the hashes of
+    # [arr[i:i+window_size] for i in range(len(arr) - window_size + 1)]
+    # sample multiple modulus to avoid getting hacked
     h, t, d = (1 << (17 * window_size - 17)) % modulus, 0, 1 << 17
-    all_hashes = set()
+    all_hashes = []
 
     for i in range(window_size):
         t = (d * t + arr[i]) % modulus
 
-    all_hashes.add(t)
+    all_hashes.append(t)
 
     for i in range(len(arr) - window_size):
         t = (d * (t - arr[i] * h) + arr[i + window_size]) % modulus
-        all_hashes.add(t)
+        all_hashes.append(t)
 
     return all_hashes
 
+# example of modulus that you can use
+[
+    1000000007, 1000000009, 1000000021, 1000000033, 1000000087, 
+    1000000093, 1000000097, 1000000103, 1000000123, 1000000181, 
+    1000000207, 1000000223, 1000000241, 1000000271, 1000000289, 
+    1000000297, 1000000321, 1000000349, 1000000363, 1000000403, 
+    1000000409, 1000000411, 1000000427, 1000000433, 1000000439, 
+    1000000447, 1000000453, 1000000459, 1000000483, 1000000513, 
+    1000000531, 1000000579, 1000000607, 1000000613, 1000000637, 
+    1000000663, 1000000711, 1000000753, 1000000787, 1000000801, 
+    1000000829, 1000000861, 1000000871, 1000000891, 1000000901, 
+    1000000919, 1000000931, 1000000933, 1000000993, 1000001011
+]
 
 def sort_deques(deques):
     # in place sort
