7edf4fe96f1a1f97641e37f81dad9e18e3065099
Hui Kang Tong <you@example.com>
2024-04-30 08:21:58 -0700

    dump

--- template/c.py
+++ template/c.py
@@ -63,10 +63,76 @@ def minus_one_matrix(mrr):
 # ---------------------------- template ends here ----------------------------
 
 
+
+def binary_search(
+    func_,  # condition function
+    first=True,  # else last
+    target=True,  # else False
+    left=0,
+    right=2**31 - 1,
+) -> int:
+    # https://leetcode.com/discuss/general-discussion/786126/
+    # ASSUMES THAT THERE IS A TRANSITION
+    # MAY HAVE ISSUES AT THE EXTREMES
+
+    def func(val):
+        # if first True or last False, assume search space is in form
+        # [False, ..., False, True, ..., True]
+
+        # if first False or last True, assume search space is in form
+        # [True, ..., True, False, ..., False]
+        # for this case, func will now be negated
+        if first ^ target:
+            return not func_(val)
+        return func_(val)
+
+    while left < right:
+        mid = (left + right) // 2
+        if func(mid):
+            right = mid
+        else:
+            left = mid + 1
+    if first:  # find first True
+        return left
+    else:  # find last False
+        return left - 1
+
+
 def solve_(n,k,arr):
     # your solution here
 
-    return ""
+    # fill to brim first, find the break point
+
+    def func(val):
+        # you could fill everything to at least val
+        cost = sum(max(0, val-x) for x in arr)
+        if cost <= k:
+            return True
+        
+    val = binary_search(func, first=False, target=True, left=0, right=10**15)
+
+    brr = [max(val, x) for x in arr]
+    
+    if len(set(brr)) == 1:
+        return val * len(brr) - (n-1)
+
+    k -= sum(brr) - sum(arr)
+
+    binarr = [int(x > val) for x in brr]
+
+    binarr = binarr + binarr + binarr
+    idxs = [i for i,x in enumerate(binarr) if x == 0]
+    log(brr, k)
+    # log(binarr)
+    # log(idxs)
+
+    maxdist = 0
+    for a,b in zip(idxs, idxs[1+k:]):
+        maxdist = max(maxdist, b - a)
+
+    log(n, val, maxdist)
+
+    return (val - 1) * len(brr) + (maxdist)
 
 
 # for case_num in [0]:  # no loop over test case
