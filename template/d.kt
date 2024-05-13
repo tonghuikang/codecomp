e7d546549be20c6f20a2d2ba91e00fb4b4ab2f65
Hui Kang Tong <you@example.com>
2024-05-13 09:52:18 -0700

    dump

--- template/d.kt
+++ template/d.kt
@@ -5,43 +5,52 @@ fun main() {
     val scanner = Scanner(System.`in`)
     val LARGE = 1e18.toLong() + 10
 
-    val t = scanner.nextInt()  // Read the number of test cases
+    val t = scanner.nextInt()  // Number of test cases
     repeat(t) {
-        val n = scanner.nextInt()  // Read the size of the array
+        val n = scanner.nextInt()  // Size of the array
         val arr = LongArray(n) { scanner.nextLong() }
 
-        val dp = Array(2) { LongArray(2) { LARGE } }
+        var dp = Array(2) { LongArray(2) { LARGE } }
         dp[1][0] = 0
         dp[1][1] = 0
 
         for (x in arr) {
             if (x == 0L) {
-                dp[1][0] = LARGE
-                dp[1][1] = min(dp[1][1], dp[0][1])
+                if (compareTuples(dp[0], dp[1]) < 0) {
+                    dp[1] = dp[0]
+                }
+                dp[0][0] = LARGE
+                dp[0][1] = LARGE
                 continue
             }
 
             val newDp = Array(2) { LongArray(2) { LARGE } }
 
-            // Fix the current step with fix-forward
-            newDp[1][0] = min(newDp[1][0], dp[0][0])
-            newDp[1][1] = min(newDp[1][1], dp[0][1] + 2 * x)
+            // Transition considering the current step with fix-forward
+            val fixedForward = longArrayOf(dp[0][0], dp[0][1] + 2 * x)
+            val noFixForward = longArrayOf(dp[1][0] + 1, dp[1][1] + x)
+            val fixForwardCurrent = longArrayOf(dp[1][0] + 1, dp[1][1] + 2 * x)
 
-            // Fix the current step with no fix-forward
-            newDp[1][0] = min(newDp[1][0], dp[1][0] + 1)
-            newDp[1][1] = min(newDp[1][1], dp[1][1] + x)
-            
-            // Fix-forward the current step
-            newDp[0][0] = min(newDp[0][0], dp[1][0] + 1)
-            newDp[0][1] = min(newDp[0][1], dp[1][1] + 2 * x)
+            // Maintain the minimum tuple
+            if (compareTuples(noFixForward, fixedForward) < 0) {
+                newDp[1] = noFixForward
+            } else {
+                newDp[1] = fixedForward
+            }
 
-            dp[0][0] = newDp[0][0]
-            dp[0][1] = newDp[0][1]
-            dp[1][0] = newDp[1][0]
-            dp[1][1] = newDp[1][1]
+            newDp[0] = fixForwardCurrent
+
+            dp = newDp
         }
 
         val result = dp[1][1]
         println(result)
     }
+}
+
+fun compareTuples(a: LongArray, b: LongArray): Int {
+    if (a[0] != b[0]) {
+        return a[0].compareTo(b[0])
+    }
+    return a[1].compareTo(b[1])
 }
